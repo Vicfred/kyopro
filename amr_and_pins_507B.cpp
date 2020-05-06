a6ad333df6ac8cda699e3860aa6fa44058da388b
Vicfred <vicfred@vicfred.dev>
2020-05-05 20:24:24 -0500

    amr and pins 507B

---
+++ amr_and_pins_507B.cpp
@@ -0,0 +1,24 @@
+// Vicfred
+// https://codeforces.com/problemset/problem/507/B
+// math, greedy
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long double euclidean(long long int x, long long int y, long long int u, long long int v) {
+    return sqrt((x-u)*(x-u) + (y-v)*(y-v));
+}
+
+int main() {
+    long long int r, x, y, u, v;
+    cin >> r >> x >> y >> u >> v;
+
+    long double dist = euclidean(x, y, u, v);
+
+    cout << static_cast<long long int>(ceil((dist)/(2.0*r))) << endl;
+
+    return 0;
+}
+
