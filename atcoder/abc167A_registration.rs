116fa4b880813a70405b35e3c113178ac59f47bc
Vicfred <vicfred@vicfred.dev>
2020-05-15 00:39:10 -0500

    abc167A registration

---
+++ atcoder/abc167A_registration.rs
@@ -0,0 +1,27 @@
+// Vicfred
+// https://atcoder.jp/contests/abc167/tasks/abc167_a
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+
+    io::stdin()
+        .read_line(&mut s)
+        .unwrap();
+
+    let s = s.trim();
+
+    let mut t = String::new();
+
+    io::stdin()
+        .read_line(&mut t)
+        .unwrap();
+
+    let t = t.trim();
+
+    if &s[0..] == &t[0..t.len()-1] {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
