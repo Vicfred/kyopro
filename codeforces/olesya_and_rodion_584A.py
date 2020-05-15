37e193d1988c4aa1150434bf118e904d907ba696
Vicfred <vicfred@vicfred.dev>
2020-05-14 20:05:23 -0500

    olesya and rodion 584A

---
+++ codeforces/olesya_and_rodion_584A.py
@@ -0,0 +1,16 @@
+# Vicfred
+# https://codeforces.com/problemset/problem/584/A
+# implementation
+
+n, t = map(int, input().split())
+
+found = False
+for i in range(10**(n-1), 10**n):
+    if i%t == 0:
+        print(i)
+        found = True
+        break
+
+if not found:
+    print(-1)
+
