1ce0cd7863326af60952f0e8acc542f8009adbe6
Vicfred <vicfred@vicfred.dev>
2020-05-14 23:03:52 -0500

    WA adding powers 1312C

--- codeforces/adding_powers_1312C.cpp
+++ codeforces/adding_powers_1312C.cpp
@@ -5,30 +5,17 @@
 
 using namespace std;
 
-long long binpow(long long a, long long b) {
-    if (b == 0)
-        return 1;
-    long long res = binpow(a, b / 2);
-    if (b % 2)
-        return res * res * a;
-    else
-        return res * res;
-}
+vector<int> digits(int a, int k) {
+    vector<int> ans;
 
-vector<long long> digits(long long a, long long k) {
-    vector<long long> b(60);
-    b[0] = a%k;
-    long long maxlimit = 30LL;
-    if(k == 2LL)
-        maxlimit = 60LL;
-    for(long long pot = 1LL; pot < maxlimit; pot++) {
-        long long sum = 0LL;
-        for(long long i = 0LL; i < pot; i++) {
-            sum += b[i]*binpow(k,i);
-        }
-        b[pot] = ((a%binpow(k,pot+1LL))-sum)/(binpow(k,pot));
+    while(a >= k) {
+        ans.push_back(a%k);
+        a = a/k;
     }
-    return b;
+    
+    ans.push_back(a);
+
+    return ans;
 }
 
 int main() {
@@ -36,37 +23,38 @@ int main() {
     cin >> t;
 
     while(t--) {
-        long long n, k;
+        int n, k;
         cin >> n >> k;
 
-        vector<long long> a(n);
-        for(long long i = 0LL; i < n; ++i)
+        vector<int> a(n);
+        for(int i = 0; i < n; ++i)
             cin >> a[i];
 
         bool impossible = false;
-        for(auto item: a) {
-            for(auto digit: digits(item, k)) {
-                if(digit > 1LL) {
+        for(int item: a) {
+            for(int digit: digits(item, k)) {
+                if(digit > 1) {
                     impossible = true;
                 }
             }
         }
 
-        vector<int> used(60);
+        vector<int> used(66);
         for(int item: a) {
-            auto coefs = digits(item,k);
-            for(int i = 0; i < 60; i++) {
-                if(coefs[i] >= 1LL) {
+            vector<int> coefs = digits(item,k);
+            for(int i = 0; i < coefs.size(); i++) {
+                if(coefs[i] >= 1) {
                     used[i] += 1;
                 }
             }
         }
 
-        for(auto i: used) {
+        for(int i: used) {
             if(i > 1) {
                 impossible = true;
             }
         }
+
         if(impossible)
             cout << "NO" << endl;
         else
