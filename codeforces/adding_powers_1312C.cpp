fdb5f7f5031f8f3af5cd363a7e41161a2244cb17
Vicfred <vicfred@vicfred.dev>
2020-05-14 23:35:43 -0500

    adding powers 1312C

--- codeforces/adding_powers_1312C.cpp
+++ codeforces/adding_powers_1312C.cpp
@@ -1,64 +1,40 @@
-// Vicfred
+// unihernandez22 Vicfred
 // https://codeforces.com/problemset/problem/1312/C
+// greedy, math
+#include <algorithm>
+#include <iterator>
+#include <string>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector<int> digits(int a, int k) {
-    vector<int> ans;
-
-    while(a >= k) {
-        ans.push_back(a%k);
-        a = a/k;
-    }
-    
-    ans.push_back(a);
-
-    return ans;
-}
-
 int main() {
-    int t;
+    long long t;
     cin >> t;
 
     while(t--) {
-        int n, k;
+        long long n, k;
         cin >> n >> k;
 
-        vector<int> a(n);
-        for(int i = 0; i < n; ++i)
+        vector<long long> a(n);
+        for(long long i = 0; i < n; ++i)
             cin >> a[i];
 
-        bool impossible = false;
-        for(int item: a) {
-            for(int digit: digits(item, k)) {
-                if(digit > 1) {
-                    impossible = true;
-                }
+        string ans = "YES";
+        while(any_of(a.begin(), a.end(), [](long long x) { return x != 0; })) {
+            auto b = a;
+            transform(b.begin(), b.end(), b.begin(), [=](long long x) { return x%k; });
+            vector<long long> c;
+            copy_if(b.begin(), b.end(), back_inserter(c), [](long long x) { return x != 0; });
+            vector<long long> d = {1LL};
+            if(c > d) {
+                ans = "NO";
+                break;
             }
+            transform(a.begin(), a.end(), a.begin(), [=](long long x) { return x/k; });
         }
-
-        vector<int> used(66);
-        for(int item: a) {
-            vector<int> coefs = digits(item,k);
-            for(int i = 0; i < coefs.size(); i++) {
-                if(coefs[i] >= 1) {
-                    used[i] += 1;
-                }
-            }
-        }
-
-        for(int i: used) {
-            if(i > 1) {
-                impossible = true;
-            }
-        }
-
-        if(impossible)
-            cout << "NO" << endl;
-        else
-            cout << "YES" << endl;
+        cout << ans << endl;
     }
 }
 
