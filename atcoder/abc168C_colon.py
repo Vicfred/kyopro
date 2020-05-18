c43eb7eadf0d67bb0f35cb3be2158a7fa6df10d3
Vicfred <vicfred@vicfred.dev>
2020-05-17 22:26:26 -0500

    abc168C colon

---
+++ atcoder/abc168C_colon.py
@@ -0,0 +1,12 @@
+# unihernande22
+# https://atcoder.jp/contests/abc168/tasks/abc168_c
+# math
+ 
+from math import pi, cos
+ 
+a, b, h, m = map(int, input().split())
+x = pi/6
+y = pi/30
+z = x/60
+C = abs(x*h+z*m - y*m)
+print(f"{(a**2 + b**2 - 2 * a*b * cos(C))**2**-1:.20f}")
