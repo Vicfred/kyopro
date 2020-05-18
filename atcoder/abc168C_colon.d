368a9e782cdd43e11e40dee2e07948c7180538e8
Vicfred <vicfred@vicfred.dev>
2020-05-17 22:30:28 -0500

    abc168C colon

---
+++ atcoder/abc168C_colon.d
@@ -0,0 +1,21 @@
+// Vicfred
+// https://atcoder.jp/contests/abc168/tasks/abc168_c
+// math
+import std.stdio;
+import std.math;
+
+void main() {
+    real a, b, h, m;
+    readf("%s %s %s %s", &a, &b, &h, &m);
+
+    real x = PI/6;
+    real y = PI/30;
+    real z = x/60;
+
+    real rad = abs(x*h + z*m - y*m);
+
+    real c = sqrt(a*a+b*b-2.0*a*b*cos(rad));
+
+    writefln("%.23f", c);
+}
+
