969cc16662d35215e38717b82dc4834dcd814236
Vicfred <vicfred@vicfred.dev>
2020-05-17 20:10:16 -0500

    abc168B triple dots

---
+++ atcoder/abc168B_triple_dots.d
@@ -0,0 +1,19 @@
+// Vicfred
+// https://atcoder.jp/contests/abc168/tasks/abc168_b
+// implementation
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int K = readln.chomp.to!int;
+    string S = readln.strip;
+
+    if(S.length > K) {
+        S[0..K].write;
+        "...".writeln;
+    } else {
+        S.writeln;
+    }
+}
+
