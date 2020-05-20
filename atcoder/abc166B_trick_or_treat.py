c774715e3ea14740acfb9c9ef8caffc449b61d8c
Vicfred <vicfred@vicfred.dev>
2020-05-19 20:40:40 -0500

    abc166B trick or treat

---
+++ atcoder/abc166B_trick_or_treat.py
@@ -0,0 +1,14 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc166/tasks/abc166_b
+# implemtation
+n, k = map(int, input().split())
+ 
+snucks = [0]*n
+ 
+for _ in range(k):
+    input()
+    for i in map(int, input().split()):
+        snucks[i-1] = 1;
+ 
+print(snucks.count(0))
+
