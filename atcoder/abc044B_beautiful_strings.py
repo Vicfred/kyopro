e57bf14d31422b534462606c02c4bf937deec302
Vicfred <vicfred@vicfred.dev>
2020-05-20 01:35:56 -0500

    abc044B beautiful strings

---
+++ atcoder/abc044B_beautiful_strings.py
@@ -0,0 +1,22 @@
+# Vicfred
+# https://atcoder.jp/contests/abc044/tasks/abc044_b
+# implementation
+w = input()
+
+english = {}
+
+for ch in w:
+    if ch in english:
+        english[ch] += 1
+    else:
+        english[ch] = 1
+
+beautiful = True
+for value in english.values():
+    if value%2 == 1:
+        beautiful = False
+
+if beautiful:
+    print("YES")
+else:
+    print("NO")
