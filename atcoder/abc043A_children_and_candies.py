409f63790121db2c0bc48f59e9c040669c5ed98a
Vicfred <vicfred@vicfred.dev>
2020-05-20 01:09:41 -0500

    abc043A children and candies

---
+++ atcoder/abc043A_children_and_candies.py
@@ -0,0 +1,11 @@
+# Vicfred
+# https://atcoder.jp/contests/abc043/tasks/abc043_a
+# simulation
+n = int(input())
+
+candies = 0
+for child in range(1,n+1):
+    candies += child
+
+print(candies)
+
---
+++ atcoder/abc043A_children_and_candies_.py
@@ -0,0 +1,3 @@
+n = int(input())
+
+print(int((n*(n+1))/2))
