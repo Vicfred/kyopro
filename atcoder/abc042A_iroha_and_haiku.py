d47f1fccfa5673a496e94ccf46f4da5fb98854bf
Vicfred <vicfred@vicfred.dev>
2020-05-20 00:37:06 -0500

    abc042A iroha and haiku

---
+++ atcoder/abc042A_iroha_and_haiku.py
@@ -0,0 +1,11 @@
+# Vicfred
+# https://atcoder.jp/contests/abc042/tasks/abc042_a
+# implementation, sorting
+ins = list(map(int, input().split()))
+ins.sort()
+
+if ins == [5, 5, 7]:
+    print("YES")
+else:
+    print("NO")
+
