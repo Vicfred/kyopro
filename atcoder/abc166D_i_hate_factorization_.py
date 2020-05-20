84db6cb53fd91deb3b118a1ae651d29a3585eb01
Vicfred <vicfred@vicfred.dev>
2020-05-19 23:09:11 -0500

    abc166D i hate factorization (random version)

---
+++ atcoder/abc166D_i_hate_factorization_.py
@@ -0,0 +1,14 @@
+# unihernandez22 & Vicfred
+# https://atcoder.jp/contests/abc166/tasks/abc166_d
+# math, probability, random
+from random import randint
+
+n = int(input())
+
+for _ in range(10**6):
+    x = randint(-200,200)
+    y = randint(-200,200)
+    if x**5 - y**5 == n:
+        print(x,y)
+        break;
+
