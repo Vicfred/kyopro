7eaac9712ee3c515eb08dd987826bba72281fd44
Vicfred <vicfred@vicfred.dev>
2020-05-19 20:06:28 -0500

    abc166A a?c

---
+++ atcoder/abc166A_a_c.py
@@ -0,0 +1,12 @@
+# Vicfred
+# https://atcoder.jp/contests/abc166/tasks/abc166_a
+# implementation
+
+s = input()
+
+answer = {
+        'ABC': 'ARC',
+        'ARC': 'ABC'
+        }
+
+print(answer[s])
