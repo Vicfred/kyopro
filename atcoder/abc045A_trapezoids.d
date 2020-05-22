05e9f04d1847552a964c3214cfe77c1c16d4ccbb
Vicfred <vicfred@vicfred.dev>
2020-05-22 00:16:10 -0500

    abc045A trapezoids

---
+++ atcoder/abc045A_trapezoids.d
@@ -0,0 +1,12 @@
+// Vicfred
+// https://atcoder.jp/contests/abc045/tasks/abc045_a
+// math
+import std.stdio;
+
+void main() {
+    int a, b, h;
+    readf("%s\n%s\n%s\n", &a, &b, &h);
+
+    writefln("%s", (a+b)*h/2);
+}
+
