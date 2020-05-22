1cd5c9a53f4011fe53d2054c092d4e0a40c755ef
Vicfred <vicfred@vicfred.dev>
2020-05-22 01:08:24 -0500

    abc046A atcodeer and paint cans

---
+++ atcoder/abc046A_atcodeer_and_paint_cans.d
@@ -0,0 +1,15 @@
+// Vicfred
+// https://atcoder.jp/contests/abc046/tasks/abc046_a
+// simulation, data structures
+import std.algorithm;
+import std.array;
+import std.container;
+import std.conv;
+import std.stdio;
+
+void main() {
+    int[] cans = readln.split.map!(to!int).array;
+    auto rbt = redBlackTree(cans);
+    rbt.length.writeln;
+}
+
