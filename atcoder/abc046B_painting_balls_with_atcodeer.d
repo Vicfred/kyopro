d3d89f3df02ee6311635dcb9c88726e5400ff92a
Vicfred <vicfred@vicfred.dev>
2020-05-22 01:15:12 -0500

    abc046B painting balls with atcodeer

---
+++ atcoder/abc046B_painting_balls_with_atcodeer.d
@@ -0,0 +1,12 @@
+// Vicfred
+// https://atcoder.jp/contests/abc046/tasks/abc046_b
+// math
+import std.stdio;
+
+void main() {
+    long n, k;
+    readf("%s %s\n", &n, &k);
+
+    writefln("%s", k*(k-1)^^(n-1));
+}
+
