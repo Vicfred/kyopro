3fe43cc004ee2c65c0d7c20ac882634199891dd6
Vicfred <vicfred@vicfred.dev>
2020-05-24 22:51:45 -0500

    abc163A circle pond

---
+++ atcoder/abc163A_circle_pond.d
@@ -0,0 +1,13 @@
+// Vicfred
+// https://atcoder.jp/contests/abc163/tasks/abc163_a
+// math
+import std.math;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int r = readln.chomp.to!int;
+    writefln("%0.12f", 2.0*PI*r);
+}
+
