7893eece000e8cfd0ab53094deac50f99ff5b285
Vicfred <vicfred@vicfred.dev>
2020-05-24 22:15:17 -0500

    abc164D multiple of 2019

---
+++ atcoder/abc164D_multiple_of_2019.cpp
@@ -0,0 +1,42 @@
+// Vicfred
+// https://atcoder.jp/contests/abc164/tasks/abc164_d
+// implementation
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    const int mod = 2019;
+
+    vector<int> dp(mod);
+
+    dp[0] += 1;
+
+    int rem = 0;
+    int base = 1;
+
+    for(int i = 0; i < n; i++) {
+        int d = s[n-i-1]-'0';
+        rem = (rem + base * d)%mod;
+        dp[rem] += 1;
+        base *= 10;
+        base %= mod;
+    }
+
+    long ans = 0LL;
+    for(int i = 0; i < mod; i++) {
+        ans += (dp[i]*(dp[i]-1))/2;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
+
