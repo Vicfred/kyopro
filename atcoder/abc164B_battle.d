55e816f5bb1b74744b597b8eba7c841885be535a
Vicfred <vicfred@vicfred.dev>
2020-05-24 20:17:29 -0500

    abc164B battle

---
+++ atcoder/abc164B_battle.d
@@ -0,0 +1,26 @@
+// Vicfred
+// https://atcoder.jp/contests/abc164/tasks/abc164_b
+// simulation
+import std.stdio;
+
+void main() {
+    int a, b, c, d;
+    readf("%s %s %s %s\n", &a, &b, &c, &d);
+
+    int step = 1;
+    while(a > 0 && c > 0) {
+        if(step%2 == 1)
+            c -= b;
+        else
+            a -= d;
+        step += 1;
+    }
+
+    if(step%2 == 1) {
+        "No".writeln;
+    }
+    else {
+        "Yes".writeln;
+    }
+}
+
