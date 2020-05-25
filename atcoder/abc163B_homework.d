425ac667f76065b841f1a29dd07451bad9ee3c1b
Vicfred <vicfred@vicfred.dev>
2020-05-24 23:15:26 -0500

    abc163B homework

---
+++ atcoder/abc163B_homework.d
@@ -0,0 +1,20 @@
+// Vicfred
+// https://atcoder.jp/contests/abc163/tasks/abc163_b
+// simulation
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+
+void main() {
+    long n, m;
+    readf("%s %s\n", &n, &m);
+
+    long[] a = readln.split.map!(to!long).array;
+
+    if(n >= sum(a))
+        writefln("%s", n-sum(a));
+    else
+        "-1".writefln;
+}
+
