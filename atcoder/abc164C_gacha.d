f387ba94540e9c857c39035df7558ce625073815
Vicfred <vicfred@vicfred.dev>
2020-05-24 20:39:22 -0500

    abc164C gacha

---
+++ atcoder/abc164C_gacha.d
@@ -0,0 +1,21 @@
+// Vicfred
+// https://atcoder.jp/contests/abc164/tasks/abc164_c
+// set
+import std.conv;
+import std.stdio;
+import std.container.rbtree;
+import std.string;
+
+void main() {
+    int n = readln.chomp.to!int;
+
+    auto rbt = redBlackTree!string;
+
+    while(n--) {
+        string s = readln.chomp;
+        rbt.insert(s);
+    }
+
+    rbt.length.writeln;
+}
+
