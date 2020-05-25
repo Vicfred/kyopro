30cec9a60c2ee5d18b01d0420faca97fb4594d05
Vicfred <vicfred@vicfred.dev>
2020-05-24 20:33:11 -0500

    abc164C gacha

---
+++ atcoder/abc164C_gacha.rs
@@ -0,0 +1,29 @@
+// Vicfred
+// https://atcoder.jp/contests/abc164/tasks/abc164_c
+// set
+use std::io;
+use std::collections::HashSet;
+
+fn main() {
+    let mut n = String::new();
+
+    io::stdin()
+        .read_line(&mut n)
+        .unwrap();
+
+    let n: i64 = n.trim().parse().unwrap();
+
+    let mut kinds = HashSet::new();
+    for _ in 0..n {
+        let mut s = String::new();
+
+        io::stdin()
+            .read_line(&mut s)
+            .unwrap();
+
+        kinds.insert(s);
+    }
+    
+    println!("{}", kinds.len());
+}
+
