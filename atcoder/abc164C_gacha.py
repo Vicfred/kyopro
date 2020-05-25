b932ef2e2ad93c3f5cda6d5041c933a472053cb0
Vicfred <vicfred@vicfred.dev>
2020-05-24 20:33:53 -0500

    abc164C gacha

---
+++ atcoder/abc164C_gacha.py
@@ -0,0 +1,14 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc164/tasks/abc164_c
+# dictionary
+a = {}
+ans = 0
+
+n = int(input())
+for _ in range(n):
+    i = input()
+    ans += a.get(i, 1)
+    a[i] = 0
+
+print(ans)
+
