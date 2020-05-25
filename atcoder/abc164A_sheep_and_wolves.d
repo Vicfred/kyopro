2534a14f4c5c6195ff1c681e5cb318f4863bf451
Vicfred <vicfred@vicfred.dev>
2020-05-24 20:16:23 -0500

    abc164A sheep and wolves

---
+++ atcoder/abc164A_sheep_and_wolves.d
@@ -0,0 +1,14 @@
+// Vicfred
+// https://atcoder.jp/contests/abc164/tasks/abc164_a
+// simulation
+import std.stdio;
+
+void main() {
+    int s, w;
+    readf("%s %s\n", &s, &w);
+    if(w >= s)
+        writeln("unsafe");
+    else
+        writeln("safe");
+}
+
