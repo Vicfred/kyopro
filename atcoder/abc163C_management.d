5c59e932beb7cda40c4bcd773d75ced6587ef47d
Vicfred <vicfred@vicfred.dev>
2020-05-24 23:15:43 -0500

    abc163C management

---
+++ atcoder/abc163C_management.d
@@ -0,0 +1,22 @@
+// Vicfred
+// https://atcoder.jp/contests/abc163/tasks/abc163_c
+// map
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int n = readln.chomp.to!int;
+    int[] a = readln.split.map!(to!int).array;
+
+    int[] boss = new int[n+1];
+
+    foreach(person; a)
+        boss[person] += 1;
+
+    for(int i = 1; i <= n; i++)
+        boss[i].writeln;
+}
+
