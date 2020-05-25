cc0f008d3f64537be8010a44538fde8111e6049c
Vicfred <vicfred@vicfred.dev>
2020-05-24 20:20:59 -0500

    abc164B battle

---
+++ atcoder/abc164B_battle.py
@@ -0,0 +1,26 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc164/tasks/abc164_b
+# simulation
+class Monster:
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+a = list(map(int, input().split()))
+
+t_monster = Monster(*a[:2])
+a_monster = Monster(*a[2:])
+
+i = 0
+while t_monster.health > 0 and a_monster.health > 0:
+    if i % 2 == 0:
+        a_monster.health -= t_monster.strength
+    else:
+        t_monster.health -= a_monster.strength
+    i += 1
+
+if t_monster.health > 0:
+    print("Yes")
+else:
+    print("No")
+
