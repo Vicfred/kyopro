9714e3c0bc1f3772ac5a0f6f543416e6965de9ac
Vicfred <vicfred@vicfred.dev>
2020-05-26 20:04:30 -0500

    abc162B fizzbuzz sum

---
+++ atcoder/abc162B_fizzbuzz_sum.py
@@ -0,0 +1,9 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc162/tasks/abc162_b
+# simulation
+ans = 0
+for i in range(int(input())+1):
+    if not (i%3 == 0 or i%5 == 0):
+        ans += i
+print(ans)
+
