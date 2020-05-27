999566b0e474ee76682b0d52b2e2abfbbd48840e
Vicfred <vicfred@vicfred.dev>
2020-05-26 22:55:16 -0500

    abc162F select half

---
+++ atcoder/abc162F_select_half.d
@@ -0,0 +1,34 @@
+// Vicfred
+// https://atcoder.jp/contests/abc162/tasks/abc162_f
+// dynamic programming, cumulative sum
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    long n = readln.chomp.to!long;
+    long[] b = readln.split.map!(to!long).array;
+    long[] a;
+    a ~= 0;
+    a ~= b;
+
+    long[] cumulative = new long[n+1];
+    long[] dp = new long[n+1];
+    cumulative[1] = a[1];
+
+    for(long i = 3; i <= n; i++)
+        cumulative[i] = cumulative[i-2]+a[i];
+
+    for(long i = 2; i <= n; i++) {
+        if(i%2 == 1)
+            dp[i] = max(dp[i-1], dp[i-2]+a[i]);
+        else {
+            dp[i] = max(cumulative[i-1], dp[i-2]+a[i]);
+        }
+    }
+
+    dp[n].writeln;
+}
+
