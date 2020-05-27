25c8a6a1b8eab533d408dbbc1d543477eda13288
Vicfred <vicfred@vicfred.dev>
2020-05-26 19:52:15 -0500

    abc162A lucky 7

---
+++ atcoder/abc162A_lucky_7.d
@@ -0,0 +1,17 @@
+// Vicfred
+// https://atcoder.jp/contests/abc162/tasks/abc162_a
+// implementation
+import std.stdio;
+
+void main() {
+    string n = readln;
+
+    foreach(ch; n) {
+        if(ch == '7') {
+            "Yes".writeln;
+            return;
+        }
+    }
+    "No".writeln;
+}
+
