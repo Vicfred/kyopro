93ad600b02c3d0d5d6bbf3061a9575091f8e66f9
Vicfred <vicfred@vicfred.dev>
2020-05-26 20:04:18 -0500

    abc162B fizzbuzz sum

---
+++ atcoder/abc162B_fizzbuzz_sum.d
@@ -0,0 +1,18 @@
+// Vicfred
+// https://atcoder.jp/contests/abc162/tasks/abc162_b
+// simulation
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int n = readln.chomp.to!int;
+
+    long sum = 0;
+    foreach(i; 1..n+1) {
+        if(i%3 != 0 && i%5 != 0)
+            sum += i;
+    }
+    sum.writeln;
+}
+
