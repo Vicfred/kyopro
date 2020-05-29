6e20cd2add521e25cfcabe6dea2ce76149722100
Vicfred <vicfred@vicfred.dev>
2020-05-28 20:18:09 -0500

    abc160A coffee

---
+++ atcoder/abc160A_coffee.rs
@@ -0,0 +1,24 @@
+// Vicfred
+// https://atcoder.jp/contests/abc160/tasks/abc160_a
+// implementation
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+
+    io::stdin()
+        .read_line(&mut s)
+        .unwrap();
+
+    let s = s.trim();
+
+    let s: Vec<char> = s.chars().collect();
+
+    if &s[2] == &s[3] && &s[4] == &s[5] {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+
+}
+
