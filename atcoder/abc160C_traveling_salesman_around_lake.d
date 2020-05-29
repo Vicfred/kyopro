6713762c32ca94ba543c4c33a63eb0238532d5ed
Vicfred <vicfred@vicfred.dev>
2020-05-28 21:02:40 -0500

    abc160C traveling salesman around lake

---
+++ atcoder/abc160C_traveling_salesman_around_lake.d
@@ -0,0 +1,25 @@
+// Vicfred
+// https://atcoder.jp/contests/abc160/tasks/abc160_c
+// greedy
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+
+void main() {
+    int k, n;
+    readf("%s %s", &k, &n);
+    readln;
+
+    int[] a = readln.split.map!(to!int).array;
+
+    int[] dist;
+
+    for(long i = a.length-1; i > 0; i--)
+        dist ~= a[i]-a[i-1];
+    dist ~= k-a[$-1]+a[0];
+
+    sort(dist);
+    sum(dist[0..$-1]).writeln;
+}
+
