41900f010946ca35001281be61fe0e29c0d5fb92
Vicfred <vicfred@vicfred.dev>
2020-05-31 21:49:00 -0500

    abc159D banned k

---
+++ atcoder/abc159D_banned_k.py
@@ -0,0 +1,19 @@
+# Vicfred & unihernandez22
+# https://atcoder.jp/users/unihernandez22
+# math
+n = int(input())
+a = list(map(int, input().split()))
+
+c = {}
+
+for i in a:
+    c[i] = c.get(i, 0) + 1
+
+ways = 0
+for i in c:
+    ways += c[i]*(c[i]-1)//2
+
+for k in a:
+    ans = ways-c[k]+1
+    print(ans)
+
