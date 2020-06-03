344ebf62e062e694554c0f8f49ce08557599685c
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-03 00:42:33 -0300

    File rename

---
+++ codeforces/1312C_adding_powers.rs
@@ -0,0 +1,43 @@
+// unihernandez22
+// https://codeforces.com/contest/1312/problem/C
+// greedy, math
+
+use std::io::stdin;
+
+fn main() {
+    let mut t = String::new();
+    stdin().read_line(&mut t).unwrap();
+    let t = t.trim().parse().unwrap();
+
+    for _ in 0..t {
+        let mut line = String::new();
+        stdin().read_line(&mut line).unwrap();
+
+        let mut words: Vec<i64> = line
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+        
+        let n = words[0];
+        let k = words[1];
+        
+        line = String::new();
+        stdin().read_line(&mut line).unwrap();
+
+        let mut a: Vec<i64> = line
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        let mut ans = "YES";
+        while a.iter().any(|&x| x != 0) {
+            let b = a.iter().map(|x| x%k).filter(|&x| x != 0).collect::<Vec<i64>>();
+            if b > vec![1] {
+                ans = "NO";
+                break;
+            }
+            a = a.iter().map(|x| x/k as i64).collect();
+        }
+        println!("{}", ans);
+    }
+}
