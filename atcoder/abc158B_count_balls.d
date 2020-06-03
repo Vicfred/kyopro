3bdd04b95256b0929ca5f668a01ee734dcdb0720
Vicfred <vicfred@vicfred.dev>
2020-06-02 21:37:11 -0500

    abc158B count balls

---
+++ atcoder/abc158B_count_balls.d
@@ -0,0 +1,14 @@
+// Vicfred
+// https://atcoder.jp/contests/abc158/tasks/abc158_b
+// math
+import std.algorithm;
+import std.stdio;
+
+void main() {
+    long n, a, b;
+    readf("%s %s %s", &n, &a, &b);
+
+    long ans = n/(a+b)*a;
+    ans += min(a, n%(a+b));
+    ans.writeln;
+}
