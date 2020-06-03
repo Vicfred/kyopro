cc35570130807808517e36c598621aed48cfe013
Vicfred <vicfred@vicfred.dev>
2020-06-02 21:35:24 -0500

    abc158A station and bus

---
+++ atcoder/abc158A_station_and_bus.d
@@ -0,0 +1,14 @@
+// Vicfred
+// https://atcoder.jp/contests/abc158/tasks/abc158_a
+// implementation
+import std.stdio;
+import std.string;
+
+void main() {
+    string s = readln.chomp;
+    if(s.count('A') > 0 && s.count('B') > 0)
+        "Yes".writeln;
+    else
+        "No".writeln;
+}
+
