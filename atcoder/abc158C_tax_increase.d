5831d4e73dcdd600e3275302618966ce75ce796c
Vicfred <vicfred@vicfred.dev>
2020-06-02 21:53:22 -0500

    abc158C tax increase

---
+++ atcoder/abc158C_tax_increase.d
@@ -0,0 +1,17 @@
+// Vicfred
+// https://atcoder.jp/contests/abc158/tasks/abc158_c
+// implementation
+import std.stdio;
+
+void main() {
+    int a, b;
+    readf("%s %s", &a, &b);
+    for(int i = 1; i < 1010; i++) {
+        if(i/10 == b && a == (i*8)/100) {
+            i.writeln;
+            return;
+        }
+    }
+    (-1).writeln;
+}
+
