5fcd015d54a7c6f227893ebdd5292c3f5f48861e
Vicfred <vicfred@vicfred.dev>
2020-06-03 00:05:00 -0500

    abc157A duplex printing

---
+++ atcoder/abc157A_duplex_printing.rs
@@ -0,0 +1,17 @@
+// Vicfred
+// https://atcoder.jp/contests/abc157/tasks/abc157_a
+// implementation
+use std::io;
+
+fn main() {
+    let mut n = String::new();
+
+    io::stdin()
+        .read_line(&mut n)
+        .unwrap();
+
+    let n: i64 = n.trim().parse().unwrap();
+    
+    println!("{}", (n+1)/2);
+}
+
