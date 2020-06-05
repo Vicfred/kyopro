ab71428461a352f9984c454685641d9c75743f8c
Vicfred <vicfred@vicfred.dev>
2020-06-05 01:28:52 -0500

    abc049A uoiauai

---
+++ atcoder/abc049A_uoiauai.py
@@ -0,0 +1,11 @@
+# Vicfred
+# https://atcoder.jp/contests/abc049/tasks/abc049_a
+# simulation
+c = input()
+
+vowels = ['a', 'i', 'u', 'e', 'o']
+
+if c in vowels:
+    print("vowel")
+else:
+    print("consonant")
