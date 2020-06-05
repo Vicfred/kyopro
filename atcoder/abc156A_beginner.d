6f3ed30bd56a94abd5cbfa778f5fd12c0aadafe4
Vicfred <vicfred@vicfred.dev>
2020-06-04 20:20:07 -0500

    abc156A beginner

---
+++ atcoder/abc156A_beginner.d
@@ -0,0 +1,15 @@
+// Vicfred
+// https://atcoder.jp/contests/abc156/tasks/abc156_a
+// math
+import std.stdio;
+
+void main() {
+    int n, r;
+    readf("%s %s\n", &n, &r);
+
+    if(n >= 10)
+        r.writeln;
+    else
+        writefln("%s", r+100*(10-n));
+}
+
