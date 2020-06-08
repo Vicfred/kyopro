d1e287e61e9a9fe10a1fac56fa0106738c60dbf2
Vicfred <vicfred@vicfred.dev>
2020-06-08 01:50:25 -0500

    abc051B sum of three integers

---
+++ atcoder/abc051B_sum_of_three_integers.d
@@ -0,0 +1,21 @@
+// Vicfred
+// https://atcoder.jp/contests/abc051/tasks/abc051_b
+// simulation
+import std.stdio;
+
+void main() {
+    int k, s;
+    readf("%s %s\n", &k, &s);
+    
+    int answer = 0;
+    for(int x = 0; x <= k; ++x) {
+        for(int y = 0; y <= k; ++y) {
+            int z = s - x - y;
+            if(z >= 0 && z <= k)
+                answer += 1;
+        }
+    }
+
+    answer.writeln;
+}
+
