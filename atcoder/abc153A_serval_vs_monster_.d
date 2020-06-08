894c5ee38b25664ea3c95d1c2d17b9916a4186df
Vicfred <vicfred@vicfred.dev>
2020-06-07 22:22:24 -0500

    abc153A serval vs monster

---
+++ atcoder/abc153A_serval_vs_monster_.d
@@ -0,0 +1,12 @@
+// Vicfred
+// https://atcoder.jp/contests/abc153/tasks/abc153_a
+// math
+import std.stdio;
+
+void main() {
+    int h, a;
+    readf("%s %s", &h, &a);
+
+    writefln("%s", (h+a-1)/a);
+}
+
