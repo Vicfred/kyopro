a1eae78d918e9d4af2a0c4f102b2aedb6032cf64
Vicfred <vicfred@vicfred.dev>
2020-06-07 22:16:55 -0500

    abc154C distinct or not

---
+++ atcoder/abc154C_distinct_or_not_.py
@@ -0,0 +1,19 @@
+# Vicfred
+# https://atcoder.jp/contests/abc154/tasks/abc154_c
+# sorting
+n = int(input())
+a = list(map(int, input().split()))
+
+a.sort()
+
+repeated = False
+for i in range(len(a)-1):
+    if a[i] == a[i+1]:
+        repeated = True
+        break
+
+if repeated:
+    print("NO")
+else:
+    print("YES")
+
