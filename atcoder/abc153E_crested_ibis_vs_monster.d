3e79d3921a9ecbb913c33b401b47ae967bf62b98
Vicfred <vicfred@vicfred.dev>
2020-06-08 00:56:47 -0500

    abc153E crested ibis vs monster

---
+++ atcoder/abc153E_crested_ibis_vs_monster.d
@@ -0,0 +1,40 @@
+// Vicfred
+// https://atcoder.jp/contests/abc153/tasks/abc153_e
+// dynamic programming
+import std.algorithm;
+import std.stdio;
+
+void main() {
+    long h, n;
+    readf("%s %s\n", &h, &n);
+
+    long[] a = new long[n];
+    long[] b = new long[n];
+
+    foreach(i; 0..n) {
+        readf("%s %s\n", &a[i], &b[i]);
+    }
+
+    const long MAXN = 10^^5;
+    const long INF = 1<<30;
+
+    long[] dp = new long[MAXN];
+    for(long i = 0; i < MAXN; ++i)
+        dp[i] = INF;
+
+    dp[0] = 0;
+
+    for(long i = 0; i < n; ++i) {
+        for(long hp = 0; hp < h; ++hp) {
+            if(dp[hp] < INF)
+                dp[hp+a[i]] = min(dp[hp]+b[i], dp[hp+a[i]]);
+        }
+    }
+
+    long minima = INF;
+    for(long i = h; i < MAXN; ++i)
+        minima = min(minima, dp[i]);
+
+    minima.writeln;
+}
+
