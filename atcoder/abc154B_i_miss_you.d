3dec5272881960fefc4577725bd4fc9a591dde30
Vicfred <vicfred@vicfred.dev>
2020-06-07 21:30:07 -0500

    abc154B i miss you

---
+++ atcoder/abc154B_i_miss_you.d
@@ -0,0 +1,14 @@
+// Vicfred
+// https://atcoder.jp/contests/abc154/tasks/abc154_b
+// simulation
+import std.stdio;
+import std.string;
+
+void main() {
+    string S = readln.chomp;
+
+    foreach(ch; S) {
+        'x'.write;
+    } writeln;
+}
+
