d883773d85d5775b714a0116406daa01fc7c0bd5
Vicfred <vicfred@vicfred.dev>
2020-06-07 23:04:32 -0500

    abc153C fennec vs monster

---
+++ atcoder/abc153C_fennec_vs_monster.d
@@ -0,0 +1,23 @@
+// Vicfred
+// https://atcoder.jp/contests/abc153/tasks/abc153_c
+// greedy
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    long n, k;
+    readf("%s %s", &n, &k);
+    readln;
+
+    long[] h = readln.split.map!(to!long).array;
+    h.sort!("a > b");
+
+    if(k >= n)
+        0.writeln;
+    else
+        sum(h[k..$]).writeln;
+}
+
