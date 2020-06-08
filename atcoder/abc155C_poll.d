4424dfcad6e885f9cdc16e56bd0359172b98ab1d
Vicfred <vicfred@vicfred.dev>
2020-06-07 20:05:52 -0500

    abc155C poll

---
+++ atcoder/abc155C_poll.d
@@ -0,0 +1,34 @@
+// Vicfred
+// https://atcoder.jp/contests/abc155/tasks/abc155_c
+import std.algorithm;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int n = readln.chomp.to!int;
+
+    int[string] votes;
+
+    foreach(_; 0..n) {
+        string s = readln.chomp;
+        votes[s] = votes.get(s,0)+1;
+    }
+
+    int maxima = 0;
+    foreach(item; votes.values()) {
+        maxima = max(maxima, item);
+    }
+
+    string[] ans;
+    foreach(item; votes.keys()) {
+        if(votes[item] == maxima)
+            ans ~= item;
+    }
+
+    ans.sort;
+
+    foreach(item; ans)
+        item.writeln;
+}
+
