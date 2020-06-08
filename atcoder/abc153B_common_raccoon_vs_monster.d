90628ecd51d0af566eb79134740bceb1df58d80c
Vicfred <vicfred@vicfred.dev>
2020-06-07 23:04:16 -0500

    abc153B common raccoon vs monster

---
+++ atcoder/abc153B_common_raccoon_vs_monster.d
@@ -0,0 +1,25 @@
+// Vicfred
+// https://atcoder.jp/contests/abc153/tasks/abc153_b
+// greedy
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int n, h;
+    readf("%s %s", &h, &n);
+    readln;
+
+    int[] a = readln.split.map!(to!int).array;
+
+    int power = sum(a);
+
+    if(power >= h)
+        "Yes".writeln;
+    else
+        "No".writeln;
+
+}
+
