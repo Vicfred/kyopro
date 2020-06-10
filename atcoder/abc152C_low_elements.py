7c9a08dc0087baf37ca8b523e49bb587f7617693
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-09 23:25:21 -0300

    152C Low Elements

---
+++ atcoder/abc152C_low_elements.py
@@ -0,0 +1,15 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc152/tasks/abc152_c
+# greedy
+
+input()
+first, *p = map(int, input().split())
+
+last = first
+ans = 1
+for i in p:
+    if i < last:
+        ans += 1
+        last = i
+
+print(ans)
