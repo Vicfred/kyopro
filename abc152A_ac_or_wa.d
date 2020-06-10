cb8d25f4d384f5aa6c24e7ed60e6273dd44c9085
Vicfred <vicfred@vicfred.dev>
2020-06-09 20:53:46 -0500

    abc152A ac or wa

---
+++ abc152A_ac_or_wa.d
@@ -0,0 +1,16 @@
+// Vicfred
+// https://atcoder.jp/contests/abc152/tasks/abc152_a
+// simulation
+import std.stdio;
+
+void main() {
+    int n, m;
+    readf("%s %s\n", &n, &m);
+
+    if(n == m) {
+        writefln("%s", "Yes");
+    } else {
+        writefln("%s", "No");
+    }
+}
+
