fd2c8119f129b8bd021e1c980712a856bbe383d9
Vicfred <vicfred@vicfred.dev>
2020-06-09 23:48:19 -0500

    cf580C kefa and park

---
+++ codeforces/cf580C_kefa_and_park.d
@@ -0,0 +1,42 @@
+// Vicfred
+// https://codeforces.com/problemset/problem/580/C
+// dfs
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+int n, m, answer;
+int[][] tree = new int[][](10^^5+5);
+int[] a;
+
+void dfs(int x, int parent, int cats, bool flag) {
+    if(a[x] == 1)
+        cats += 1;
+    if(cats > m)
+        flag = true;
+    if(tree[x].length == 1 && x != 1 && !flag)
+        answer += 1;
+    foreach(item; tree[x])
+        if(item != parent)
+            dfs(item, x, cats*a[x], flag);
+}
+
+void main() {
+    readf("%s %s", &n, &m);
+    readln;
+    a = 0~readln.split.map!(to!int).array;
+
+    foreach(i; 1..n) {
+        int x, y;
+        readf("%s %s", &x, &y);
+        readln;
+        tree[x] ~= y;
+        tree[y] ~= x;
+    }
+
+    dfs(1, 0, 0, 0);
+    answer.writeln;
+}
+
