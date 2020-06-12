95004922874002d9e147fe7f97580cc6098df45d
Vicfred <vicfred@vicfred.dev>
2020-06-11 19:55:24 -0500

    abc052A two rectangles

---
+++ atcoder/abc052A_two_rectangles.d
@@ -0,0 +1,17 @@
+// Vicfred
+// https://atcoder.jp/contests/abc052/tasks/abc052_a
+// math
+import std.stdio;
+
+void main() {
+    long a, b, c, d;
+
+    readf("%s %s %s %s", &a, &b, &c, &d);
+
+    if(a*b > c*d) {
+        writefln("%s", a*b);
+    } else {
+        writefln("%s", c*d);
+    }
+}
+
