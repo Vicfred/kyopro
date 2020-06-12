49c005d45c7cc086f2ea6215ae7cc305c086bb61
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-12 13:10:15 -0300

    58A

---
+++ atcoder/abc058A.nim
@@ -0,0 +1,13 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc058/tasks/abc058_a
+# implementation
+
+import strutils, sequtils
+
+var a, b, c: int
+(a, b, c) = stdin.readLine.split.map parseInt
+
+if b - a == c - b:
+  echo "YES"
+else:
+  echo "NO"
