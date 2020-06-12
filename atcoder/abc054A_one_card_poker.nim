6b5b54c990469da64c730c35a3f0b2fc7735fb84
Vicfred <vicfred@vicfred.dev>
2020-06-11 23:57:07 -0500

    abc054A one card poker

---
+++ atcoder/abc054A_one_card_poker.nim
@@ -0,0 +1,22 @@
+# Vicfred
+# https://atcoder.jp/contests/abc054/tasks/abc054_a
+# implementation
+import strutils, sequtils
+
+let line = stdin.readLine.split.map(parseInt)
+let (a, b) = (line[0], line[1])
+
+if a == 1 or b == 1:
+  if a == b:
+    echo "Draw"
+  elif a == 1:
+    echo "Alice"
+  else:
+    echo "Bob"
+elif a > b:
+  echo "Alice"
+elif a < b:
+  echo "Bob"
+else:
+  echo "Draw"
+
