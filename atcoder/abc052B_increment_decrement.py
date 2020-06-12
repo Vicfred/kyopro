f0a9b44781632630d0e9e28b0e7035f89e72f775
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-11 22:24:40 -0300

    52B Increment Decrement

---
+++ atcoder/abc052B_increment_decrement.py
@@ -0,0 +1,17 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc052/tasks/abc052_b
+# implementation
+
+input()
+s = input()
+
+ans = 0
+x = 0
+for i in s:
+    if i == 'I':
+        x += 1
+    elif i == 'D':
+        x -= 1
+    ans = max(ans, x)
+
+print(ans)
