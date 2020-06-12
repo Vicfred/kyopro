234e675307d2d648b02a18adbe566ff2a75d6a0b
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-12 13:38:38 -0300

    58B

---
+++ atcoder/abc058B.nim
@@ -0,0 +1,20 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc058/tasks/abc058_b
+# implementation
+
+import strutils, sequtils
+
+let O = stdin.readLine
+let E = stdin.readLine
+
+var ans = ""
+
+for i in 0 ..< min(O.len, E.len):
+  ans &= O[i] & E[i]
+
+if O.len > E.len:
+  ans &= $O[O.len-1]
+elif E.len > O.len:
+  ans &= $E[E.len-1]
+
+ans.echo
