b34163d46218849c0e23b2b07b738b58f125ceb8
Vicfred <vicfred@vicfred.dev>
2020-06-12 00:30:59 -0500

    abc054B template matching

---
+++ atcoder/abc054B_template_matching.nim
@@ -0,0 +1,20 @@
+# Vicfred
+# https://atcoder.jp/contests/abc054/tasks/abc054_b
+# implementation
+import strutils, sequtils
+
+var n, m: int; (n, m) = stdin.readLine.split.map parseInt
+let A = (1..n).mapIt(stdin.readLine)
+let B = (1..m).mapIt(stdin.readLine)
+
+for i in 0..n-m:
+  for j in 0..n-m:
+    var valid = true
+    for y in 0..<m:
+      if A[i+y][j..j+m-1] != B[y]:
+        valid = false
+    if valid:
+      echo "Yes"
+      quit()
+echo "No"
+
