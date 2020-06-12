3665d90c58b47c0149da612daf296cb8b0827a86
Vicfred <vicfred@vicfred.dev>
2020-06-11 23:27:13 -0500

    abc053A ABC/ARC

---
+++ atcoder/abc053A_abc_arc.nim
@@ -0,0 +1,10 @@
+# Vicfred
+# https://atcoder.jp/contests/abc053/tasks/abc053_a
+# implementation
+import strutils
+let rating = stdin.readLine.parseInt
+
+if rating < 1200:
+  echo "ABC"
+else:
+  echo "ARC"
