e666b1d6894bcddb4b41aca3f4b650d2af62579f
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-11 22:15:53 -0300

    155C Poll

---
+++ atcoder/abc155C_poll.py
@@ -0,0 +1,28 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc155/tasks/abc155_c
+
+class Counter(dict):
+  def __missing__(self, i):
+    return 0
+
+n = int(input())
+
+s = Counter()
+
+for _ in range(n):
+  i = input()
+  s[i] += 1
+
+s = list(sorted(s.items(), key=lambda x: -x[1]))
+m = s[0][1]
+
+ans = []
+for key, value in s:
+  if value == m:
+    ans.append(key)
+  else:
+    break
+
+ans.sort()
+
+print('\n'.join(ans))
