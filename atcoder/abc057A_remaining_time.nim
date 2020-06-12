8d5c748ceab3eb567db9e92b48e5533fffc13f0f
Vicfred <vicfred@vicfred.dev>
2020-06-12 01:59:53 -0500

    abc057A remaining time

---
+++ atcoder/abc057A_remaining_time.nim
@@ -0,0 +1,9 @@
+# Vicfred
+# https://atcoder.jp/contests/abc057/tasks/abc057_a
+# implementation
+import strutils, sequtils
+
+var a, b: int
+(a, b) = stdin.readLine.split.map(parseInt)
+echo `mod`(a+b, 24)
+
