6245b77419d5e8df92948bb5a1d479f880c0bc74
Vicfred <vicfred@vicfred.dev>
2020-06-11 23:16:47 -0500

    abc166B trick or treat

---
+++ atcoder/abc166B_trick_or_treat.nim
@@ -0,0 +1,18 @@
+# Vicfred
+# https://atcoder.jp/contests/abc166/tasks/abc166_b
+# implementation
+import strutils, sequtils
+
+let line = stdin.readLine.split.map(parseInt)
+let n = line[0]
+let k = line[1]
+
+var snukes = newSeq[int](n);
+
+for i in 0..<k:
+  let d = stdin.readLine.parseInt
+  let a = stdin.readLine.split.map(parseInt)
+  for j in a:
+    snukes[j-1] += 1
+snukes.count(0).echo
+
