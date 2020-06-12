4e64b1de1da266efa3f7ccd94ec95bf352d65a3d
Vicfred <vicfred@vicfred.dev>
2020-06-12 01:24:40 -0500

    abc056B NarrowRectangleEasy

---
+++ atcoder/abc056B_narrowrectangleseasy.nim
@@ -0,0 +1,13 @@
+# Vicfred
+# https://atcoder.jp/contests/abc056/tasks/abc056_b
+# implementation
+import strutils, sequtils
+
+var w, a, b: int
+(w, a, b) = stdin.readLine.split.map(parseInt)
+
+if abs(a-b) <= w:
+  echo 0
+else:
+  echo abs(a-b)-w
+
