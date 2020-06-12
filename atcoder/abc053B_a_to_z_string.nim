6323991dde5ce7b49c7d83543862eabb9fa7e8fd
Vicfred <vicfred@vicfred.dev>
2020-06-11 23:43:36 -0500

    abc053B A to Z string

---
+++ atcoder/abc053B_a_to_z_string.nim
@@ -0,0 +1,15 @@
+# Vicfred
+# https://atcoder.jp/contests/abc053/tasks/abc053_b
+# string manipulation
+import algorithm
+import strutils
+
+var s = stdin.readLine.strip
+let ini = s.find('A')
+
+s.reverse
+let fin = s.find('Z')
+
+let ans = s.len - ini - fin
+ans.echo
+
