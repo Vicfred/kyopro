ed871737eb9a707c7d19c092051929b12ec0e6d7
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-13 15:50:20 -0300

    47B Snuke's Coloring 2

---
+++ atcoder/abc047B_snukes_coloring_2.nim
@@ -0,0 +1,24 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc047/tasks/abc047_b
+# simulation
+
+import strutils, sequtils
+
+var w, h, n: int
+(w, h, n) = stdin.readLine.split.map parseInt
+
+var borders = @[0, w, 0, h]
+
+var x, y, a: int
+for _ in 1..n:
+  (x, y, a) = stdin.readLine.split.map parseInt
+  if a == 1:
+    borders[a-1] = max(borders[a-1], x)
+  elif a == 2:
+    borders[a-1] = min(borders[a-1], x)
+  elif a == 3:
+    borders[a-1] = max(borders[a-1], y)
+  elif a == 4:
+    borders[a-1] = min(borders[a-1], y)
+
+echo max(0, borders[1]-borders[0])*max(0, borders[3]-borders[2])
