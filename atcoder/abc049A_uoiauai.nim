4fe4e256a9af1aebb79d07ec6a8cad37316b7ea2
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-13 18:16:20 -0300

    49A UOIAUAI

---
+++ atcoder/abc049A_uoiauai.nim
@@ -0,0 +1,10 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc049/tasks/abc049_a
+# implementation
+
+from strutils import strip
+
+if stdin.readLine.strip in @["a", "e", "i", "o", "u"]:
+  echo "vowel"
+else:
+  echo "consonant"
