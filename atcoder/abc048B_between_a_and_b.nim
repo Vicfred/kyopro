ff8acd68acfefabb01302819124fa69ef8ab3d74
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-13 18:10:12 -0300

    48B Between a and b

---
+++ atcoder/abc048B_between_a_and_b.nim
@@ -0,0 +1,16 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc048/tasks/abc048_b
+# implementation
+
+import strutils, sequtils
+
+proc parseInt(x: bool): int =
+  if x:
+    return 1
+  else:
+    return 0
+
+var a, b, x: int
+(a, b, x) = stdin.readLine.split.map parseInt
+
+echo b div x - a div x + (a mod x == 0).parseInt
