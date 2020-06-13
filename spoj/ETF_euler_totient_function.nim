99d56d547d8f318224543a90efb36281c4fbbb8d
Vicfred <vicfred@vicfred.dev>
2020-06-12 19:54:06 -0500

    ETF euler totient function first spoj problem

---
+++ spoj/ETF_euler_totient_function.nim
@@ -0,0 +1,26 @@
+# Vicfred
+# https://www.spoj.com/problems/ETF/
+# math
+import strutils, sequtils, math
+
+proc phi(N: int64): int64 =
+  var n = N
+  var euler = n
+  for i in 2..n:
+    if i*i > n:
+      break
+    if n mod i == 0:
+      while n mod i == 0:
+        n = n div i
+      euler -= euler div i
+  if n > 1:
+    euler -= euler div n
+  return euler
+
+var T = stdin.readLine.parseInt
+
+while T > 0:
+  let n = stdin.readLine.parseInt
+  echo phi(n)
+  T -= 1
+
