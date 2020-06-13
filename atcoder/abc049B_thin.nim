ca2d4a25766910257dbcd9f7ca5ba265e6be5079
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-13 18:24:42 -0300

    49B Thin

---
+++ atcoder/abc049B_thin.nim
@@ -0,0 +1,12 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc049/tasks/abc049_b
+# implementation
+
+import strutils
+
+let h = stdin.readLine.split[0].parseInt
+
+for _ in 1..h:
+  var input = stdin.readLine
+  echo input
+  echo input
