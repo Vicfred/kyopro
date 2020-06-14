f6063a66483979ca5ed7f1ea224adcd9c5361e1d
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-14 01:45:47 -0300

    52B increment decrement

---
+++ atcoder/abc052B_increment_decrement.nim
@@ -0,0 +1,17 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc052/tasks/abc052_b
+# simulation
+
+discard stdin.readLine
+let s = stdin.readLine
+
+var ans = 0
+var x = 0
+for i in s:
+  case i:
+    of 'I': inc x
+    of 'D': dec x
+    else: echo "WTF"
+  ans = max(ans, x)
+
+echo ans
