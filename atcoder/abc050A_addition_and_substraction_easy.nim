bdd9cdc6ca6a17304f5a36a2bc1b889a0e649872
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-14 00:53:17 -0300

    50A addition and substraction easy

---
+++ atcoder/abc050A_addition_and_substraction_easy.nim
@@ -0,0 +1,12 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc050/tasks/abc050_a
+# implementation
+
+import strutils
+
+let s = stdin.readLine.split
+
+if s[1] == "+":
+  echo s[0].parseInt + s[2].parseInt
+else:
+  echo s[0].parseInt - s[2].parseInt
