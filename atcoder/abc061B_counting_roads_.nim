343249aa166d4d32046c043f86346860270f85ea
Vicfred <vicfred@vicfred.dev>
2020-06-14 22:35:52 -0500

    abc061B counting roads

---
+++ atcoder/abc061B_counting_roads_.nim
@@ -0,0 +1,18 @@
+# Vicfred
+# https://atcoder.jp/contests/abc061/tasks/abc061_b
+# bucket sort 
+import strutils, sequtils, tables
+
+var n, m: int
+(n, m) = stdin.readLine.split.map parseInt
+
+var cities = initCountTable[int]()
+
+for _ in 1..m:
+  var a, b: int
+  (a, b) = stdin.readLine.split.map parseInt
+  cities.inc(a)
+  cities.inc(b)
+
+for city in 1..n:
+  echo cities.getOrDefault(city)
