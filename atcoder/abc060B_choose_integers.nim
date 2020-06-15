b832977b5bcdc63d881382367b0fedf9812b34b9
Vicfred <vicfred@vicfred.dev>
2020-06-14 21:34:36 -0500

    abc060B choose integers

---
+++ atcoder/abc060B_choose_integers.nim
@@ -0,0 +1,19 @@
+# Vicfred
+# https://atcoder.jp/contests/abc060/tasks/abc060_b
+# math, implementation
+import strutils, sequtils
+
+var a, b, c: int
+(a, b, c) = stdin.readLine.split.map parseInt
+
+var found = false
+
+for i in 1..100:
+  if (a*i) mod b == c:
+    found = true
+
+if found:
+  echo "YES"
+else:
+  echo "NO"
+
