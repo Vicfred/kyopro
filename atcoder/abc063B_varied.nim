4e958a7bd5e1ff1fcf2128830557c13b4a5c1284
Vicfred <vicfred@vicfred.dev>
2020-06-15 00:58:13 -0500

    abc063B varied

---
+++ atcoder/abc063B_varied.nim
@@ -0,0 +1,17 @@
+# Vicfred
+# https://atcoder.jp/contests/abc063/tasks/abc063_b
+# implementation
+import strutils, tables
+
+let s = stdin.readLine.strip
+var letters = initCountTable[char]()
+
+for c in s:
+  letters.inc(c)
+
+for item in letters.values:
+  if item > 1:
+    echo "no"
+    quit()
+echo "yes"
+
