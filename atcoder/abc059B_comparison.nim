cc55dc3261e7c715034b3ab9c8b5a4d2fb823e51
Vicfred <vicfred@vicfred.dev>
2020-06-14 20:55:37 -0500

    abc059B comparison

---
+++ atcoder/abc059B_comparison.nim
@@ -0,0 +1,20 @@
+# Vicfred
+# https://atcoder.jp/contests/abc059/tasks/abc059_b
+# implementation
+import strutils
+
+let A = stdin.readLine.strip
+let B = stdin.readLine.strip
+
+if A.len == B.len:
+  if A > B:
+    echo "GREATER"
+  elif A < B:
+    echo "LESS"
+  else:
+    echo "EQUAL"
+elif A.len < B.len:
+    echo "LESS"
+else:
+    echo "GREATER"
+
