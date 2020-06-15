d160d2d40ef8c630aa2014ef990dbd0db7863b44
Vicfred <vicfred@vicfred.dev>
2020-06-14 20:44:02 -0500

    abc059A three letter acronym

---
+++ atcoder/abc059A_three_letter_acronym.nim
@@ -0,0 +1,13 @@
+# Vicfred
+# https://atcoder.jp/contests/abc059/tasks/abc059_a
+# implementation
+import strutils
+
+var s1, s2, s3: string
+(s1, s2, s3) = stdin.readLine.split
+s1 = s1.toUpper
+s2 = s2.toUpper
+s3 = s3.toUpper
+
+echo s1[0] & s2[0] & s3[0]
+
