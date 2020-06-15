2d4ce5ef75cb08b0a56dabfe67d95d26d3869adc
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-14 22:47:17 -0300

    59A three letter acronym

---
+++ atcoder/abc059A_three_letter_acronym.d
@@ -0,0 +1,9 @@
+import std.stdio: writefln, readln;
+import std.array: split, array;
+import std.string: capitalize;
+import std.algorithm: map;
+
+void main() {
+	string[] s = readln.split.map!(capitalize).array;
+	"%s%s%s".writefln(s[0][0], s[1][0], s[2][0]);
+}
