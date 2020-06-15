8c299e572c86122c71cc6423e8448dc5e8790134
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-15 00:01:38 -0300

    61B counting roads

---
+++ atcoder/abc061B_counting_roads.nim
@@ -0,0 +1,19 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc061/tasks/abc061_b
+# implementation
+
+import strutils, sequtils
+
+var n, m: int
+(n, m) = stdin.readLine.split.map parseInt
+
+var cities = newSeq[int](n)
+
+var a, b: int
+for _ in 1..m:
+  (a, b) = stdin.readLine.split.map parseInt
+  inc cities[a-1]
+  inc cities[b-1]
+
+for i in cities:
+  echo i
