edc3f2d48266885fa81f93d91dafd3ce22174847
Vicfred <vicfred@vicfred.dev>
2020-06-14 21:34:27 -0500

    abc060A shiritori

---
+++ atcoder/abc060A_shiritori.nim
@@ -0,0 +1,13 @@
+# Vicfred
+# https://atcoder.jp/contests/abc060/tasks/abc060_a
+# implementation
+import strutils
+
+var a, b, c: string
+(a, b, c) = stdin.readLine.split
+
+if a[^1] == b[0] and b[^1] == c[0]:
+  echo "YES"
+else:
+  echo "NO"
+
