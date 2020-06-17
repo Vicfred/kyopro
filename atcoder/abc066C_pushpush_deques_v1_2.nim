d3f25e687fb8f095e4d031400e019802f138864e
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-17 01:52:00 -0300

    Cpushpush :v

---
+++ atcoder/abc066C_pushpush_deques_v1_2.nim
@@ -0,0 +1,25 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc066/tasks/arc077_a
+# data structures
+import strutils, deques
+
+let n = stdin.readLine.parseInt
+var a = stdin.readLine.split
+
+var b = initDeque[string]();
+
+for i, elem in a:
+  if i mod 2 == 0:
+    b.addLast(elem)
+  else:
+    b.addFirst(elem)
+
+if a.len mod 2 == 1:
+  while b.len > 0:
+    stdout.write b.popLast & " "
+else:
+  while b.len > 0:
+    stdout.write b.popFirst & " "
+
+echo()
+
