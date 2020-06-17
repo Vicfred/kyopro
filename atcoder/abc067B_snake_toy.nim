2e9d8e47b52523d4262349110e44720a1221da9a
Vicfred <vicfred@vicfred.dev>
2020-06-16 22:42:38 -0500

    abc067B snake toy

---
+++ atcoder/abc067B_snake_toy.nim
@@ -0,0 +1,18 @@
+# Vicfred
+# https://atcoder.jp/contests/abc067/tasks/abc067_b
+# sorting, greedy
+import strutils, sequtils, algorithm
+
+var n, k: int
+(n, k) = stdin.readLine.split.map parseInt
+var l = stdin.readLine.split.map parseInt
+
+l.sort system.cmp
+l.reverse
+
+var ans = 0
+for i in 0..<k:
+  ans += l[i]
+
+echo ans
+
