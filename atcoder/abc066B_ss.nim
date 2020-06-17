ec3fdc3690ad6f323a921b894062ae752bdab594
Vicfred <vicfred@vicfred.dev>
2020-06-16 22:41:55 -0500

    abc066B ss

---
+++ atcoder/abc066B_ss.nim
@@ -0,0 +1,22 @@
+# Vicfred
+# https://atcoder.jp/contests/abc066/tasks/abc066_b
+# string manipulation
+import strutils
+
+proc even(str: string): bool =
+  if str.len mod 2 != 0:
+    return false
+  let size = str.len
+  let first = str[0..<size div 2]
+  let second = str[size div 2..<size]
+  if first == second:
+    return true
+  return false
+
+let s = stdin.readLine.strip
+
+for i in 1..<s.len:
+  if even(s[0..<s.len-i]):
+    echo s.len-i
+    quit()
+
