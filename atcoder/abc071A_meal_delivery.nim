c83f4513fd6bd5f77ca79a12c250677124f33232
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-19 02:18:37 -0300

    71A meal delivery

---
+++ atcoder/abc071A_meal_delivery.nim
@@ -0,0 +1,13 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc071/tasks/abc071_a
+# implementation
+
+import strutils, sequtils
+
+var x, a, b: int
+(x, a, b) = stdin.readLine.split.map parseInt
+
+if abs(x-a) < abs(x-b):
+  echo "A"
+else:
+  echo "B"
