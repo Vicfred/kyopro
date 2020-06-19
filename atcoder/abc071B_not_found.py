4ab0d0dd3b82991bb75fc5e57b40b3cbd2afa976
Vicfred <vicfred@vicfred.dev>
2020-06-19 00:27:07 -0500

    abc071B Not Found

---
+++ atcoder/abc071B_not_found.py
@@ -0,0 +1,12 @@
+# Vicfred
+# https://atcoder.jp/contests/abc071/tasks/abc071_b
+# string manipulation, implementation
+S = input().strip()
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+for ch in alphabet:
+    if ch not in S:
+        print(ch)
+        exit()
+print("None")
+
