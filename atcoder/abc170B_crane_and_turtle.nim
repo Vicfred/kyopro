a0f14befbced5eb18d457ad45de045bf5b703d1e
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-19 00:31:56 -0300

    170B crane and turtle

---
+++ atcoder/abc170B_crane_and_turtle.nim
@@ -0,0 +1,15 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc170/tasks/abc170_b
+# implementation
+
+from strutils import split, parseInt
+from sequtils import map
+
+var x, y: int
+(x, y) = stdin.readLine.split.map(parseInt)
+
+if y mod 2 == 0 and y <= 4*x and y >= 2*x:
+  echo "Yes"
+else:
+  echo "No"
+
