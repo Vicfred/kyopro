1e2877387c7a95dc9d7112b27954116a6c5a4ac3
Vicfred <vicfred@vicfred.dev>
2020-06-19 01:18:33 -0500

    abc072B oddstring

---
+++ atcoder/abc072B_oddstring.py
@@ -0,0 +1,11 @@
+# Vicfred
+# https://atcoder.jp/contests/abc072/tasks/abc072_b
+# string manipulation, simulation
+s = input()
+
+ans = ""
+for i in range(len(s)):
+    if i%2 == 0:
+        ans += s[i]
+print(ans)
+
