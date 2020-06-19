1a56a423ff740c9757f80294f528cc6ab16df580
Vicfred <vicfred@vicfred.dev>
2020-06-18 21:58:46 -0500

    abc070A palindromic number

---
+++ atcoder/abc070A_palindromic_number.nim
@@ -0,0 +1,9 @@
+# Vicfred
+# https://atcoder.jp/contests/abc070/tasks/abc070_a
+# string manipulation
+import strutils, sequtils
+
+let n = stdin.readLine
+
+echo(if n[0] == n[2]: "Yes" else: "No")
+
