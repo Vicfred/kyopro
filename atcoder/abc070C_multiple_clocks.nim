b4d749efd277c00c447da583905a3d5c4ec6d455
Vicfred <vicfred@vicfred.dev>
2020-06-19 00:18:51 -0500

    abc070C multiple clocks

---
+++ atcoder/abc070C_multiple_clocks.nim
@@ -0,0 +1,13 @@
+# Vicfred
+# https://atcoder.jp/contests/abc070/tasks/abc070_c
+# math
+import strutils, sequtils, math
+
+let n = stdin.readLine.parseInt
+
+var ans = 1
+for i in 1..n:
+  let t = stdin.readLine.parseInt
+  ans = lcm(ans, t)
+echo ans
+
