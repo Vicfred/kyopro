585c1b325033bdeb120df5bab921892bb2c6fd34
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-19 01:07:34 -0300

    168A therefore

---
+++ atcoder/abc168A_therefore.nim
@@ -0,0 +1,13 @@
+from strutils import parseInt
+
+const hon = ['2', '4', '5', '7', '9']
+const pon = ['0', '1', '6', '8']
+
+let n = stdin.readLine.parseInt
+
+if ($n)[^1] in hon:
+  echo "hon"
+elif ($n)[^1] in pon:
+  echo "pon"
+else:
+  echo "bon"
