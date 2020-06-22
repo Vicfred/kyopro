11d96910a65fd72de11ad58c86bc45677841a283
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-22 00:03:18 -0300

    171E red scarf

---
+++ atcoder/abc171E_red_scarf.py
@@ -0,0 +1,14 @@
+# Implementation by unihernandez22 || Idea by Vicfred
+# https://atcoder.jp/contests/abc171/tasks/abc171_e
+# bitwise
+
+input()
+a = list(map(int, input().split()))
+
+xor = a[0]
+for i in a[1:]:
+    xor ^= i
+
+for i in a:
+    print(xor ^ i, end=' ')
+print()
