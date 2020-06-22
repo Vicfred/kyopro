0552c5fedf4d0107db503d1cd6afa4f745a5c331
Vicfred <vicfred@vicfred.dev>
2020-06-22 00:57:20 -0500

    abc151B achieve the goal

---
+++ atcoder/abc151B_achieve_the_goal.nim
@@ -0,0 +1,19 @@
+# Vicfred
+# https://atcoder.jp/contests/abc151/tasks/abc151_b
+# brute force, simulation
+import strutils, sequtils, math
+
+var n, k, m: int
+(n, k, m) = stdin.readLine.split.map parseInt
+
+let a = stdin.readLine.split.map parseInt
+
+let score = sum(a)
+
+for i in 0..k:
+  let average = (score+i)/n
+  if average >= float(m):
+    echo i
+    quit()
+echo -1
+
