292ab4c52a9f959095abcd32ba514cd623d82613
Vicfred <vicfred@vicfred.dev>
2020-06-21 20:01:41 -0500

    abc171A alphabet

---
+++ atcoder/abc171A_alphabet.nim
@@ -0,0 +1,12 @@
+# Vicfred
+# https://atcoder.jp/contests/abc171/tasks/abc171_a
+# implementation, string manipulation
+import strutils
+
+let s = stdin.readLine.strip
+
+if s[0].isUpperAscii:
+  echo "A"
+else:
+  echo "a"
+
