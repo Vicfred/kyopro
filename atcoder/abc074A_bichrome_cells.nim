e1c0a00abd0ccdc018727f0064566cd9e098ea01
Vicfred <vicfred@vicfred.dev>
2020-06-23 20:07:22 -0500

    abc074A Bichrome Cells

---
+++ atcoder/abc074A_bichrome_cells.nim
@@ -0,0 +1,10 @@
+# Vicfred
+# https://atcoder.jp/contests/abc074/tasks/abc074_a
+# simulation
+import strutils
+
+let n = stdin.readLine.parseInt
+let a = stdin.readLine.parseInt
+let ans = n*n-a
+echo ans
+
