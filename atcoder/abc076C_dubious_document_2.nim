b0ac19e515593c843f20f3efb09aec2a538c3ed4
Vicfred <vicfred@vicfred.dev>
2020-06-25 21:08:31 -0500

    abc076C Dubious Document 2

---
+++ atcoder/abc076C_dubious_document_2.nim
@@ -0,0 +1,34 @@
+# Vicfred
+# https://atcoder.jp/contests/abc076/tasks/abc076_c
+# string manipulation
+import strutils
+
+var s = stdin.readLine.strip
+let t = stdin.readLine.strip
+
+var idx = -1
+
+for i in 0..(s.len-t.len):
+  if s[i] == '?' or s[i] == t[0]:
+    var matched = true
+    for j in 0..<t.len:
+      if s[i+j] != '?' and s[i+j] != t[j]:
+        matched = false
+        break
+    if matched:
+      idx = i
+
+if idx == -1:
+  echo "UNRESTORABLE"
+  quit()
+
+for i in 0..<t.len:
+  s[idx+i] = t[i]
+
+for ch in s:
+  if ch == '?':
+    stdout.write 'a'
+  else:
+    stdout.write ch
+echo ""
+
