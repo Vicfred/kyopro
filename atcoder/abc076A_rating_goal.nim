1500ab4b34b68fc01be6daab493bef44edd86133
Vicfred <vicfred@vicfred.dev>
2020-06-25 19:53:44 -0500

    abc076A Rating Goal

---
+++ atcoder/abc076A_rating_goal.nim
@@ -0,0 +1,11 @@
+# Vicfred
+# https://atcoder.jp/contests/abc076/tasks/abc076_a
+# math
+import strutils, sequtils
+
+let r = stdin.readLine.parseInt
+let g = stdin.readLine.parseInt
+
+let ans = 2*g-r
+echo ans
+
