a285bb70cb4884738602761290279d99829f69cf
Vicfred <vicfred@vicfred.dev>
2020-06-25 20:19:46 -0500

    abc076B Addition and Multiplication

---
+++ atcoder/abc076B_addition_and_multiplication.nim
@@ -0,0 +1,27 @@
+# Vicfred
+# https://atcoder.jp/contests/abc076/tasks/abc076_b
+# simulation, dfs
+import strutils
+
+let n = stdin.readLine.parseInt
+let k = stdin.readLine.parseInt
+
+var queue = newSeq[int]()
+
+queue.add(1)
+
+var steps = 0
+
+var minima = int.high
+while steps < n:
+  steps += 1
+  var items = newSeq[int]()
+  while queue.len > 0:
+    items.add(queue.pop)
+  for item in items:
+    queue.add(2*item)
+    queue.add(item+k)
+minima = min(queue)
+
+echo minima
+
