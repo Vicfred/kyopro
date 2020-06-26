ee2ca736bc6225771fae5ef576f3e2f202ea42aa
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-25 23:33:36 -0300

    76B_addition_and_multiplication

---
+++ atcoder/abc076B_addition_and_multiplication.d
@@ -0,0 +1,17 @@
+// unihernandez22
+// https://atcoder.jp/contests/abc076/tasks/abc076_b
+// implementation
+
+import std.stdio;
+void main() {
+	int n, k;
+	"%s\n%s\n".readf(&n, &k);
+	int x = 1;
+	foreach(_; 0..n) {
+		if (x < k)
+			x += x;
+		else
+			x += k;
+	}
+	x.writeln;
+}
