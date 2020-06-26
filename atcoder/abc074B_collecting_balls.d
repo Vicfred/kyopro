cfc4abbc6b8e5626818f3af21357debed44cc00b
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-25 23:26:35 -0300

    74B Collecting Balls

---
+++ atcoder/abc074B_collecting_balls.d
@@ -0,0 +1,20 @@
+// unihernandez22
+// https://atcoder.jp/contests/abc074/tasks/abc074_b
+// implementation
+
+import std.stdio;
+import std.array;
+import std.algorithm;
+import std.conv;
+
+void main() {
+	int n, k;
+	"%s\n%s\n".readf(&n, &k);
+	int[] x = readln.split.map!(to!int).array;
+
+	long ans = 0;
+	foreach(int i; x) {
+		ans += min(k-i, i)*2;
+	}
+	ans.writeln;
+}
