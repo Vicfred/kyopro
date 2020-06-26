91d207cd62182b8dcb5bdb8c0e03c0184f112d41
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-25 23:25:55 -0300

    76A Rating Goal

---
+++ atcoder/abc076A_rating_goal.d
@@ -0,0 +1,10 @@
+// unihernandez22
+// https://atcoder.jp/contests/abc076/tasks/abc076_a
+// math
+
+import std.stdio;
+void main() {
+  float r, g;
+  "%s\n%s\n".readf(&r, &g);
+  writeln(2*g-r);
+}
