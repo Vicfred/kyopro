d58191469c1f662ff87d75efb6a1045c5c9724c9
Vicfred <vicfred@vicfred.dev>
2020-06-26 06:23:22 -0500

    cf4A Watermellon

---
+++ codeforces/cf4A_watermelon.hs
@@ -0,0 +1,12 @@
+-- Vicfred
+-- https://codeforces.com/problemset/problem/4/A
+
+f :: Int -> String
+f x
+    | x == 2 = "NO"
+    | even x = "YES"
+    | otherwise = "NO"
+
+main :: IO ()
+main = interact $ f . read
+
