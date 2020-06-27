21b440c4423733231cd9a1ba8f2a05daf0059628
Naveen <fredericktrotteville99@gmail.com>
2020-06-27 12:40:48 +0530

    ABC171.C - One Quadrillion and One Dalmatians

---
+++ atcoder/abc171C_one_quadrillion_and_one_dalmatians.hs
@@ -0,0 +1,13 @@
+import Data.Char (chr)
+ 
+solve :: Int -> Int -> (Int, String)
+solve p n = (q, chr (97 + r) : s)
+  where
+    (m, s) = if n > p
+               then solve (p * 26) (n - p)
+               else (n - 1, "")
+    (q, r) = m `divMod` 26
+ 
+main :: IO ()
+main = getLine >>= putStrLn . snd . solve 26 . read
+
