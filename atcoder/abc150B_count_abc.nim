c4ac68b546a301941cecfa0a7aef0971d6fa1706
Vicfred <vicfred@vicfred.dev>
2020-06-26 20:41:20 -0500

    abc150B Count ABC

---
+++ atcoder/abc150B_count_abc.nim
@@ -0,0 +1,10 @@
+# Vicfred
+# https://atcoder.jp/contests/abc150/tasks/abc150_b
+# string manipulation
+import strutils
+
+let n = stdin.readLine.parseInt
+let s = stdin.readLine.strip
+
+echo s.count("ABC")
+
