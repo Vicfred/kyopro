0eb09f7aad10c246fc40be0489988bf08ff8d563
Vicfred <vicfred@vicfred.dev>
2020-06-28 07:38:28 -0500

    abc170A Five Variables

---
+++ atcoder/abc170A_five_variables.ml
@@ -0,0 +1,12 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc170/tasks/abc170_a
+ * implementation
+ * *)
+open Printf
+open Scanf
+
+let solve a b c d e = 15 - a - b - c - d - e
+
+let main =
+    scanf "%d %d %d %d %d" solve |> printf "%d\n"
+
