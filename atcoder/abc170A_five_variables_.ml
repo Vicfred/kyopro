728ce922a54c99ae8c8a5eab89855a0185b227c3
Vicfred <vicfred@vicfred.dev>
2020-06-28 07:38:58 -0500

    abc170A Five Variables

---
+++ atcoder/abc170A_five_variables_.ml
@@ -0,0 +1,14 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc170/tasks/abc170_a
+ * implementation
+ * *)
+open Printf
+open Scanf
+
+let main =
+    let xs = Array.init 5 @@ fun _ -> scanf"%d " @@ fun x -> x in
+    for i = 0 to 4 do
+        if xs.(i) = 0 then
+            printf "%d\n" @@ i + 1
+    done
+
