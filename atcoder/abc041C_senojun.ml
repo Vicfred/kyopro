ab5bca2bce3d5681134a58837929e80bb6ff0a11
Vicfred <vicfred@vicfred.dev>
2020-06-28 00:29:05 -0500

    abc041C 背の順

---
+++ atcoder/abc041C_senojun.ml
@@ -0,0 +1,10 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc041/tasks/abc041_c
+ * sorting
+ * *)
+Scanf.scanf "%d" (fun n ->
+    let a = Array.init n (fun i -> Scanf.scanf " %d" (fun a -> -a, i+1)) in
+    Array.sort compare a;
+    Array.iter (fun (_, y) -> Printf.printf "%d\n" y) a
+)
+
