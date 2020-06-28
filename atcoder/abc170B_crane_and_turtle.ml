9ea48e088604b815092963adcf4a78a1ddd17e48
Vicfred <vicfred@vicfred.dev>
2020-06-28 09:12:40 -0500

    abc170B Crane and Turtle

---
+++ atcoder/abc170B_crane_and_turtle.ml
@@ -0,0 +1,10 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc170/tasks/abc170_b
+ * brute force *)
+let solve x y =
+    let a, b = 4*x - y, y - 2*x in
+    if a mod 2 = 0 && b mod 2 = 0 && a >= 0 && b >= 0 then "Yes" else "No"
+
+let main =
+    Scanf.scanf "%d %d" solve |> Printf.printf "%s\n"
+
