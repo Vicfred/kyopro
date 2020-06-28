c9d41ff17a716630907390bf91f097a8df67e943
Vicfred <vicfred@vicfred.dev>
2020-06-27 23:10:18 -0500

    abc041 直方体

---
+++ atcoder/abc041_chokuhoudai.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc041/tasks/abc041_b
+ * math, implementation
+ * *)
+let ( * ) a b = (a * b) mod 1000000007
+
+let () = Scanf.scanf "%d %d %d" (fun a b c ->
+    Printf.printf "%d\n" (a*b*c))
+
