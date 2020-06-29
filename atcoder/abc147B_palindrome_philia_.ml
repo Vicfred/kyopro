67c73165d6928cc687a19d9d53ef866a235b5e67
Vicfred <vicfred@vicfred.dev>
2020-06-28 23:01:07 -0500

    abc147B Palindrome-philia

---
+++ atcoder/abc147B_palindrome_philia_.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc147/tasks/abc147_b
+ * string manipulation
+ * *)
+let s, ans = read_line(), ref 0
+let main =
+    String.(iteri (fun i c -> if s.[length s - i - 1] <> c then incr ans) s);
+    Printf.printf "%d\n" @@ !ans / 2
+
