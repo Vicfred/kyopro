fe0454c66ca00b6074301c96ae8a777d9df5d987
Vicfred <vicfred@vicfred.dev>
2020-06-28 20:20:17 -0500

    abc148A Round One

---
+++ atcoder/abc148A_round_one.ml
@@ -0,0 +1,10 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc148/tasks/abc148_a
+ * implementation
+ * *)
+
+let solve a b = 6 - a - b
+
+let main =
+    Scanf.scanf"%d %d" solve |> Printf.printf "%d\n"
+
