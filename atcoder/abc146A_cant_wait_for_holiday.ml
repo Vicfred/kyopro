ad531d15bcccf6aafdaaef3cc69db850b0f7c471
Vicfred <vicfred@vicfred.dev>
2020-06-28 23:26:36 -0500

    abc146A Can't Wait for Holiday

---
+++ atcoder/abc146A_cant_wait_for_holiday.ml
@@ -0,0 +1,14 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc146/tasks/abc146_a
+ * implementation
+ * *)
+Printf.printf "%d\n" @@ match read_line () with
+| "SUN" -> 7
+| "MON" -> 6
+| "TUE" -> 5
+| "WED" -> 4
+| "THU" -> 3
+| "FRI" -> 2
+| "SAT" -> 1
+| _ -> -1
+
