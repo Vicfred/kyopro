a545b0c678a8a39cb1d9e52120e636e3f56ac1d6
Vicfred <vicfred@vicfred.dev>
2020-06-28 20:31:00 -0500

    abc148B Strings With the Same Length

---
+++ atcoder/abc148B_strings_with_the_same_length.py
@@ -0,0 +1,10 @@
+# Vicfred
+# https://atcoder.jp/contests/abc148/tasks/abc148_b
+# string manipulation
+n = int(input())
+s, t = input().split()
+
+for i in range(n):
+    print(s[i]+t[i], end="")
+print()
+
