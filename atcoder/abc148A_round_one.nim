ebdfb3d331d42ecdfc40befbd2da19681164a4e8
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-06-28 22:10:41 -0300

    148A Round One

---
+++ atcoder/abc148A_round_one.nim
@@ -0,0 +1,10 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc148/tasks/abc148_a
+# implementation
+
+from strutils import parseInt
+
+let a = stdin.readLine.parseInt
+let b = stdin.readLine.parseInt
+
+echo 6 - a - b
