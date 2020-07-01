ad8097619aeb4467a32fdffad11ee0da1e0f5e44
Vicfred <vicfred@vicfred.dev>
2020-06-30 20:28:05 -0500

    abc144B 81

---
+++ atcoder/abc144B_81.ml
@@ -0,0 +1,14 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc144/tasks/abc144_b
+ * implementation
+ * *)
+
+let main =
+    Scanf.scanf "%d" @@ fun n ->
+      print_endline @@ if
+        Array.fold_left (Array.fold_left ( || )) false @@
+        Array.init 9 @@ fun i ->
+          Array.init 9 @@ fun j -> n = (i+1)*(j+1)
+    then "Yes"
+    else "No"
+
