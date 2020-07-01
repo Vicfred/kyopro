048add83a0b20aa7d8ec7874ede0498d192152a6
Vicfred <vicfred@vicfred.dev>
2020-06-30 23:52:59 -0500

    abc142C Go to School

---
+++ atcoder/abc142C_go_to_school.ml
@@ -0,0 +1,10 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc142/tasks/abc142_c
+ * implementation, simulation
+ * *)
+Scanf.scanf "%d\n" @@ fun n ->
+    let a = Array.init n (fun i -> Scanf.scanf "%d " (fun a -> i+1,a)) in
+    Array.sort (fun (a,b) (x,y) -> compare b y) a;
+    Array.iter (fun (x, _) -> Printf.printf "%d " x) a;
+    Printf.printf "\n"
+
