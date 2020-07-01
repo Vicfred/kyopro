d1c3ec7f249e91532a399ee9510eef8cf2b52123
Vicfred <vicfred@vicfred.dev>
2020-07-01 08:10:32 -0500

    abc140B Buffet

---
+++ atcoder/abc140B_buffet.ml
@@ -0,0 +1,17 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc140/tasks/abc140_b
+ * implementation
+ * *)
+
+let f () = Scanf.scanf " %d" ( + ) 0
+let n = f ()
+let a = Array.init n @@ fun _ -> f ()
+let b = Array.init n @@ fun _ -> f ()
+let c = Array.init (n - 1) @@ fun _ -> f ()
+let ans = ref @@ Array.fold_left ( + ) 0 b
+let main =
+    for i = 0 to (n - 2) do
+        if a.(i + 1) = a.(i) + 1 then ans := !ans + c.(a.(i) - 1)
+    done;
+    Printf.printf "%d\n" !ans
+
