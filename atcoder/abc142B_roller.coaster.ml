4c81bba43fa72d9d86209329e5f844f17f6cf860
Vicfred <vicfred@vicfred.dev>
2020-06-30 23:19:30 -0500

    abc142B Roller Coaster

---
+++ atcoder/abc142B_roller.coaster.ml
@@ -0,0 +1,8 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc142/tasks/abc142_b
+ * implementation, simulation
+ * *)
+let n, k = Scanf.scanf "%d %d\n" @@ fun n k -> n, k
+let cs = Array.init n @@ fun _ -> Scanf.scanf "%d " @@ fun h -> if h >= k then 1 else 0
+let main = Printf.printf "%d\n" @@ Array.fold_left ( + ) 0 cs 
+
