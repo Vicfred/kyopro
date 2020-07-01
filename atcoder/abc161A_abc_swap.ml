cdff1e41bacae00581a28ac5bca3f9271da5aaac
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-07-01 15:36:36 -0300

    161A ABC Swap

---
+++ atcoder/abc161A_abc_swap.ml
@@ -0,0 +1,7 @@
+(* unihernandez22
+ * https://atcoder.jp/contests/abc161/tasks/abc161_a
+ * implementation
+ * *)
+
+Printf.printf "%s\n" @@
+Scanf.scanf "%s %s %s\n" @@ fun x y z -> z ^ " " ^ x ^ " " ^ y;
