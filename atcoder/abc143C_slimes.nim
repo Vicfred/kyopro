4256ac975f9e8b78586c365268e575beebf6ddd1
Vicfred <vicfred@vicfred.dev>
2020-06-30 21:57:22 -0500

    abc143C Slimes

---
+++ atcoder/abc143C_slimes.nim
@@ -0,0 +1,20 @@
+# Vicfred
+# https://atcoder.jp/contests/abc143/tasks/abc143_c
+# implementation
+import strutils
+
+let n = stdin.readLine.parseInt
+let s = stdin.readLine.strip
+
+var x = 0
+var ans = 1
+var last = s[0]
+
+while x < s.len:
+  if s[x] != last:
+    ans += 1
+    last = s[x]
+  x += 1
+
+echo ans
+
