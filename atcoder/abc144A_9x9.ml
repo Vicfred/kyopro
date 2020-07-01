6da47d268d92d684188ecaa23161a191a21b3002
Vicfred <vicfred@vicfred.dev>
2020-06-30 19:55:54 -0500

    abc144A 9x9

---
+++ atcoder/abc144A_9x9.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc144/tasks/abc144_a
+ * implementation
+ * *)
+Scanf.scanf "%d %d" (fun a b ->
+    if a <= 9 && b <= 9 then a*b
+    else -1
+) |> Printf.printf "%d\n"
+
