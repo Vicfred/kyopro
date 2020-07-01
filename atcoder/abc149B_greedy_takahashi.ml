f421c45945b062e59280391fb4b51eb9ac459192
Vicfred <vicfred@vicfred.dev>
2020-07-01 00:09:49 -0500

    abc149B Greedy Takahashi

---
+++ atcoder/abc149B_greedy_takahashi.ml
@@ -0,0 +1,10 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc149/tasks/abc149_b
+ * implementation
+ * *)
+Scanf.scanf "%d %d %d\n" (fun a b k ->
+    if k <= a then (a - k, b)
+    else if k <= a+b then (0, b - k + a)
+    else (0, 0)
+) |> fun (x, y) -> Printf.printf "%d %d\n" x y
+
