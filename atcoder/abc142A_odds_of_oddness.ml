05a22e46629b6f8f90df2d8b2b5f4724c5413c8f
Vicfred <vicfred@vicfred.dev>
2020-06-30 22:58:27 -0500

    abc142A Odds of Oddness

---
+++ atcoder/abc142A_odds_of_oddness.ml
@@ -0,0 +1,7 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc142/tasks/abc142_a
+ * math
+ * *)
+Scanf.scanf "%d" @@ fun n ->
+    Printf.printf "%.9f\n" (float_of_int (n - n / 2) /. float_of_int n)
+
