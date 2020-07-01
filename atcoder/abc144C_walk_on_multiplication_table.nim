7d3e253f3e8f256cfdc6ff89ccdb142d9cfab138
Vicfred <vicfred@vicfred.dev>
2020-06-30 21:04:41 -0500

    abc144C Walk on Multiplication Table

---
+++ atcoder/abc144C_walk_on_multiplication_table.nim
@@ -0,0 +1,13 @@
+# Vicfred
+# https://atcoder.jp/contests/abc144/tasks/abc144_c
+# math
+import strutils, math
+
+let n = stdin.readLine.parseInt
+
+var d = 1
+for i in 1..int(sqrt(float(n))):
+  if n mod i == 0:
+    d = i
+echo d - 1 + n div d - 1
+
