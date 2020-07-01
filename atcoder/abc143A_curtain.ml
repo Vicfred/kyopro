ab7365e2e12e556f8d296fc69e13fb66ec4075cc
Vicfred <vicfred@vicfred.dev>
2020-06-30 21:11:23 -0500

    abc143A Curtain

---
+++ atcoder/abc143A_curtain.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc143/tasks/abc143_a
+ * implementation
+ * *)
+Scanf.scanf "%d %d" (fun a b ->
+    if 2*b >= a then 0
+    else a - 2 * b
+) |> Printf.printf "%d\n"
+
