9dbd75f06819f8543734b604fe8fabeac1a7d39d
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-07-01 00:03:05 -0300

    144C

---
+++ atcoder/abc144C_walk_on_multiplication_table_.ml
@@ -0,0 +1,12 @@
+(* Vicfred & unihernandez22
+ * https://atcoder.jp/contests/abc144/tasks/abc144_c
+ * math
+ * *)
+
+Scanf.scanf "%d" (fun n ->
+    let d = ref 1 in
+    for i = 1 to (int_of_float @@ sqrt @@ float_of_int @@ n) do
+        if n mod i == 0 then d := i
+    done;
+    Printf.printf "%d\n" ( !d - 1 + n / !d - 1 )
+)
