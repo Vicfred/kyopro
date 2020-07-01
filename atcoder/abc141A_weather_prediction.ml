58e4a4dc41848623938bbf1b20c05724884ba251
Vicfred <vicfred@vicfred.dev>
2020-07-01 00:25:14 -0500

    abc141A Weather Prediction

---
+++ atcoder/abc141A_weather_prediction.ml
@@ -0,0 +1,10 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc141/tasks/abc141_a
+ * implementation
+ * *)
+Scanf.scanf "%s\n" (fun s ->
+    if s = "Sunny" then "Cloudy"
+    else if s = "Cloudy" then "Rainy"
+    else "Sunny"
+) |> Printf.printf "%s\n"
+
