306409a2e16475944d832c8dadaee8527aecfeb3
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-07-01 23:01:06 -0300

    149B Greedy Takahashi

---
+++ atcoder/abc149B_greedy_takahashi.nim
@@ -0,0 +1,10 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc149/tasks/abc149_b
+# implementation
+
+import strutils, sequtils
+
+var a, b, k: int
+(a, b, k) = stdin.readLine.split.map parseInt
+
+echo max(0, a - k), " ", max(0, b - max(0, k - a))
