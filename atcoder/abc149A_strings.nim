e9a88ab3ff3f178b710914e4f1d25db4c301e7c5
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-07-01 22:54:49 -0300

    149A Strings

---
+++ atcoder/abc149A_strings.nim
@@ -0,0 +1,8 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc149/tasks/abc149_a
+# implementation
+
+from strutils import split, join
+from algorithm import reversed
+
+echo stdin.readLine.split.reversed.join("")
