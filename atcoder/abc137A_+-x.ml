4757f3790910853a0a89af2cfeeae0294aa20051
Vicfred <vicfred@vicfred.dev>
2020-07-02 23:44:25 -0500

    abc137A +-x

---
+++ atcoder/abc137A_+-x.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc137/tasks/abc137_a
+ * implementation
+ * *)
+
+Scanf.scanf "%d %d" (fun a b ->
+    max (a + b) @@ max (a - b) @@ a * b
+) |> Printf.printf "%d\n"
+
