81038c793ae7410a89af1670809179d14f7cd30a
Vicfred <vicfred@vicfred.dev>
2020-07-02 22:29:09 -0500

    abc138B Resistors in Parallel

---
+++ atcoder/abc138B_resistors_in_parallel.ml
@@ -0,0 +1,10 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc138/tasks/abc138_b
+ * implementation
+ * *)
+let n = Scanf.scanf "%d\n" ( + ) 0
+let a = Array.init n @@ fun _ -> Scanf.scanf " %f" ( +. ) 0.0
+let main = Printf.printf "%.9f\n" @@
+((/.) 1.0) @@
+Array.fold_left (fun x y -> x +. (1.0 /. y)) 0.0 a
+
