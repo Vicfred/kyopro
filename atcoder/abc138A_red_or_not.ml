228bec1aaec2aa7b04d690f33f64d07e8ac109f1
Vicfred <vicfred@vicfred.dev>
2020-07-02 22:09:36 -0500

    abc138A Red or Not

---
+++ atcoder/abc138A_red_or_not.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc138/tasks/abc138_a
+ * implementation
+ * *)
+Scanf.scanf "%d\n%s\n" (fun a s ->
+    if a >= 3200 then s
+    else "red"
+) |> Printf.printf "%s\n"
+
