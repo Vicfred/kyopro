b81f42c5322d32229e643610030c25a99694c129
Vicfred <vicfred@vicfred.dev>
2020-07-02 20:25:08 -0500

    abc139A Tenki

---
+++ atcoder/abc139A_tenki.hs
@@ -0,0 +1,9 @@
+-- Vicfred
+-- https://atcoder.jp/contests/abc139/tasks/abc139_a
+-- implementation
+
+predict :: String -> String -> Int
+predict s t = length $ filter id $ zipWith (==) s t
+
+main = predict <$> getLine <*> getLine >>= print
+
