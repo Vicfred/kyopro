6778e73255f5d69db9c96e28f8b2e28030b6c28f
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-07-02 22:33:32 -0300

    139B Power Socket

---
+++ atcoder/abc139B_power_socket.cpp
@@ -0,0 +1,13 @@
+// unihernandez22
+// https://atcoder.jp/contests/abc139/tasks/abc139_b
+// math
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << (b+a-3)/(a-1) << endl;
+	return 0;
+}
