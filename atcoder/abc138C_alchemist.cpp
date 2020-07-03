cc9bd3ea960ee6b8f8ce076a0625979aceab78e2
Tsuki <TsukiZombina@users.noreply.github.com>
2020-07-03 15:12:06 -0500

    Add 'abc138C_alchemist.cpp'
    Added alchemist abc138C in C++

---
+++ atcoder/abc138C_alchemist.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int N;
+    float number = 0, mix;
+    std::cin >> N;
+    std::vector<int> numbers(N);
+    for(auto& x: numbers)
+    {
+        std::cin >> x;
+    }
+
+    std::sort(numbers.begin(), numbers.end());
+     
+    mix = numbers[0];
+    for (const auto& number: numbers) {
+        mix = (mix + number)/2;
+    }
+    std::cout << mix;
+    return 0;
+}
