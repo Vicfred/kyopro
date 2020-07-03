bae7027fb836898d06bdcfe71bde05a9907b26ff
Vicfred <vicfred@vicfred.dev>
2020-07-02 22:50:47 -0500

    abc138C Alchemist

---
+++ atcoder/abc138C_alchemist.d
@@ -0,0 +1,23 @@
+// Vicfred
+// https://atcoder.jp/contests/abc138/tasks/abc138_c
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    long n = readln.chomp.to!long;
+    long[] v = readln.split.map!(to!long).array;
+
+    v.sort;
+
+    double answer = v[0];
+    for(int i = 1; i < n; i++) {
+        answer += double(v[i]);
+        answer /= 2.0;
+    }
+
+    writefln("%.9f", answer);
+}
+
