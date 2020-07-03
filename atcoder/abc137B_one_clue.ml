30aec163291bfb7461ac18ea7a832eaf48ba81e8
Vicfred <vicfred@vicfred.dev>
2020-07-02 23:59:00 -0500

    abc137B One Clue

---
+++ atcoder/abc137B_one_clue.ml
@@ -0,0 +1,12 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc137/tasks/abc137_b
+ * implementation
+ * *)
+
+let rec solve i n =
+    if i > n then Printf.printf "\n" else
+        (Printf.printf "%d " i; solve (i+1) n);;
+
+Scanf.scanf "%d %d" @@ fun k x ->
+    solve (x - k + 1) (x + k - 1);;
+
