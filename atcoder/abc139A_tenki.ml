c0a4ca07113e321e849c699c22e38e0a928f5b65
Vicfred <vicfred@vicfred.dev>
2020-07-02 20:19:33 -0500

    abc139A Tenki

---
+++ atcoder/abc139A_tenki.ml
@@ -0,0 +1,13 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc139/tasks/abc139_a
+ * implementation
+ * *)
+Scanf.scanf "%s\n%s\n" (fun s t ->
+    let ans = ref 0 in
+    for i = 0 to (String.length s - 1) do
+        if s.[i] = t.[i] then
+            ans := !ans + 1
+    done;
+    Printf.printf "%d\n" !ans
+)
+
