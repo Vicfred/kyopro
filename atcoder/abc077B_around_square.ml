9c58f18716d63e7a702d8e5066cffc7ed27b4874
Vicfred <vicfred@vicfred.dev>
2020-07-04 07:49:55 -0500

    abc077B Around Square

---
+++ atcoder/abc077B_around_square.ml
@@ -0,0 +1,10 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc077/tasks/abc077_b
+ * implementation, math
+ * *)
+Scanf.scanf "%f\n" (fun x -> x)
+|> sqrt
+|> int_of_float
+|> (fun x -> x * x)
+|> Printf.printf "%d\n"
+
