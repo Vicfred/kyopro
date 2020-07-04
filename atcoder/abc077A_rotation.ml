4d263180f071acf0b21c16e97301af41181bcb39
Vicfred <vicfred@vicfred.dev>
2020-07-04 07:33:04 -0500

    abc077A Rotation

---
+++ atcoder/abc077A_rotation.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc077/tasks/abc077_a
+ * implementation
+ * *)
+Scanf.scanf "%s\n%s\n" ( fun a b ->
+    if a.[0] = b.[2] && a.[1] = b.[1] && a.[2] = b.[0] then "YES"
+    else "NO"
+) |> Printf.printf "%s\n"
+
