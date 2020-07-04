9952b6b3c09728642fb824dd5eb88ae5d67a7309
Vicfred <vicfred@vicfred.dev>
2020-07-04 08:10:45 -0500

    abc077B Around Square (Binary Search)

---
+++ atcoder/abc077B_around_square_.ml
@@ -0,0 +1,15 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc077/tasks/abc077_b
+ * binary search
+ * *)
+let rec upper_bound l r p =
+    if r <= 1 + l then l
+    else let m = (l + r) / 2 in
+    if p m
+    then upper_bound m r p
+    else upper_bound l m p
+
+let main = Scanf.scanf "%d\n" @@ fun n ->
+    let m = upper_bound 0 (n + 1) @@ fun m -> m * m <= n in
+    Printf.printf "%d\n" @@ m * m
+
