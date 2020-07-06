f075d62024563acf2adee736d409a7f24ccb479e
Vicfred <vicfred@vicfred.dev>
2020-07-05 20:49:12 -0500

    abc136B Uneven Numbers

---
+++ atcoder/abc136B_uneven_numbers.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc136/tasks/abc136_b
+ * implementation
+ * *)
+Scanf.scanf "%d\n" @@ fun n ->
+    Printf.printf "%d\n" @@
+    Array.fold_left ( + ) 0 @@
+    Array.init n @@ fun i -> if (String.length @@ string_of_int @@ i + 1) mod 2 = 1 then 1 else 0
+
