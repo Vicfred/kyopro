9b5a0a2dea5ac1d51bc527b3c2bb5e8ff587cf46
Vicfred <vicfred@vicfred.dev>
2020-07-05 21:48:11 -0500

    abc135A Harmony

---
+++ atcoder/abc135A_harmony.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc135/tasks/abc135_a
+ * math
+ * *)
+Scanf.scanf "%d %d\n" (fun a b ->
+    if (a + b) mod 2 != 0 then "IMPOSSIBLE"
+    else string_of_int @@ (a + b) / 2
+) |> Printf.printf "%s\n"
+
