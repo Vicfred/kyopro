e1ba5027d1550a515b353916ccc4313782809c28
Vicfred <vicfred@vicfred.dev>
2020-07-06 01:30:45 -0500

    abc132A Fifty-Fifty

---
+++ atcoder/abc132A_fifty_fifty.ml
@@ -0,0 +1,11 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc132/tasks/abc132_a
+ * implementation
+ * *)
+Scanf.scanf "%c%c%c%c\n" @@ fun a b c d ->
+    print_endline @@
+    if a = b && c = d && a != c ||
+    a = c && b = d && a != b ||
+    a = d && b = c && a != b then "Yes"
+    else "No"
+
