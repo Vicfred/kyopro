cc9bc580b49ba65f6e62a8c9757b4e6b7a9600e9
Vicfred <vicfred@vicfred.dev>
2020-07-05 21:32:09 -0500

    abc136C Build Stairs

---
+++ atcoder/abc136C_build_stairs.ml
@@ -0,0 +1,14 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc136/tasks/abc136_c
+ * greedy
+ * *)
+let n = Scanf.scanf "%d\n" ( + ) 0
+let h = Array.init n @@ fun _ -> Scanf.scanf " %d" ( + ) 0
+let maxima = ref @@ h.(0) - 1
+let main =
+    for i = 1 to n - 1 do
+        if !maxima > h.(i) then ( print_endline "No"; exit 0);
+        maxima := h.(i) - if !maxima < h.(i) then 1 else 0
+    done;
+    print_endline "Yes"
+
