8b82f758a45653fc772de60887eb00af9a2a92ab
Vicfred <vicfred@vicfred.dev>
2020-07-06 03:28:07 -0500

    abc133B Good Distance

---
+++ atcoder/abc133B_good_distance.ml
@@ -0,0 +1,9 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc133/tasks/abc133_b
+ * math, implementation
+ * *)
+let ans, n, xss = Scanf.(scanf "%d %d" @@ fun n d -> ref 0, n, Array.(init n @@ fun _ -> init d @@ fun _ -> scanf " %d" ( + ) 0))
+let rec f i n = if i * i >= n then i * i  = n else f (i + 1) n
+let g n = n * n
+let main = Array.(iteri (fun i xs -> for j = i +  1 to n - 1 do if f 1 @@ fold_left ( + ) 0 @@ mapi (fun k x -> g (x - xss.(j).(k))) xs then incr ans done) xss); Printf.printf "%d\n" !ans
+
