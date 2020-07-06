7b9fd09b5a122b4e062dffd0200db31d4f683445
Vicfred <vicfred@vicfred.dev>
2020-07-06 01:45:52 -0500

    abc132B Ordinary Number

---
+++ atcoder/abc132B_ordinary_number.py
@@ -0,0 +1,13 @@
+# Vicfred
+# https://atcoder.jp/contests/abc132/tasks/abc132_b
+# implementation
+n = int(input())
+p = list(map(int,input().split()))
+
+answer = 0
+for i in range(1, n-1):
+    if p[i-1] < p[i] < p[i+1] or p[i-1] > p[i] > p[i+1]:
+        answer += 1
+
+print(answer)
+
