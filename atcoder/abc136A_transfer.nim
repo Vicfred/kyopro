a094f84fd6a7494ba5127caf4df4e3a10557976b
Unai Hernández Minaberry <unihernandez22@gmail.com>
2020-07-05 22:43:23 -0300

    136A transfer

---
+++ atcoder/abc136A_transfer.nim
@@ -0,0 +1,10 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc136/tasks/abc136_a
+# implementation
+
+import strutils, sequtils
+
+var a, b, c: int
+(a, b, c) = stdin.readLine.split.map parseInt
+
+echo max(0, c-a+b)
