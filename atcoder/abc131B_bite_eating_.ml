24ffba5ce75e6af0db79e4b09c6a3dbbdf05db47
Unai Hernández <unihernandez22@gmail.com>
2020-07-07 23:18:42 -0300

    131B bite eating

---
+++ atcoder/abc131B_bite_eating_.ml
@@ -0,0 +1,18 @@
+(* unihernandez22
+ * https://atcoder.jp/contests/abc131/tasks/abc131_b
+ * implementation
+ * *)
+
+let rec solution n l i cum eat =
+    if i > n then cum-eat
+    else
+        let curr = i+l-1 in
+        if abs curr < abs eat then
+            solution n l (i+1) (cum+curr) curr
+        else
+            solution n l (i+1) (cum+curr) eat;;
+
+Printf.printf "%d\n" @@
+Scanf.scanf "%d %d\n" @@ fun n l ->
+    solution n l 1 0 300;;
+
