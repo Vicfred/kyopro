0a0f9083ee3b9d41868891be4c28739c93071b3e
Vicfred <vicfred@vicfred.dev>
2020-07-12 20:27:43 -0500

    abc130A Rounding

---
+++ atcoder/abc130A_rounding.ml
@@ -0,0 +1,8 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc130/tasks/abc130_a
+ * implementation
+ * *)
+Scanf.scanf "%d %d\n" (fun x a ->
+    if x < a then 0 else 10
+) |> Printf.printf "%d\n"
+
