353debd3bcdd17c75c8a52ae124976546dfc0499
Vicfred <vicfred@vicfred.dev>
2020-07-12 21:22:05 -0500

    abc130D Enough Array

---
+++ atcoder/abc130D_enough_array.d
@@ -0,0 +1,32 @@
+// Vicfred
+// https://atcoder.jp/contests/abc130/tasks/abc130_d
+// two pointers, sliding window
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    long n, k;
+    readf("%d %d", &n, &k);
+    readln;
+    long[] as = readln.split.map!(to!long).array;
+
+    long ans = 0;
+    long cum = 0;
+
+    int r = 0;
+    for(int l = 0; l < n; l++) {
+        while(cum < k) {
+            if(r == n) break;
+            cum += as[r++];
+        }
+        if(cum < k) break;
+        ans += n - r + 1;
+        cum -= as[l];
+    }
+
+    ans.writeln;
+}
+
