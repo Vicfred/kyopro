66b07b160c250f07ea7f6bdb7f33e0bd6e826898
Vicfred <vicfred@vicfred.dev>
2020-07-12 23:58:40 -0500

    abc129B Balance

---
+++ atcoder/abc129B_balance.d
@@ -0,0 +1,29 @@
+// Vicfred
+// https://atcoder.jp/contests/abc129/tasks/abc129_b
+// brute force
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.math;
+import std.stdio;
+import std.string;
+
+void main() {
+    const long n = readln.chomp.to!long;
+    const long[] ws = readln.split.map!(to!long).array;
+
+    long[] sum = new long[n];
+    sum[0] = ws[0];
+    foreach(i; 1..n)
+        sum[i] += sum[i - 1] + ws[i];
+
+    long minima = long.max;
+    for(int i = 1; i < n; i++) {
+        const long left = sum[i - 1];
+        const long right = sum[n-1] - sum[i - 1];
+        minima = min(minima, abs(left - right));
+    }
+
+    minima.writeln;
+}
+
