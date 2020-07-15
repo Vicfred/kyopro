8f7dcd7a36e7353997ca937361c080c8ed589a62
Vicfred <vicfred@vicfred.dev>
2020-07-14 22:23:50 -0500

    abc128B Guidebook

---
+++ atcoder/abc128B_guidebook.ml
@@ -0,0 +1,8 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc128/tasks/abc128_b
+ * implementation
+ * *)
+let n = Scanf.scanf " %d" ( + ) 0
+let restaurants = Array.init n @@ fun i -> Scanf.scanf " %s %d" @@ fun a b -> a, -b, i + 1
+let main = Array.sort compare restaurants; Array.iter (fun (_, _, i) -> Printf.printf "%d\n" i) restaurants
+
