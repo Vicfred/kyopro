884b39b0d603142ed96d78ab6d56e457bee28a5f
Vicfred <vicfred@vicfred.dev>
2020-07-15 00:09:01 -0500

    abc127A Ferris Wheel

---
+++ atcoder/abc127A_ferris_wheel.ml
@@ -0,0 +1,8 @@
+(* Vicfred
+ * https://atcoder.jp/contests/abc127/tasks/abc127_a
+ * implementation
+ * *)
+Scanf.scanf "%d %d" (fun a b ->
+    if a >= 13 then b else if a >= 6 then b / 2 else 0
+) |> Printf.printf "%d\n"
+
