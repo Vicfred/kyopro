90ea12e80134f046c8e025c101b73f2fd1efd76b
Vicfred <vicfred@vicfred.dev>
2020-07-21 19:51:50 -0500

    abc043C Be Together

---
+++ atcoder/abc043C_be_together.d
@@ -0,0 +1,24 @@
+// Vicfred
+// https://atcoder.jp/contests/abc043/tasks/arc059_a
+// brute force
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int n = readln.chomp.to!int;
+    int[] a = readln.split.map!(to!int).array;
+
+    long minima = long.max;
+    for(int i = -100; i <= 100; i++) {
+        long cost = 0;
+        foreach(item; a)
+            cost += (item - i)^^2;
+        minima = min(minima, cost);
+    }
+
+    minima.writeln;
+}
+
