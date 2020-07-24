93dbe3a7280f818514b5cf27e4c234fc32a8c32d
Vicfred <vicfred@vicfred.dev>
2020-07-23 20:26:06 -0500

    abc055C Scc Puzzle

---
+++ atcoder/abc055C_scc_puzzle.d
@@ -0,0 +1,18 @@
+// Vicfred
+// https://atcoder.jp/contests/abc055/tasks/arc069_a
+// math
+import std.algorithm;
+import std.stdio;
+
+void main() {
+    long n, m;
+    readf("%s %s\n", &n, &m);
+
+    if(m < 2*n) {
+        min(n, m/2).writeln;
+        return;
+    }
+
+    ((2*n + m)/4).writeln;
+}
+
