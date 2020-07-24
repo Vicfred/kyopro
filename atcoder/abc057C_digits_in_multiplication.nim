34b32e0c73d05cf08135276f91689e55b6857109
Unai Hernández <unihernandez22@gmail.com>
2020-07-24 01:15:37 -0300

    057C Digits in Multiplication

---
+++ atcoder/abc057C_digits_in_multiplication.nim
@@ -0,0 +1,15 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc057/submissions/15385990
+# math
+
+from strutils import parseInt, intToStr
+from math import sqrt
+
+let n = stdin.readLine.parseInt
+
+var ans = int.high
+for i in 1 .. n.float.sqrt.int:
+  if n mod i == 0:
+    ans = min(ans, max(i.intToStr.len, intToStr(n div i).len))
+
+echo ans
