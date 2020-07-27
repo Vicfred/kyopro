2f828a1052577a455bd4f8c41eb01257a65b8816
Vicfred <vicfred@vicfred.dev>
2020-07-26 21:01:28 -0500

    cudoviste

---
+++ kattis/cudoviste.d
@@ -0,0 +1,37 @@
+// Vicfred
+// https://open.kattis.com/problems/cudoviste
+// brute force
+import std.stdio;
+import std.string;
+
+void main() {
+    int R, C;
+    readf("%s %s\n", &R, &C);
+
+    string[] parking;
+
+    for(int i = 0; i < R; i++) {
+        string S = readln.strip;
+        parking ~= S;
+    }
+
+    int[] ans = new int[5];
+    for(int i = 0; i < R - 1; i++) {
+        for(int j = 0; j < C - 1; j++) {
+            int[char] count;
+            count['.'] = 0;
+            count['X'] = 0;
+            count['#'] = 0;
+            count[parking[i][j]] += 1;
+            count[parking[i+1][j]] += 1;
+            count[parking[i][j+1]] += 1;
+            count[parking[i+1][j+1]] += 1;
+            if(count['.'] + count['X'] == 4)
+                ans[count['X']] += 1;
+        }
+    }
+
+    for(int i = 0; i < 5; i++)
+        ans[i].writeln;
+}
+
