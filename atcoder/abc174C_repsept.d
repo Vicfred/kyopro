6509b683ba73b4fea10461e119151c4ccbb3284a
Vicfred <vicfred@vicfred.dev>
2020-08-02 21:13:34 -0500

    abc174C Repsept

---
+++ atcoder/abc174C_repsept.d
@@ -0,0 +1,28 @@
+// Vicfred
+// https://atcoder.jp/contests/abc174/tasks/abc174_c
+// math
+
+import std.bigint;
+import std.stdio;
+
+void main() {
+    int k;
+    readf("%s\n", &k);
+
+    int kkk = 7;
+    int steps = 1;
+
+    while(kkk%k != 0) {
+        if(steps >= 10^^6+5) {
+            steps = -1;
+            break;
+        }
+
+        steps += 1;
+        kkk = kkk * 10 + 7;
+        kkk %= k;
+    }
+
+    steps.writeln;
+}
+
