84bc64d75949c7b9a0e4006c02aa7d78255d63aa
Vicfred <vicfred@vicfred.dev>
2020-08-04 19:49:53 -0500

    kattis candydivision

---
+++ kattis/candydivision.cpp
@@ -0,0 +1,28 @@
+// Vicfred
+// https://open.kattis.com/problems/candydivision
+// math
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    long long N;
+    cin >> N;
+
+    set<long> divisors;
+
+    divisors.insert(0);
+    for(long i = 1; i*i <= N; ++i) {
+        if(N%i == 0) {
+            divisors.insert(i-1);
+            divisors.insert(N/i-1);
+        }
+    }
+
+    for(long item: divisors) {
+        cout << item << " ";
+    } cout << endl;
+
+    return 0;
+}
