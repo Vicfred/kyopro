4821d341a2c03a7ce76d528e9ea9c3cba32f4ab3
Vicfred <vicfred@vicfred.dev>
2020-08-09 22:07:49 -0500

    atcoder dp_d knapsack (iterative, one parameter)

---
+++ atcoder/dp_d_knapsack_1__.d
@@ -0,0 +1,22 @@
+// Vicfred
+// https://atcoder.jp/contests/dp/tasks/dp_d
+// dynamic programming
+import std.algorithm;
+import std.stdio;
+
+void main() {
+    int N, W;
+    readf("%s %s\n", &N, &W);
+
+    long[] dp = new long[W+1];
+
+    long w, v;
+    foreach(_; 0..N) {
+        readf("%s %s\n", &w, &v);
+        for(int i = W; i >= w; --i)
+            dp[i] = max(dp[i], dp[i - w] + v);
+    }
+
+    dp[W].writeln;
+}
+
