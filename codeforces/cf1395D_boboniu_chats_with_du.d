653b740c952d1d58e26173a44fa4546682bb9322
Vicfred <vicfred@vicfred.dev>
2020-08-20 22:18:34 -0500

    codeforces 1395D Boboniu Chats with Du

--- codeforces/cf1395D_boboniu_chats_with_du.d
+++ codeforces/cf1395D_boboniu_chats_with_du.d
@@ -1,6 +1,6 @@
 // Vicfred
 // https://codeforces.com/contest/1395/problem/D
-// 
+// implementation, greedy, sorting
 import std.algorithm;
 import std.array;
 import std.conv;
@@ -14,21 +14,41 @@ void main() {
 
     long[] a = readln.split.map!(to!long).array;
 
-    a.sort;
-    a.writeln;
+    long[] good, bad;
 
-    long ans = long.min;
-    while(a.nextPermutation) {
-        //a.writeln;
-        int idx = 0;
-        long fun = 0;
-        while(idx < n) {
-            fun += a[idx];
-            if(a[idx] > m)
-                idx += d;
-            idx += 1;
-        }
-        ans = max(ans, fun);
+    foreach(item; a)
+        if(item > m)
+            good ~= item;
+        else
+            bad ~= item;
+
+    long x = good.length;
+    long y = bad.length;
+
+    good ~= 0;
+    bad ~= 0;
+
+    good.sort;
+    good.reverse;
+    bad.sort;
+    bad.reverse;
+
+    for(int i = 1; i <= x; i++)
+        good[i] += good[i-1];
+
+    for(int i = 1; i <= y; i++)
+        bad[i] += bad[i-1];
+
+    good = 0~good;
+    bad = 0~bad;
+
+    long ans = bad[$-1];
+    long remain = n - 1;
+    for(int i = 1; i <= x; i++) {
+        if(remain < 0)
+            break;
+        ans = max(ans, good[i] + bad[min(remain, y)]);
+        remain -= d + 1;
     }
 
     ans.writeln;
