874003045c94be22c7d26b9434de92cbc58dda04
Vicfred <vicfred@vicfred.dev>
2020-08-23 21:17:12 -0500

    abc176B Multiple of 9

---
+++ atcoder/abc176B_multiple_of_9.d
@@ -0,0 +1,21 @@
+// Vicfred
+// https://atcoder.jp/contests/abc176/tasks/abc176_b
+// implementation
+import std.stdio;
+import std.string;
+
+void main() {
+    string n = readln.strip;
+
+    int sum = 0;
+    foreach(ch; n) {
+        sum += ch - '0';
+        sum %= 9;
+    }
+
+    if(sum == 0)
+        "Yes".writeln;
+    else
+        "No".writeln;
+}
+
