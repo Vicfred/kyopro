9e971f0b228c16a7e23192144f8249f4473638c6
Unai Hernández <unihernandez22@gmail.com>
2020-08-28 00:40:25 -0300

    1401A

---
+++ codeforces/cf1401A_distance_and_axis.d
@@ -0,0 +1,21 @@
+// unihernandez22
+// https://codeforces.com/contest/1401/problem/A
+// implementation
+
+import std.stdio;
+
+void main() {
+  int n, k, t;
+  scanf("%d", &t);
+  foreach (_; 0 .. t) {
+    scanf("%d %d", &n, &k);
+    if (k <= n) {
+      if (n%2 == k%2) {
+        writeln("0");
+      } else {
+        writeln("1");
+      }
+    } else
+      writeln(k - n);
+  }
+}
