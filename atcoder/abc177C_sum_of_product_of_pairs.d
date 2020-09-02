aaed8e96f6846dd8f1ce3fdef05e228184809543
Vicfred <vicfred@vicfred.dev>
2020-09-01 20:49:06 -0500

    abc177C Sum of Product of Pairs

---
+++ atcoder/abc177C_sum_of_product_of_pairs.d
@@ -0,0 +1,33 @@
+// Vicfred
+// https://atcoder.jp/contests/abc177/tasks/abc177_c
+// math
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    const long mod = 10^^9+7;
+    long n = readln.chomp.to!long;
+    long[] a = readln.split.map!(to!long).array;
+
+    long sum = 0;
+    for(long i = 0; i < n; i++) {
+        sum += a[i];
+        sum %= mod;
+    }
+
+
+    long ans = 0;
+    for(int i = 0; i < n; i++) {
+        sum -= a[i];
+        if(sum < 0) sum += mod;
+
+        ans += a[i] * sum;
+        ans %= mod;
+    }
+
+    ans.writeln;
+}
+
