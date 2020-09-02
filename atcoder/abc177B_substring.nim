351c99709c0efeea61e2e0412850eef2673da920
Unai Hernández <unihernandez22@gmail.com>
2020-09-01 23:27:30 -0300

    177B Substring

---
+++ atcoder/abc177B_substring.nim
@@ -0,0 +1,22 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc177/tasks/abc177_b
+# implementation
+
+import strutils
+
+let
+  s = stdin.readLine.strip
+  t = stdin.readLine.strip
+
+var
+  diff: int
+  ans = int.high
+
+for i in 0 .. s.len - t.len:
+  diff = 0
+  for j in 0 ..< t.len:
+    if s[i+j] != t[j]:
+      inc diff
+  ans = min(ans, diff)
+
+ans.echo
