47a282634e58ccdcff006cc2cbbfbab81419a5a6
Vicfred <vicfred@vicfred.dev>
2020-09-01 20:23:23 -0500

    abc177B Substring

---
+++ atcoder/abc177B_substring.d
@@ -0,0 +1,26 @@
+// Vicfred
+// https://atcoder.jp/contests/abc177/tasks/abc177_b
+// implementation, string manipulation
+import std.algorithm;
+import std.stdio;
+import std.string;
+
+void main() {
+    string s = readln.strip;
+    string t = readln.strip;
+
+    long minima = t.length;
+
+    for(int i = 0; i + t.length <= s.length; ++i) {
+        long mismatches = 0;
+
+        for(int j = 0; j < t.length; ++j)
+            if(t[j] != s[i + j])
+                mismatches += 1;
+
+        minima = min(minima, mismatches);
+    }
+
+    minima.writeln;
+}
+
