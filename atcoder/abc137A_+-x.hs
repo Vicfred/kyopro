274a089ed74f96973c04a05563c6c9fdc8c45086
Unai Hernández <unihernandez22@gmail.com>
2020-09-03 14:01:41 -0300

    abc137A_+-x.hs

---
+++ atcoder/abc137A_+-x.hs
@@ -0,0 +1,9 @@
+-- unihernandez22
+-- https://atcoder.jp/contests/abc137/tasks/abc137_a
+-- implementation
+
+main = do
+  li <- getLine
+  let a:b:[] = map read $ words li
+  print $
+    max (a + b) $ max (a - b) (a * b)
