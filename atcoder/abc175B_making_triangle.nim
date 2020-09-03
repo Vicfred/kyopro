817a7d353a777413d440ea13f8cde896b067619c
Unai Hernández <unihernandez22@gmail.com>
2020-09-03 01:45:38 -0300

    That was a wrong solution

--- atcoder/abc175B_making_triangle.nim
+++ atcoder/abc175B_making_triangle.nim
@@ -2,10 +2,10 @@
 # https://atcoder.jp/contests/abc175/tasks/abc175_b
 # brute force
 
-import strutils, sequtils, algorithm, sets
+import strutils, sequtils, algorithm
 
 discard stdin.readLine
-var l = stdin.readLine.split.map(parseInt).toSet.toSeq
+var l = stdin.readLine.split.map(parseInt)
 
 proc cmp2(x, y: int): int = -cmp(x, y)
 
@@ -15,8 +15,8 @@ l.sort cmp2
 var ans = 0
 for x in 0 ..< l.len:
   for y in x+1 ..< l.len:
-    for z in x+1 ..< l.len:
-      if y == z: continue
-      if l[y] + l[z] > l[x]: inc ans
+    for z in y+1 ..< l.len:
+      if l[y] + l[z] > l[x] and l[x] != l[y] and l[x] != l[z] and l[y] != l[z]:
+        inc ans
 
 ans.echo
