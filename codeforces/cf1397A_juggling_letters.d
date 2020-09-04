23ac0873b1b7e626920e857055c065e6d50d0b59
Vicfred <vicfred@vicfred.dev>
2020-09-03 20:19:11 -0500

    cf1397A Juggling Letters

---
+++ codeforces/cf1397A_juggling_letters.d
@@ -0,0 +1,38 @@
+// Vicfred
+// https://codeforces.com/contest/1397/problem/A
+// simulation, math
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    long t = readln.chomp.to!long;
+
+    while(t--) {
+        long[char] counter;
+
+        long n = readln.chomp.to!long;
+        string[] s;
+        foreach(_; 0..n)
+            s ~= readln.strip;
+        foreach(str; s) {
+            foreach(ch; str) {
+                if(ch in counter)
+                    counter[ch] += 1;
+                else
+                    counter[ch] = 1;
+            }
+        }
+
+        string ans = "YES";
+        foreach(value; counter.values) {
+            if(value%n != 0) {
+                ans = "NO";
+                break;
+            }
+        }
+        ans.writeln;
+    }
+
+}
+
