40afd9bc24e680cbe512e224a909c2d9a6e44a70
Vicfred <vicfred@vicfred.dev>
2020-09-06 20:28:16 -0500

    cf1405A Permutation Forgery

---
+++ codeforces/cf1405A_permutation_forgery.d
@@ -0,0 +1,22 @@
+// Vicfred
+// https://codeforces.com/contest/1405/problem/A
+// math
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int t = readln.chomp.to!int;
+
+    while(t--) {
+        int n = readln.chomp.to!int;
+        int[] p = readln.split.map!(to!int).array;
+        p.reverse;
+        foreach(x; p)
+            writef("%s ", x);
+        "".writeln;
+    }
+}
+
