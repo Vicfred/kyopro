66e71e4fd115557e11445a27ec4e400197df777d
Vicfred <vicfred@vicfred.dev>
2020-09-13 21:31:10 -0500

    abc178B Product

---
+++ atcoder/abc178B_product_max.lua
@@ -0,0 +1,13 @@
+-- Vicfred
+-- https://atcoder.jp/contests/abc178/tasks/abc178_b
+-- math
+
+a, b, c, d = io.read("*n", "*n", "*n", "*n")
+
+maxima = c*b
+maxima = math.max(maxima, d*b)
+maxima = math.max(maxima, c*a)
+maxima = math.max(maxima, a*d)
+
+print(maxima)
+
