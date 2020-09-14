3717534c8bb4fd1fb464cd32990a9d50f232e151
Vicfred <vicfred@vicfred.dev>
2020-09-14 00:17:29 -0500

    abc178D Redistribution

---
+++ atcoder/abc178D_redistribution.d
@@ -0,0 +1,28 @@
+// Vicfred
+// https://atcoder.jp/contests/abc178/tasks/abc178_d
+// math
+import std.stdio;
+
+void main() {
+    const int mod = 10^^9+7;
+    int n;
+    readf("%s", &n);
+
+    int[] dp = new int[n+1];
+
+    dp[0] = 1;
+
+    for(int i = 3; i <= n; i++)
+        dp[i] = 1;
+
+    for(int i = 3; i <= n; i++) {
+        for(int j = 3; j <= i-3; j++) {
+            dp[i] += dp[j];
+            dp[i] %= mod;
+        }
+        dp[i] %= mod;
+    }
+
+    dp[n].writeln;
+}
+
