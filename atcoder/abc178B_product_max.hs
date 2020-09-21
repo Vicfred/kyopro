9ea4f4dc3420d272e8ec3ac46e5a227c450e1fbc
Unai Hernández <unihernandez22@gmail.com>
2020-09-20 23:37:47 -0300

    atcoder/abc178B_product_max.hs

---
+++ atcoder/abc178B_product_max.hs
@@ -0,0 +1,9 @@
+-- unihernandez22
+-- https://atcoder.jp/contests/abc178/tasks/abc178_b
+-- math
+
+main = do
+  li <- getLine
+  let a:b:c:d:[] = map read $ words li :: [Integer]
+  print $ foldl max (-10000000000000000001) [a * c, a * d, b * c, b * d]
+
