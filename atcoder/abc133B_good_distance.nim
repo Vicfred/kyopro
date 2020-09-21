88c1385e6c866a852be9ac249414a52742974418
Unai Hernández <unihernandez22@gmail.com>
2020-09-20 23:37:47 -0300

    atcoder/abc133B_good_distance.nim

---
+++ atcoder/abc133B_good_distance.nim
@@ -0,0 +1,31 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc133/tasks/abc133_b
+# brute force
+
+import strutils, sequtils
+from math import `^`, sqrt
+
+func getDistance(a, b: seq[int]): float =
+  for i in 0 ..< a.len:
+    result += float(abs(a[i] - b[i]) ^ 2)
+  result = sqrt(result)
+
+var n, d: int
+(n, d) = stdin.readLine.split.map(parseInt)
+
+var
+  x = newSeq[seq[int]](n)
+  ans = 0
+  distance: float
+  
+
+for i in 0 ..< n:
+  x[i] = stdin.readLine.split.map(parseInt)
+
+for i in 0 ..< n:
+  for j in i+1 ..< n:
+    distance = x[i].getDistance(x[j])
+    if distance == distance.int.float:
+      inc ans
+
+ans.echo
