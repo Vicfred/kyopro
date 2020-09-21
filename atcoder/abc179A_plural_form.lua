c859d0f4ecbe0c94b3554476efa694294e035396
Vicfred <vicfred@vicfred.dev>
2020-09-20 20:30:04 -0500

    abc179A Plural Form

---
+++ atcoder/abc179A_plural_form.lua
@@ -0,0 +1,11 @@
+-- Vicfred
+-- https://atcoder.jp/contests/abc179/tasks/abc179_a
+-- string manipulation
+
+s = io.read()
+if s:sub(#s, #s) == "s" then
+    print(s .. "es")
+else
+    print(s .. "s")
+end
+
