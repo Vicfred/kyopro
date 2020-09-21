6a6057e14c5ff28a8ba1f119fe3bac57fb78412a
Unai Hernández <unihernandez22@gmail.com>
2020-09-20 23:37:47 -0300

    atcoder/abc129B_balance.nim

---
+++ atcoder/abc129B_balance.nim
@@ -0,0 +1,21 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc129/tasks/abc129_b
+# implementation
+
+import strutils, sequtils
+from math import sum
+
+let
+  n = stdin.readLine.parseInt
+  w = stdin.readLine.split.map(parseInt)
+  total_sum = w.sum
+
+var
+  partial_sum = 0
+  ans = int.high
+
+for i in 0 ..< n-1:
+  partial_sum += w[i]
+  ans = min(ans, abs(partial_sum - total_sum + partial_sum))
+
+ans.echo
