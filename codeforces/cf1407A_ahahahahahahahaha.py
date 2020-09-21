e6099b5603441e969fe3e57bcdcab2ae67818f3e
Unai Hernández <unihernandez22@gmail.com>
2020-09-20 23:37:47 -0300

    codeforces/cf1407A_ahahahahahahahaha.py

---
+++ codeforces/cf1407A_ahahahahahahahaha.py
@@ -0,0 +1,19 @@
+# unihernandez22
+# https://codeforces.com/contest/1407/problem/A
+# greedy
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    if a.count(1) <= n//2:
+        print(a.count(0))
+        print(*[i for i in a if i == 0])
+    else:
+        if sum(a) % 2 == 0:
+            print(a.count(1))
+            print(*[i for i in a if i == 1])
+        else:
+            print(a.count(1)-1)
+            print(*([i for i in a if i == 1][:-1]))
