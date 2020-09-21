72efc73dd3845d73d9c4c68b6f9eb4f1c1e13d04
Unai Hernández <unihernandez22@gmail.com>
2020-09-21 00:16:44 -0300

    abc179C

---
+++ atcoder/abc179C_axb+c.py
@@ -0,0 +1,11 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc179/tasks/abc179_c
+# math, combinatorics
+
+n = int(input())
+
+ans = 0
+for i in range(1, n):
+    ans += (n-1)//i
+
+print(ans)
