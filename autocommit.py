4393a43753b0331b82b1a4d304f9bb663d976405
Unai Hernández <unihernandez22@gmail.com>
2020-09-20 23:41:34 -0300

    RM autocommit.py

--- autocommit.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from os import system as cmd
-
-while input() != '  (use "git add <file>..." to include in what will be committed)': pass
-
-while (i := input()) != '':
-    cmd(f'git add {i.strip()}')
-    cmd(f'git commit -m \'{i.strip()}\'')
