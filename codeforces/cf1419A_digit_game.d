c21767e145430b7f0d997e0209d90f512a8170ea
Vicfred <vicfred@vicfred.dev>
2020-09-22 21:35:09 -0500

    cf1417A Digit Game

---
+++ codeforces/cf1419A_digit_game.d
@@ -0,0 +1,45 @@
+// Vicfred
+// https://codeforces.com/contest/1419/problem/A
+// greedy
+import std.stdio;
+
+void main() {
+    int t;
+    readf("%s\n", &t);
+
+    int n;
+    string s;
+    while(t--) {
+        readf("%s\n", &n);
+        readf("%s\n", &s);
+
+        int even, odd;
+        even = 0;
+        odd = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 == 1) {
+                odd += (s[i - 1] - '0') % 2;
+            } else {
+                even += ((s[i - 1] - '0') + 1) % 2;
+            }
+        }
+
+        int winner;
+        if(n % 2 == 1) {
+            if(odd > 0)
+                winner = 1;
+            else
+                winner = 2;
+            winner.writeln;
+            continue;
+        }
+
+        if(even > 0)
+            winner = 2;
+        else
+            winner = 1;
+        winner.writeln;
+    }
+}
+
