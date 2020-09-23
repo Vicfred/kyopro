fae5b9db09cb841502595fbfc66dd658cb53078a
Vicfred <vicfred@vicfred.dev>
2020-09-22 22:34:30 -0500

    codeforces 1419D1 Sage's Birthday Easy Version

---
+++ codeforces/cf1419D1_sages_birthday_easy_version.d
@@ -0,0 +1,42 @@
+// Vicfred
+// https://codeforces.com/contest/1419/problem/D1
+// greedy
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.container;
+import std.stdio;
+import std.string;
+
+void main() {
+    long n = readln.chomp.to!long;
+
+    long[] a = readln.split.map!(to!long).array;
+    a.sort;
+
+    auto dlist = DList!long(a);
+
+    long[] solution;
+
+    for(int i = 0; i < n; i++) {
+        if(i % 2 == 0) {
+            solution ~= dlist.back;
+            dlist.removeBack;
+        } else {
+            solution ~= dlist.front;
+            dlist.removeFront;
+        }
+    }
+
+    int ans = 0;
+    for(int i = 1; i + 1 < n; i++) {
+        if(solution[i] < solution[i - 1] &&
+           solution[i] < solution[i + 1])
+            ans += 1;
+    }
+
+    ans.writeln;
+    foreach(item; solution)
+        writef("%s ", item);
+    "".writeln;
+}
