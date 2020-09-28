995742cf10abdf8bc57c7d17b879903d2fb7526d
Vicfred <vicfred@vicfred.dev>
2020-09-27 20:33:28 -0500

    ablA Repeat ACL

---
+++ atcoder/ablA_repeat_acl.nim
@@ -0,0 +1,13 @@
+# Vicfred
+# https://atcoder.jp/contests/abl/tasks/abl_a
+# basic string manipulation
+import strutils
+
+let k = stdin.readline.parseInt
+
+var ans = ""
+for _ in 1..k:
+    ans.add("ACL")
+
+echo ans
+
