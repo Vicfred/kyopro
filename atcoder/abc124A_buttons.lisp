151ab6191c48b236f297faf06b137dbb476502ec
Unai Hernández <unihernandez22@gmail.com>
2020-09-29 01:27:31 -0300

    First lisp solution

---
+++ atcoder/abc124A_buttons.lisp
@@ -0,0 +1,8 @@
+; unihernandez22
+; https://atcoder.jp/contests/abc124/tasks/abc124_a
+; implementation
+
+(let ((a (read))
+      (b (read)))
+
+(write (+ (max a b) (max (- (max a b) 1) (min a b)))))
