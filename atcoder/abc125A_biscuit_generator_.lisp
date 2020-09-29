b5df543b4fa9610053a98fc183221a315d2fbd9f
Unai Hernández <unihernandez22@gmail.com>
2020-09-29 15:14:13 -0300

    abc125A

---
+++ atcoder/abc125A_biscuit_generator_.lisp
@@ -0,0 +1,15 @@
+; unihernandez22
+; https://atcoder.jp/contests/abc125/tasks/abc125_a
+; math
+
+(
+
+let ((a (read))
+     (b (read))
+     (c (read)))
+
+(write (
+  * b (values (floor c a))
+))
+
+)
