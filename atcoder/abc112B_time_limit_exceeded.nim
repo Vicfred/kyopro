68d5f1866b8a01f19730b36dbf01245310f86b2c
Unai Hernández <unihernandez22@gmail.com>
2020-09-30 15:29:00 -0300

    atcoder/abc112B_time_limit_exceeded.nim

---
+++ atcoder/abc112B_time_limit_exceeded.nim
@@ -0,0 +1,21 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc112/tasks/abc112_b
+# implementation
+
+import strutils, sequtils
+
+proc readintseq: seq[int] = stdin.readLine.split.map parseInt
+
+var N, T, c, t: int
+(N, T) = readintseq()
+
+var ans = -1
+for i in 0 ..< N:
+  (c, t) = readintseq()
+  if t <= T and (ans == -1 or c < ans):
+    ans = c
+
+if ans == -1:
+  echo "TLE"
+else:
+  echo ans
