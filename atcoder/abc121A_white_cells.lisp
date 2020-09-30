19b15104472d6b7b15afb42583389ca6e2689d91
Unai Hernández <unihernandez22@gmail.com>
2020-09-30 15:29:00 -0300

    atcoder/abc121A_white_cells.lisp

---
+++ atcoder/abc121A_white_cells.lisp
@@ -0,0 +1,18 @@
+; unihernandez22
+; https://atcoder.jp/contests/abc121/tasks/abc121_a
+; math
+
+(
+
+let (
+  (h (read))
+  (w (read))
+  (a (read))
+  (b (read))
+)
+
+(write (
+  * (- h a) (- w b)
+))
+
+)
