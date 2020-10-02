4886b9f2aa6f095d1b5f05a46e0af1145056a9c5
Vicfred <vicfred@vicfred.dev>
2020-10-01 20:45:29 -0500

    cf1426A Floor Number

---
+++ codeforces/cf1426A_floor_number.d
@@ -0,0 +1,21 @@
+// Vicfred
+// https://codeforces.com/contest/1426/problem/A
+// math
+import std.stdio;
+
+void main() {
+    int t;
+    readf("%s\n", &t);
+
+    int n, x;
+    while(t--) {
+        readf("%s %s\n", &n, &x);
+        if(n <= 2) {
+            "1".writeln;
+            continue;
+        }
+        int ans = (n - 3)/x + 2;
+        ans.writeln;
+    }
+}
+
