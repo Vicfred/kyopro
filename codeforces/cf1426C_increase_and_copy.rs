ce392d3b2482471b498fb031c85ccf24c7b88c4d
Unai Hernández <unihernandez22@gmail.com>
2020-10-02 00:17:02 -0300

    cf1426C

---
+++ codeforces/cf1426C_increase_and_copy.rs
@@ -0,0 +1,28 @@
+// Vicfred && unihernandez22
+// https://codeforces.com/contest/1426/problem/C
+// greedy
+
+use std::io::stdin;
+use std::cmp::min;
+
+fn readint() -> i64 {
+    let mut line = String::new();
+    stdin().read_line(&mut line).unwrap();
+    return line.trim().parse().unwrap();
+}
+
+fn main() {
+    let t = readint();
+    
+    for _ in 0 .. t {
+        let n = readint();
+
+        let mut minima = std::i64::MAX;
+        let mut i = 1;
+        while i * i <= n {
+            minima = min(i + (n - i - 1) / i, minima);
+            i += 1;
+        }
+        println!("{}", minima);
+    }
+}
