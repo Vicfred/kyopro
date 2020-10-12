624ba50878fe29c836000c62e75aca6d6e806f2d
Vicfred <vicfred@vicfred.dev>
2020-10-11 21:16:23 -0500

    hhkb2020C Neq Min

---
+++ atcoder/hhkb2020C_neq_min.d
@@ -0,0 +1,24 @@
+// Vicfred
+// https://atcoder.jp/contests/hhkb2020/tasks/hhkb2020_c
+// data structures
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.container;
+import std.range;
+import std.stdio;
+import std.string;
+
+void main() {
+    int maxn = 200001;
+    auto rbt = redBlackTree(iota(0, maxn));
+
+    int n = readln.chomp.to!int;
+    int[] p = readln.split.map!(to!int).array;
+
+    foreach(item; p) {
+        rbt.removeKey(item);
+        rbt.front().writeln;
+    }
+}
+
