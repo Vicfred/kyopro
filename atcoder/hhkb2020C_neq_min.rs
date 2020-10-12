15a9cbb08ca1717436f3b28a2f4ee06cf722f6fd
Unai Hernández <unihernandez22@gmail.com>
2020-10-12 01:02:05 -0300

    hhkb2020C

---
+++ atcoder/hhkb2020C_neq_min.rs
@@ -0,0 +1,26 @@
+use std::io::stdin;
+use std::collections::BTreeSet;
+
+fn main() {
+    let mut line = String::new();
+    stdin().read_line(&mut line).unwrap();
+    let n: i64 = line.trim().parse().unwrap();
+
+    line = String::new();
+    stdin().read_line(&mut line).unwrap();
+    let p: Vec<i64> = line
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let mut bts = BTreeSet::new();
+    
+    for i in 0 .. n+1 {
+        bts.insert(i);
+    }
+
+    for i in &p {
+        bts.remove(i);
+        println!("{}", bts.iter().next().unwrap());
+    }
+}
