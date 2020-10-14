c2c9368ea15f7ce532321a9802bdac25d0f41046
Vicfred <vicfred@vicfred.dev>
2020-10-13 20:57:28 -0500

    cf1430C Numbers on Whiteboard

---
+++ codeforces/cf1430C_numbers_on_whiteboard.d
@@ -0,0 +1,26 @@
+// Vicfred
+// https://codeforces.com/contest/1430/problem/C
+// math, greedy
+import std.stdio;
+
+void main() {
+    int t;
+    readf("%s\n", &t);
+
+    int n;
+    while(t--) {
+        readf("%s\n", &n);
+
+        "2".writeln;
+
+        int x = n - 2;
+        int y = n;
+        writefln("%s %s", n - 1, n);
+
+        for(int i = 0; i < n - 2; ++i) {
+            writefln("%s %s", x, y);
+            x -= 1;
+            y -= 1;
+        }
+    }
+}
