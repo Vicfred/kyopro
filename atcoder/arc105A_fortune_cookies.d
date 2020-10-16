8ad8c29a4083442678ba8b373f5d2ed6c6b08d81
Vicfred <vicfred@vicfred.dev>
2020-10-15 19:51:18 -0500

    arc105A Fortune Cookies

---
+++ atcoder/arc105A_fortune_cookies.d
@@ -0,0 +1,20 @@
+// Vicfred
+// https://atcoder.jp/contests/arc105/tasks/arc105_a
+// greedy
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int[] a = readln.split.map!(to!int).array;
+    a.sort;
+
+    if(a[3] == a[0] + a[1] + a[2] ||
+       a[0] + a[3] == a[1] + a[2])
+        "Yes".writeln;
+    else
+        "No".writeln;
+}
+
