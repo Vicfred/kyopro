128690fa9ce4cfdd42f04a57255d59f831fa155e
Unai Hernández <unihernandez22@gmail.com>
2020-10-18 23:43:43 -0300

    abc180D

---
+++ atcoder/abc180D.py
@@ -0,0 +1,13 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc180/tasks/abc180_d
+# greedy
+
+x, y, a, b = map(int, input().split())
+
+EXP = 0
+
+while x*a < y and x*a < x+b:
+    x *= a
+    EXP -=- 1
+
+print(EXP + (y-x-1)//b)
