144d2e1b101316ec32a69ba05855155f00565889
Vicfred <vicfred@vicfred.dev>
2020-10-18 20:38:45 -0500

    abc180B Various Distances

---
+++ atcoder/abc180B_various_distances.d
@@ -0,0 +1,32 @@
+// Vicfred
+// https://atcoder.jp/contests/abc180/tasks/abc180_b
+// math
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.math;
+import std.stdio;
+import std.string;
+
+void main() {
+    long n = readln.chomp.to!long;
+    long[] X = readln.split.map!(to!long).array;
+
+    long manhattan = 0L;
+    long chebyshev = -1;
+    real euclidean = 0.0L;
+
+    foreach(x; X) {
+        long y = abs(x);
+        manhattan += y;
+        euclidean += y*y;
+        chebyshev = max(chebyshev, y);
+    }
+
+    euclidean = sqrt(euclidean);
+
+    manhattan.writeln;
+    writefln("%.12s", euclidean);
+    chebyshev.writeln;
+}
+
