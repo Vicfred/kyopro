18f0251cf165ee210f0ca72fe5c2cd33a18af90b
Vicfred <vicfred@vicfred.dev>
2020-10-18 21:39:22 -0500

    abc180D Takahashi Unevolved

---
+++ atcoder/abc180D_takahashi_unevolved.d
@@ -0,0 +1,18 @@
+// Vicfred
+// https://atcoder.jp/contests/abc180/tasks/abc180_d
+// greedy
+import std.stdio;
+
+void main() {
+    long x, y, a, b;
+    readf("%s %s %s %s\n", &x, &y, &a, &b);
+
+    long ans = 0L;
+    while(x < y/a && x < (x + b)/a) {
+        x *= a;
+        ans += 1L;
+    }
+
+    (ans + (y - x - 1L)/b).writeln;
+}
+
