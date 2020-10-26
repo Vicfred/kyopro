bd6a0f43932eea5ae82567004edc2f0c9ec7b2be
Unai Hernández <unihernandez22@gmail.com>
2020-10-26 00:42:17 -0300

    arc106A

---
+++ atcoder/arc106A_106.nim
@@ -0,0 +1,15 @@
+# unihernandez22
+# https://atcoder.jp/contests/arc106/tasks/arc106_a
+# math
+
+import strutils, math
+
+let n = stdin.readLine.parseInt
+
+for a in 1 .. 37:
+  for b in 1 .. 25:
+    if 3^a + 5^b == n:
+      echo a, ' ', b
+      quit()
+
+echo -1
