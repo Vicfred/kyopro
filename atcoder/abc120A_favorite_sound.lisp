07011f5b966840436ee44e4985496bd2e4a83657
Unai Hernández <unihernandez22@gmail.com>
2020-10-31 15:48:38 -0300

    abc120A

---
+++ atcoder/abc120A_favorite_sound.lisp
@@ -0,0 +1,15 @@
+;; unihernandez22
+;; https://atcoder.jp/contests/abc120/tasks/abc120_a
+;; implementation
+
+(
+  let (
+    (a (read))
+    (b (read))
+    (c (read))
+  )
+
+  (write (
+    min (/ (- b (mod b a)) a) c
+  ))
+)
