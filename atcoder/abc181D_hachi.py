2a8e06702f798d4ad6eb10ceb4f25b86f2a2f50d
Unai Hernández <unihernandez22@gmail.com>
2020-11-02 01:16:04 -0300

    abc181D

---
+++ atcoder/abc181D_hachi.py
@@ -0,0 +1,26 @@
+# unihernandez22
+# https://atcoder.jp/contests/abc181/tasks/abc181_d
+# greedy, string manipulation, math
+
+from collections import Counter
+
+inp = input()
+s = Counter(inp)
+
+if len(inp) < 3:
+    for i in range(0, 100, 8):
+        if Counter(str(i)) == Counter(inp):
+            print("Yes")
+            quit()
+
+for i in range(0, 1000, 8):
+    a = Counter('0'*(3-len(str(i)))+str(i))
+    found = True
+    for j in a:
+        if a[j] > s[j]:
+            found = False
+    if found:
+        print("Yes")
+        quit()
+
+print("No")
