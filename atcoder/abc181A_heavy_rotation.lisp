e9f3d4947322d90b6263ec69ac977766e290bb46
Unai Hernández <unihernandez22@gmail.com>
2020-11-01 23:26:25 -0300

    abc181A

---
+++ atcoder/abc181A_heavy_rotation.lisp
@@ -0,0 +1,18 @@
+;; unihernandez22
+;; https://atcoder.jp/contests/abc181/tasks/abc181_a
+;; implementation
+
+(
+
+let (
+  (n (read))
+)
+
+(write-line
+  (cond
+    ((= (mod n 2) 0) "White")
+    (t "Black")
+  )
+)
+
+)
