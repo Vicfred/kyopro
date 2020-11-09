8181c655a82bde896b94e26d8c3c89bb3e0e8164
Vicfred <vicfred@vicfred.dev>
2020-11-08 20:53:23 -0600

    abc182B Almost GCD

---
+++ atcoder/abc182B_almost_gcd.nim
@@ -0,0 +1,29 @@
+# Vicfred & unaihernandez22
+# https://atcoder.jp/contests/abc182/tasks/abc182_b
+# math, data structures
+import strutils, sequtils, math, tables
+
+let
+  n = stdin.readLine.parseInt
+  a = stdin.readLine.split.map parseInt
+
+var divisors = initCountTable[int]()
+
+for x in a:
+  for d in 2..x:
+    if x mod d == 0:
+      divisors.inc(d)
+
+var maxima = -1
+
+for item in divisors.values:
+  maxima = max(maxima, item)
+
+var ans = 1
+
+for item in divisors.keys:
+  if divisors[item] == maxima:
+    ans = item
+
+echo ans
+
