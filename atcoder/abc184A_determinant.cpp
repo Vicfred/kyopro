18f7753cffcbaf7cc3a251b4bb0084355a6f57fa
Unai Hernández <unihernandez22@gmail.com>
2020-12-15 22:49:01 -0300

    abc184A

---
+++ atcoder/abc184A_determinant.cpp
@@ -0,0 +1,14 @@
+// unihernandez22
+// https://atcoder.jp/contests/abc184/tasks/abc184_a
+// implementation
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  cout << a*d - b*c << endl;
+}
