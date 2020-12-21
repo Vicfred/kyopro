c540fd0e297e3fd6c33ae7987f8480c4b7d5351e
Unai Hernández <unihernandez22@gmail.com>
2020-12-21 00:32:45 -0300

    abc143C

---
+++ atcoder/abc143C_slimes.d
@@ -0,0 +1,26 @@
+// unihernandez22
+// https://atcoder.jp/contests/abc143/tasks/abc143_c
+// implementation
+
+import std.stdio;
+
+void main() {
+  int n;
+  "%d\n".readf(&n);
+
+  char c, last;
+  "%c".readf(&c);
+  last = c;
+
+  int ans = 1;
+  for(int i = 1; i < n; ++i) {
+    "%c".readf(&c);
+
+    if(c != last)
+      ans++;
+
+    last = c;
+  }
+
+  ans.writeln;
+}
