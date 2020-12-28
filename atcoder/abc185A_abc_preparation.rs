88638f47899bc71d0e7f14c7cd9d5cc0ccc123bb
Vicfred <git@vicfred.dev>
2020-12-27 20:53:28 -0600

    abc185A ABC Preparation

---
+++ atcoder/abc185A_abc_preparation.rs
@@ -0,0 +1,24 @@
+// Vicfred
+// https://atcoder.jp/contests/abc185/tasks/abc185_a
+// implementation
+use std::io;
+use std::cmp::min;
+
+fn main() {
+    let mut line = String::new();
+
+    io::stdin()
+        .read_line(&mut line)
+        .unwrap();
+
+    let a: Vec<i64> =
+        line
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let ans = min(min(a[0], a[1]), min(a[2], a[3]));
+
+    println!("{}", ans);
+}
+
