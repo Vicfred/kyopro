aac4ccac398ac25eb9f887e1c716e72ab6c3e542
Vicfred <git@vicfred.dev>
2020-12-27 21:24:45 -0600

    abc185B Smartphone Addiction

---
+++ atcoder/abc185B_smartphone_addiction.d
@@ -0,0 +1,37 @@
+// Vicfred
+// https://atcoder.jp/contests/abc185/tasks/abc185_b
+// simulation
+import std.stdio;
+import std.algorithm;
+
+void main() {
+    int n, m, t;
+    readf("%s %s %s\n", &n, &m, &t);
+
+    int battery = n;
+
+    int s = 0;
+
+    int a, b;
+    foreach(i; 0..m) {
+        readf("%s %s\n", &a, &b);
+        battery -= a - s;
+
+        if(battery <= 0) {
+            "No".writeln;
+            return;
+        }
+
+        s = b;
+
+        battery = min(n, battery + b - a);
+    }
+
+    if(battery < (t - s + 1)) {
+        "No".writeln;
+        return;
+    }
+
+    "Yes".writeln;
+}
+
