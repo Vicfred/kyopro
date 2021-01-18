41a5c70b19c2f009205db681f03dadddbadc5d8a
Vicfred <git@vicfred.dev>
2021-01-17 22:06:01 -0600

    cf1469A Regular Bracket Sequence

---
+++ codeforces/cf1469A_regular_bracket_sequence.d
@@ -0,0 +1,23 @@
+// Vicfred
+// https://codeforces.com/contest/1469/problem/A
+// 
+import std.algorithm;
+import std.array;
+import std.container;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int t = readln.chomp.to!int;
+
+while(t--) {
+    string s = readln.strip;
+
+    if(s[0] == ')' || s[$-1] == '(' || s.length % 2 == 1)
+        "NO".writeln;
+    else
+        "YES".writeln;
+}
+}
+
