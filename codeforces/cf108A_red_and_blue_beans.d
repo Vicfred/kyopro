2749d27d508ff1ad94b8cc17e62a669e736217e5
Vicfred <git@vicfred.dev>
2021-06-06 21:39:15 -0500

    cf1519A Red and Blue Beans

---
+++ codeforces/cf108A_red_and_blue_beans.d
@@ -0,0 +1,23 @@
+// Vicfred
+// https://codeforces.com/contest/1519/problem/A
+// greedy
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int t = readln.chomp.to!int;
+
+while(t--) {
+    long r, b, d;
+    readf("%s %s %s\n", &r, &b, &d);
+    if(r > b)
+        swap(r,b);
+    if(b <= r*(d + 1))
+        "YES".writeln;
+    else
+        "NO".writeln;
+}
+}
