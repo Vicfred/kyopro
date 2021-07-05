96a6035ceba66cbb69a723680d5509cdb03d1a86
Vicfred <git@vicfred.dev>
2021-07-04 21:13:08 -0500

    cf1525A Potion Making

---
+++ codeforces/cf1525A_potion_making.d
@@ -0,0 +1,17 @@
+// Vicfred
+// https://codeforces.com/contest/1525/problem/A
+// basic math
+import std.stdio;
+
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a%b); }
+
+void main() {
+    int t;
+    readf("%s\n", &t);
+
+while(t--) {
+    int k;
+    readf("%d\n", &k);
+    writefln("%s", 100/gcd(100,k));
+}
+}
