0229ff50f350cf7c9d2d37b283de5744479aea86
Unai Hernández Minaberry <unihernandez22@gmail.com>
2021-07-05 21:49:52 -0300

    abc135A (prettier haskell)

---
+++ atcoder/abc135A_harmony_.hs
@@ -0,0 +1,11 @@
+-- uninhm
+-- https://atcoder.jp/contests/abc135/tasks/abc135_a
+-- basic math
+
+solve (a:b:[]) = 
+	if (a + b) `mod` 2 == 0 then
+		show $ (a + b) `div` 2
+	else
+		"IMPOSSIBLE"
+
+main = interact $ solve . map read . words
