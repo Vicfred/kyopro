f733aee94b960a3e3cbc691ece95a708a756b656
Unai Hernández Minaberry <unihernandez22@gmail.com>
2021-07-15 16:12:17 -0300

    better recursion

--- atcoder/abc208B_factorial_yen_coin.hs
+++ atcoder/abc208B_factorial_yen_coin.hs
@@ -5,9 +5,9 @@
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
-solve ans 1 p = ans+p
-solve ans n p =
-  solve (ans + p `div` f) (n-1) (p `mod` f)
+solve 1 p = p
+solve n p =
+  p `div` f + solve (n-1) (p `mod` f)
     where f = factorial n
 
-main = getLine >>= print . solve 0 10 . read
+main = getLine >>= print . solve 10 . read
