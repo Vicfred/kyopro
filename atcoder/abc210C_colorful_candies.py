56afb60e93f6fb9f018ddc591641d4c24c87c2b0
Unai Hernández Minaberry <unihernandez22@gmail.com>
2021-07-18 17:41:44 -0300

    abc210C (python)

---
+++ atcoder/abc210C_colorful_candies.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+n, k = map(int, input().split())
+c = list(map(int, input().split()))
+
+s = Counter(c[:k])
+ans = len(s)
+for i in range(n-k):
+    s[c[i]] -= 1
+    if s[c[i]] <= 0:
+        s.pop(c[i], None)
+    s[c[i+k]] += 1
+    ans = max(ans, len(s))
+
+print(ans)
