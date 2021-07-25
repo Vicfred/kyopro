6dc48aad1f05917715671091ddf151428fdd177c
Unai Hernández Minaberry <unihernandez22@gmail.com>
2021-07-25 18:32:58 -0300

    abc211B (haskell)

---
+++ atcoder/abc211B_cycle_hit.hs
@@ -0,0 +1,15 @@
+-- uninhm
+-- https://atcoder.jp/contests/abc211/tasks/abc211_b
+-- implementation
+
+import Data.List (sort)
+import Data.Bool (bool)
+
+isSet :: Ord a => [a] -> Bool
+isSet = check . sort
+  where
+    check [] = True
+    check [_] = True
+    check (x:y:xs) = x /= y && check (y:xs)
+
+main = interact $ bool "No" "Yes" . isSet . lines
