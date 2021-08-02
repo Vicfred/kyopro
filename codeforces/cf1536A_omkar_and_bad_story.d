7474635c41534858ab65c50e0b588b2c695020dd
Vicfred <git@vicfred.dev>
2021-08-01 21:45:52 -0500

    cf1536A Omkar and Bad Story

---
+++ codeforces/cf1536A_omkar_and_bad_story.d
@@ -0,0 +1,28 @@
+// Vicfred
+// https://codeforces.com/contest/1536/problem/A
+// 
+import std.algorithm;
+import std.array;
+import std.container;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int t = readln.chomp.to!int;
+
+while(t--) {
+    int n = readln.chomp.to!int;
+    int[] a = readln.split.map!(to!int).array;
+
+    if(a.count!(x => x < 0) > 0) {
+        "NO".writeln;
+    } else {
+        "YES".writeln;
+        "101".writeln;
+        for(int i = 0; i < 101; ++i)
+            writef("%s ", i);
+        "".writeln;
+    }
+}
+}
