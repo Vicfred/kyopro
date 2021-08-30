32e8240d4a774aab4f2934361b97f92a9d3d50bb
Vicfred <git@vicfred.dev>
2021-08-29 22:28:14 -0500

    cf1541C Great Graphs

---
+++ codeforces/cf1541C_great_graphs.d
@@ -0,0 +1,25 @@
+// Vicfred
+// https://codeforces.com/contest/1541/problem/C
+//
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+
+void main() {
+    int t = readln.chomp.to!int;
+
+while(t--) {
+    int n = readln.chomp.to!int;
+    long[] d = readln.split.map!(to!long).array;
+    d.sort;
+    long ans = d[n - 1];
+    long node = 0L;
+    for(int i = 0; i < n; ++i) {
+        ans -= d[i] * i - node;
+        node += d[i];
+    }
+    ans.writeln;
+}
+}
