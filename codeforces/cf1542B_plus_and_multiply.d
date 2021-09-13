7eab0e14f2a041d73d054b2e17a4bac3f8c67b68
Vicfred <git@vicfred.dev>
2021-09-12 22:23:35 -0500

    cf1542B Plus and Multiply

---
+++ codeforces/cf1542B_plus_and_multiply.d
@@ -0,0 +1,40 @@
+// Vicfred
+// https://codeforces.com/contest/1542/problem/B
+// math, greedy
+import std.container;
+import std.stdio;
+
+void main() {
+    int t;
+    readf("%s\n", &t);
+
+while(t--) {
+    long n, a, b;
+    readf("%s %s %s\n", &n, &a, &b);
+    if(a == 1L) {
+        if((n - 1L) % b == 0L)
+            "Yes".writeln;
+        else
+            "No".writeln;
+        continue;
+    }
+    auto powers = new RedBlackTree!(long,"a > b");
+    powers.insert(1L);
+    while(powers.front <= n) {
+        powers.insert(powers.front * a);
+    }
+    if(powers.front > n)
+        powers.removeFront;
+    n = n % b;
+    bool found = false;
+    while(!powers.empty) {
+        if(powers.front % b == n)
+            found = true;
+        powers.removeFront;
+    }
+    if(found)
+        "Yes".writeln;
+    else
+        "No".writeln;
+}
+}
