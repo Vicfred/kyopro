33f5d5d1f4b05642509612c1b1aecba8a7b03573
Vicfred <git@vicfred.dev>
2021-09-26 22:27:28 -0500

    cf1547B Alphabetical Strings

---
+++ codeforces/cf1547B_alphabetical_strings.d
@@ -0,0 +1,51 @@
+// Vicfred
+// https://codeforces.com/contest/1547/problem/B
+// greedy
+import std.algorithm;
+import std.stdio;
+
+int find(string s, char c) {
+    for(int i = 0; i < s.length; ++i) {
+        if(s[i] == c)
+            return i;
+    }
+    return -1;
+}
+
+void main() {
+    int t;
+    readf("%s\n", &t);
+
+while(t--) {
+    string s;
+    readf("%s\n", &s);
+    int index = find(s, 'a');
+    if(index == -1) {
+        "NO".writeln;
+        continue;
+    }
+    int R, L;
+    L = index;
+    R = index;
+    bool valid = true;
+    for(int i = 1; i < s.length; ++i) {
+        char c = cast(char)('a' + i);
+        int idx = find(s, c);
+        if(idx == -1) {
+            valid = false;
+            break;
+        }
+        if(idx != L - 1 && idx != R + 1) {
+            valid = false;
+            break;
+        }
+        L = min(L, idx);
+        R = max(R, idx);
+    }
+    if(valid) {
+        "YES".writeln;
+    } else {
+        "NO".writeln;
+    }
+}
+}
