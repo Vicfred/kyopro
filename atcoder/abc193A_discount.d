a19b7f69947bd97defc7ebf820eec1471d51337b
Vicfred <git@vicfred.dev>
2021-10-10 20:31:52 -0500

    abc193A Discount

---
+++ atcoder/abc193A_discount.d
@@ -0,0 +1,13 @@
+// Vicfred
+// https://atcoder.jp/contests/abc193/tasks/abc193_a
+// implementation
+import std.stdio;
+
+void main() {
+    real A, B;
+    readf("%s %s\n", &A, &B);
+
+    real ans = 1.0 - B/A;
+    ans *= 100.0;
+    ans.writeln;
+}
