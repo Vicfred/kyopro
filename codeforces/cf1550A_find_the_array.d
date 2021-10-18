a42683c59665b4da7a6674f5d4042671defb2665
Vicfred <git@vicfred.dev>
2021-10-17 20:43:26 -0500

    cf1550A Find The Array

---
+++ codeforces/cf1550A_find_the_array.d
@@ -0,0 +1,22 @@
+// Vicfred
+// https://codeforces.com/contest/1550/problem/A
+// greedy
+import std.stdio;
+
+void main() {
+    int t;
+    readf("%s\n", &t);
+
+while(t--) {
+    int s;
+    readf("%s\n", &s);
+
+    int ans = 1;
+
+    while(ans * ans < s) {
+        ans += 1;
+    }
+
+    ans.writeln;
+}
+}
