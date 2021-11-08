b55ca83620541dfe034bc5ab7b9adce82afe56af
Vicfred <git@vicfred.dev>
2021-11-07 21:01:56 -0600

    abc195A Health M Death

---
+++ atcoder/abc195A_health_m_death.nim
@@ -0,0 +1,11 @@
+# Vicfred
+# https://atcoder.jp/contests/abc195/tasks/abc195_a
+# implementation
+import sequtils, strutils
+var m, h : int
+(m, h) = stdin.readLine.split.map parseInt
+
+if h mod m == 0:
+  echo "Yes"
+else:
+  echo "No"
