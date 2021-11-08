5154b433ae5abd9c3a69beca5d05710d03dde260
Vicfred <git@vicfred.dev>
2021-11-07 21:02:12 -0600

    abc195B Many Oranges

---
+++ atcoder/abc195B_many_oranges.nim
@@ -0,0 +1,22 @@
+# Vicfred
+# https://atcoder.jp/contests/abc195/tasks/abc195_b
+# math, implementation, brute force
+import sequtils, strutils, math
+
+var a, b, w : int
+(a, b, w) = stdin.readLine.split.map parseInt
+
+
+var minima, maxima : int
+minima = 10 ^ 9
+maxima = -1
+
+for n in 1..1000000:
+  if a*n <= 1000*w and 1000*w <= b*n:
+    minima = min(minima, n)
+    maxima = max(maxima, n)
+
+if maxima == -1:
+  echo "UNSATISFIABLE"
+else:
+  echo minima, " ", maxima
