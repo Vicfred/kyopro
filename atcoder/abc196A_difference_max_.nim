bdfdf1db51ae51f84e39cbf86fc2cf91bf735824
Vicfred <git@vicfred.dev>
2021-11-14 21:16:49 -0600

    abc196A Difference Max

--- atcoder/abc196A_difference_max_.nim
+++ atcoder/abc196A_difference_max_.nim
@@ -7,10 +7,6 @@ var a, b, c, d: int
 (a, b) = stdin.readLine.split.map parseInt
 (c, d) = stdin.readLine.split.map parseInt
 
-let u = b - c
-let v = b - d
-
-var maxima : int
-maxima = max(u, v)
+let maxima = b - c
 
 echo maxima
