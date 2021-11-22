47f3a44b17a939e25a96fbb9227a80b2f2e0c3d5
Vicfred <git@vicfred.dev>
2021-11-21 20:57:07 -0600

    cf1555A Pizzaforces

---
+++ codeforces/cf1555A_pizzaforces.d
@@ -0,0 +1,17 @@
+// Vicfred
+// https://codeforces.com/contest/1555/problem/A
+// basic math
+import std.algorithm;
+import std.stdio;
+
+void main() {
+    long t;
+    readf("%s\n", &t);
+
+while(t--) {
+    long n;
+    readf("%s\n", &n);
+    long ans = max(6L, n + 1L) / 2L * 5L;
+    ans.writeln;
+}
+}
