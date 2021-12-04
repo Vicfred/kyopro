80fb7badb3e14b485a8d16452aebfd80f999d292
Vicfred <git@vicfred.dev>
2021-12-04 11:23:17 -0600

    Add advent of code.

---
+++ aoc/aoc2021day01sonarsweep1.nim
@@ -0,0 +1,18 @@
+import strutils
+
+let f = open("input")
+var line : string
+
+var prev, cur, total: int
+prev = 0
+total = 0
+while f.read_line(line):
+  cur = parseInt(line)
+  if cur - prev > 0:
+    total += 1
+  prev = cur
+total -= 1
+
+echo total
+
+f.close()
