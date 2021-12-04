8ee044b9b5c4412936bc3c8bfec2afcb0e103fb2
Vicfred <git@vicfred.dev>
2021-12-04 11:25:04 -0600

    Advent of code day 2 part 1 in nim.

---
+++ aoc/aoc2021day02dive.nim
@@ -0,0 +1,24 @@
+import strutils
+
+let f = open("input")
+
+var horizontal, vertical: int
+
+var line : string
+while f.read_line(line):
+  var direction = line.split[0]
+  var distance = parseInt(line.split[1])
+  case direction:
+    of "forward":
+      horizontal += distance
+    of "down":
+      vertical += distance
+    of "up":
+      vertical -= distance
+    else:
+      echo "Impossibru"
+
+echo horizontal, " ", vertical
+echo vertical * horizontal
+
+f.close()
