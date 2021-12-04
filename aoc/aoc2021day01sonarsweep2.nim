0d3c96853dde9d3eedec4ea5c5d37eb6ddeb42fb
Vicfred <git@vicfred.dev>
2021-12-04 11:24:09 -0600

    Advent of code day 2 part 2.

---
+++ aoc/aoc2021day01sonarsweep2.nim
@@ -0,0 +1,26 @@
+import strutils
+
+let f = open("input")
+
+var sweeps = newSeq[int]()
+
+var line : string
+while f.read_line(line):
+  sweeps.add(parseInt(line))
+
+var cum = newSeq[int]()
+for idx in 0..len(sweeps) - 3:
+  cum.add(sweeps[idx] + sweeps[idx + 1] + sweeps[idx + 2])
+
+var prev, total : int
+prev = 0
+total = 0
+for item in cum:
+  if item - prev > 0:
+    total += 1
+  prev = item
+total -= 1
+
+echo total
+
+f.close()
