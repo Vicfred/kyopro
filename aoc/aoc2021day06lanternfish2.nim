b9a94717c60033df88a7e231b149eea5985da811
Vicfred <git@vicfred.dev>
2021-12-16 08:01:19 -0600

    Advent of code day 6 part 2 in nim.

--- aoc/aoc2021day06lanternfish2.nim
+++ aoc/aoc2021day06lanternfish2.nim
@@ -6,10 +6,17 @@ let days = 256
 
 var dp = newSeq[int](days + 1)
 dp[0] = 1
-dp[6 + 1] = 1
-dp[8 + 1] = 1
+dp[1] = 2
+dp[2] = 2
+dp[3] = 2
+dp[4] = 2
+dp[5] = 2
+dp[6] = 2
+dp[7] = 2
+dp[8] = 3
 for i in 9..days:
   dp[i] = dp[i - 7]  + dp[i - 9]
+echo dp
 
 var ans : int
 for fish in lanternfish:
