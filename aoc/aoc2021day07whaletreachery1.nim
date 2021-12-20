61fa44957ff0df8db284a03425ba36a10ddd83cd
Vicfred <git@vicfred.dev>
2021-12-19 20:37:38 -0600

    Advent of code day 7 in nim.

---
+++ aoc/aoc2021day07whaletreachery1.nim
@@ -0,0 +1,16 @@
+# Vicfred
+# https://adventofcode.com/2021/day/7
+import sequtils
+import strutils
+
+let crabs = stdin.readLine.split(",").map(parseInt)
+
+var minima = high(int)
+for submarine in crabs:
+  var dist : int
+  for crab in crabs:
+    dist += abs(submarine - crab)
+  minima = min(minima, dist)
+
+echo minima
+
