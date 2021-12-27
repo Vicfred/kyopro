18f1afa4a3c6e84ddba22ebce13d03e269b6dec4
Vicfred <git@vicfred.dev>
2021-12-26 21:07:03 -0600

    Advent of code day 8 part 1 in nim.

---
+++ aoc/aoc2021day08sevensegment1.nim
@@ -0,0 +1,18 @@
+# Vicfred
+# https://adventofcode.com/2021/day/8
+# string manipulation, implementation
+import strutils
+
+let f = open("input")
+var line : string
+
+var ans : int
+while f.read_line(line):
+  let input = line.split("|")[1].split
+  for word in input:
+    if word.len == 2 or
+       word.len == 3 or
+       word.len == 4 or
+       word.len == 7:
+      ans += 1
+echo ans
