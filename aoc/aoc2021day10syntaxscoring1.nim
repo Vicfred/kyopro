65220a0d829a4a388a8ff55893a5dc4f6591ac13
Vicfred <git@vicfred.dev>
2022-01-09 21:44:11 -0600

    Advent of code day 10 part 1 in nim.

---
+++ aoc/aoc2021day10syntaxscoring1.nim
@@ -0,0 +1,28 @@
+# Vicfred
+# https://adventofcode.com/2021/day/10
+# stack, implementation
+import tables
+let f = open("input")
+var line : string
+
+let opening = @['(', '[', '{', '<']
+let matching = {')': '(',
+                ']': '[',
+                '}': '{',
+                '>': '<'}.toTable
+let points = {')':3,
+              ']': 57,
+              '}': 1197,
+              '>': 25137}.toTable
+
+var score : int
+while f.read_line(line):
+  var state : seq[char]
+  for ch in line:
+    if ch in opening:
+      state.add(ch)
+    else:
+      let initial = state.pop
+      if matching[ch] != initial:
+        score += points[ch]
+echo score
