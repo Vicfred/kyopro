87e0757ec8658cdf14a31b491abd938adf58acfa
Vicfred <git@vicfred.dev>
2022-02-13 20:52:27 -0600

    abc198A Div

---
+++ atcoder/abc198A_div_.nim
@@ -0,0 +1,12 @@
+# Vicfred
+# https://atcoder.jp/contests/abc198/tasks/abc198_a
+# combinatorics, math, brute force
+import strutils
+let n = stdin.readLine.parseInt
+
+var ans : int
+for x in 1..n:
+  for y in 1..n:
+    if x + y == n:
+      ans += 1
+echo ans
