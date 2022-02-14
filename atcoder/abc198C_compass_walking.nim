15d8c2376f49a7413b8a44f55647c856fe107897
Vicfred <git@vicfred.dev>
2022-02-13 22:15:49 -0600

    abc198C Compass Walking

---
+++ atcoder/abc198C_compass_walking.nim
@@ -0,0 +1,18 @@
+# Vicfred
+# https://atcoder.jp/contests/abc198/tasks/abc198_c
+# math, geometry
+import sequtils, strutils
+import math
+
+proc euclidean(x : float64, y : float64): float64 =
+  sqrt(x * x + y * y)
+
+var r, x, y : int
+(r, x, y) = stdin.readLine.split.map parseInt
+if euclidean((float64)x, (float64)y) == (float)r:
+  echo 1
+elif euclidean((float64)x, (float64)y) < 2 * (float)r:
+  echo 2
+else:
+  echo (int)ceil(euclidean((float64)x, (float64)y) / (float64)r)
+# https://atcoder.jp/contests/abc198/submissions/29240958
