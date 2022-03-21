230a6ddaf3fd70533193514b83cadc3d25e7180d
Vicfred <git@vicfred.dev>
2022-03-20 19:07:19 -0600

    abc244B Go Straight and Turn Right

---
+++ atcoder/abc244B_go_straight_and_turn_right.nim
@@ -0,0 +1,29 @@
+# Vicfred
+# https://atcoder.jp/contests/abc244/tasks/abc244_b
+# implementation
+import sequtils, strutils
+
+let n = stdin.readLine.parseInt
+let s = stdin.readLine
+
+var x, y : int
+
+var direction : int
+for ch in s:
+  if ch == 'R':
+    direction += 1
+    direction = direction mod 4
+    continue
+  case direction:
+    of 0:
+      x += 1
+    of 1:
+      y -= 1
+    of 2:
+      x -= 1
+    of 3:
+      y += 1
+    else:
+      echo "piola"
+echo x, " ", y
+
