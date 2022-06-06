8bf55a119a75d5b8b6678c1f9c0c19d9d2617eca
Vicfred <git@vicfred.dev>
2022-06-05 19:37:56 -0500

    cf550C Divisibility by Eight.

---
+++ codeforces/cf505C_divisibility_by_eight.cpp
@@ -0,0 +1,45 @@
+// Vicfred
+// https://codeforces.com/problemset/problem/550/C
+// math, brute force
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string n;
+  cin >> n;
+
+  int ans = -1;
+  for (int i = 0; i < n.size(); ++i) {
+    if (ans != -1)
+      break;
+    int x = n[i] - '0';
+    if (x % 8 == 0) {
+      ans = x;
+      break;
+    }
+    for (int j = i + 1; j < n.size(); ++j) {
+      if (ans != -1)
+        break;
+      x = (n[i] - '0') * 10 + (n[j] - '0');
+      if (x % 8 == 0) {
+        ans = x;
+        break;
+      }
+      for (int k = j + 1; k < n.size(); ++k) {
+        if (ans != -1)
+          break;
+        x = (n[i] - '0') * 100 + (n[j] - '0')*10 + (n[k] - '0');
+        if (x % 8 == 0) {
+          ans = x;
+          break;
+        }
+      }
+    }
+  }
+  if (ans != -1)
+    cout << "YES\n" << ans << endl;
+  else
+    cout << "NO" << endl;
+  return 0;
+}
