4edcd9aa5ad5c3121be51b785e14ae7f95179d33
Unai Hernández Minaberry <unihernandez22@gmail.com>
2022-10-30 21:41:39 -0300

    cf1703A (ruby)

---
+++ codeforces/cf1703A_yes_or_yes.rb
@@ -0,0 +1,14 @@
+# uninhm
+# https://codeforces.com/contest/1703/problem/A
+# implementation
+
+t = gets.to_i
+
+t.times do
+  ln = gets.chomp
+  if ln.downcase == "yes"
+    puts "YES"
+  else
+    puts "NO"
+  end
+end
