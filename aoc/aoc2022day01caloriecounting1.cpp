bf7431efa4402015cc328100e1b03dd57715ee14
Vicfred <git@vicfred.dev>
2022-12-02 19:36:05 -0800

    aoc2022day02 Rock Paper Scissors part one.

---
+++ aoc/aoc2022day01caloriecounting1.cpp
@@ -0,0 +1,27 @@
+// Vicfred
+// https://adventofcode.com/2022/day/1
+// implementation
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+  ifstream input("input.txt");
+
+  long long maxima = 0LL;
+  long long current = 0LL;
+  for(string line; getline(input, line);) {
+    if(line == "") {
+      maxima = max(maxima, current);
+      current = 0LL;
+      continue;
+    }
+    current += stoll(line);
+  }
+
+  cout << maxima << endl;
+
+  return 0;
+}
---
+++ aoc/aoc2022day02rockpaperscissors1.cpp
@@ -0,0 +1,63 @@
+// Vicfred
+// https://adventofcode.com/2022/day/1
+// data structures
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+  ifstream input("input.txt");
+
+  map<char, int> play = {{'X', 1}, {'Y', 2}, {'Z', 3}};
+  int total = 0;
+  for (string line; getline(input, line);) {
+    char opponent, you;
+    //input >> opponent >> you;
+    opponent = line[0];
+    you = line[2];
+    int score = 0;
+    score += play[you];
+    if(you == 'X') {
+      switch (opponent) {
+        case 'A':
+          score += 3;
+          break;
+        case 'C':
+          score += 6;
+          break;
+        default:
+          break;
+      }
+    }
+    if(you == 'Y') {
+      switch (opponent) {
+        case 'A':
+          score += 6;
+          break;
+        case 'B':
+          score += 3;
+          break;
+        default:
+          break;
+      }
+    }
+    if(you == 'Z') {
+      switch (opponent) {
+        case 'B':
+          score += 6;
+          break;
+        case 'C':
+          score += 3;
+          break;
+        default:
+          break;
+      }
+    }
+    total += score;
+  }
+  cout << total << endl;
+  return 0;
+}
