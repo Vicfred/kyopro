122836dcaba1bd781febe820daafa619da033860
Vicfred <git@vicfred.dev>
2023-01-22 18:38:10 -0800

    Coin combinations II alternative solution.

---
+++ cses/coin_combinations_ii_.cpp
@@ -0,0 +1,37 @@
+// Vicfred
+// https://cses.fi/problemset/task/1636
+// dynamic programming
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    const int mod = 1e9 + 7;
+    const int maxn = 1e6 + 1;
+
+    vector<int> dp(maxn, 0);
+    dp[0] = 1;
+
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> coins(n);
+    for(int &coin: coins) {
+        cin >> coin;
+    }
+
+    for(int i = 1; i <= maxn; ++i) {
+        for(int &coin: coins) {
+            int value = i - coin;
+            if(value >= 0) {
+                dp[i] += dp[value];
+                dp[i] %= mod;
+            }
+        }
+    }
+
+    cout << dp[x] << endl;
+
+    return 0;
+}
