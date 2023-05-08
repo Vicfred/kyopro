6be44e051947edf4559e9367856fb23a19eeb9e0
vicfred <git@vicfred.dev>
2023-05-07 19:09:44 -0700

    cf1829C Mr. Perfectly Fine

---
+++ codeforces/cf1829C_mr_perfectly_fine.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  long long t;
+  cin >> t;
+  for (int kase = 0; kase < t; ++kase) {
+    long long n;
+    cin >> n;
+    vector<long long> m(n);
+    vector<string> s(n);
+    long long both, l, r;
+    both = l = r = 1 << 30;
+    for (int i = 0; i < n; ++i) {
+      cin >> m[i];
+      cin >> s[i];
+      if (s[i] == "11") {
+        both = min(both, m[i]);
+      } else if (s[i] == "10") {
+        l = min(l, m[i]);
+      } else if (s[i] == "01") {
+        r = min(r, m[i]);
+      }
+    }
+    if (both == 1 << 30 && l == 1 << 30 && r == 1 << 30) {
+      cout << -1 << endl;
+      continue;
+    }
+    long long ans = 0;
+    if (both != 1 << 30) {
+      ans = min(both, l + r);
+      cout << ans << endl;
+    } else {
+      if (l == 1 << 30 || r == 1 << 30) {
+        cout << -1 << endl;
+      } else {
+        cout << l + r << endl;
+      }
+    }
+  }
+  return 0;
+}
