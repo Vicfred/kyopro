a9bda8a25795b151d370eb6c2a27881d13d19542
vicfred <git@vicfred.dev>
2023-05-14 19:51:31 -0700

    cf1807C Find and Replace

---
+++ codeforces/cf1807C_find_and_replace.cpp
@@ -0,0 +1,39 @@
+// Vicfred
+// https://codeforces.com/contest/1807/problem/C
+// greedy
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while(t--) {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    set<char> zero, one;
+    for(int i = 0; i < n; ++i) {
+      if(i % 2 == 0) {
+        zero.insert(s[i]);
+      } else {
+        one.insert(s[i]);
+      }
+    }
+    bool valid = true;
+    for(char ch = 'a'; ch <= 'z'; ++ch) {
+      if(zero.count(ch) > 0 and one.count(ch) > 0) {
+        valid = false;
+        break;
+      }
+    }
+    if(valid) {
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
+  }
+  return 0;
+}
