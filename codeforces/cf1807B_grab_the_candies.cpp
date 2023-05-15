8cdbd9ba24aba1f7c23efcceddda95b70db3c24d
vicfred <git@vicfred.dev>
2023-05-14 18:36:52 -0700

    cf1807B Grab the Candies

---
+++ codeforces/cf1807B_grab_the_candies.cpp
@@ -0,0 +1,35 @@
+// Vicfred
+// https://codeforces.com/contest/1807/problem/B
+// greedy
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while(t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; ++i) {
+      cin >> a[i];
+    }
+    int even, odd;
+    even = odd = 0;
+    for(int i = 0; i < n; ++i) {
+      if(a[i] % 2 == 0) {
+        even += a[i];
+      } else {
+        odd += a[i];
+      }
+    }
+    if(even > odd) {
+      cout << "YES" << endl;
+    } else {
+      cout << "NO" << endl;
+    }
+  }
+  return 0;
+}
