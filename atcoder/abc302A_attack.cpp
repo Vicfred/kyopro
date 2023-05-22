a81fbbd58f61d96ecb9a67251e06c3d65855887f
vicfred <git@vicfred.dev>
2023-05-21 18:22:02 -0700

    abc302A Attack

---
+++ atcoder/abc302A_attack.cpp
@@ -0,0 +1,13 @@
+// Vicfred
+// https://atcoder.jp/contests/abc302/tasks/abc302_a
+// math
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long A, B;
+  cin >> A >> B;
+  cout << (A - 1LL)/B + 1LL << endl;
+  return 0;
+}
