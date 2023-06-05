38860efdb73d0ed69971c7754cafcd9c758239a7
vicfred <git@vicfred.dev>
2023-06-04 18:24:10 -0700

    abc300A N-Choice Question

---
+++ atcoder/abc300A_n_choice_question.cpp
@@ -0,0 +1,20 @@
+// Vicfred
+// https://atcoder.jp/contests/abc300/tasks/abc300_a
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n, a, b;
+  cin >> n >> a >> b;
+  for(int i = 1; i <= n; ++i) {
+    int c;
+    cin >> c;
+    if(c == a + b) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+  return 0;
+}
