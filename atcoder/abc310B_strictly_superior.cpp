78887b67790172caad8f6d662b6eb434b3f00208
vicfred <git@vicfred.dev>
2023-07-16 19:22:28 -0700

    abc310B Strictly Superior

---
+++ atcoder/abc310B_strictly_superior.cpp
@@ -0,0 +1,57 @@
+// Vicfred
+// https://atcoder.jp/contests/abc310/tasks/abc310_b
+// brute force, data structures
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  long long int N, M;
+  cin >> N >> M;
+  vector<long long int> P(N);
+  vector<set<int>> C(N);
+  vector<vector<long long int>> F(N, vector<long long int>(M));
+  for (int i = 0; i < N; ++i) {
+    int c;
+    cin >> P[i] >> c;
+    for(int j = 0; j < c; ++j) {
+      int f;
+      cin >> f;
+      C[i].insert(f);
+    }
+  }
+  bool found = false;
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      if (i == j)
+        continue;
+      if (P[i] < P[j])
+        continue;
+      bool valid = true;
+      for (const auto &item : C[i]) {
+        if (C[j].count(item) == 0) {
+          valid = false;
+        }
+      }
+      if (!valid) {
+        continue;
+      }
+      if (P[i] > P[j]) {
+        found = true;
+      }
+      for (const auto &item : C[j]) {
+        if (C[i].count(item) == 0) {
+          found = true;
+        }
+      }
+    }
+  }
+  if (found) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
