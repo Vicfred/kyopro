e1d18b4e05efe3fba3bd6b5adadf17a6529cc52c
vicfred <git@vicfred.dev>
2023-07-16 19:47:17 -0700

    abc310C Reversible

---
+++ atcoder/abc310C_reversible.cpp
@@ -0,0 +1,26 @@
+// Vicfred
+// https://atcoder.jp/contests/abc310/tasks/abc310_c
+// data structures
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main() {
+  long long N;
+  cin >> N;
+  set<string> sticks;
+  for (int i = 0; i < N; ++i) {
+    string S;
+    cin >> S;
+    string T(S);
+    reverse(begin(T), end(T));
+    if(sticks.count(S) == 0 and sticks.count(T) == 0) {
+      sticks.insert(S);
+    }
+  }
+  cout << sticks.size() << endl;
+  return 0;
+}
