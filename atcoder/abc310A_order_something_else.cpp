15ad1913a8646e9635a87089258194fc26e3bee6
vicfred <git@vicfred.dev>
2023-07-16 18:30:14 -0700

    abc310A Order Something Else

---
+++ atcoder/abc310A_order_something_else.cpp
@@ -0,0 +1,23 @@
+// Vicfred
+// https://atcoder.jp/contests/abc310/tasks/abc310_a
+// implementation
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N, P, Q;
+  cin >> N >> P >> Q;
+  vector<long long> D(N);
+  for (int i = 0; i < N; ++i) {
+    cin >> D[i];
+  }
+  long long minima = P;
+  for (int i = 0; i < N; ++i) {
+    minima = min(minima, Q + D[i]);
+  }
+  cout << minima << endl;
+  return 0;
+}
