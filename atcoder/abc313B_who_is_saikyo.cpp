556cd62527e062d5b581fb0370949a7fa43d16e3
vicfred <git@vicfred.dev>
2023-08-06 19:40:14 -0700

    ABC313B Who is saikyo

---
+++ atcoder/abc313B_who_is_saikyo.cpp
@@ -0,0 +1,37 @@
+// Vicfred
+// https://atcoder.jp/contests/abc313/tasks/abc313_b
+// greedy
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  set<int> strong, weak, seen;
+  for(int i = 0; i < M; ++i) {
+    int A, B;
+    cin >> A >> B;
+    strong.insert(A);
+    weak.insert(B);
+    seen.insert(A);
+    seen.insert(B);
+  }
+  if(seen.size() < N) {
+    cout << -1 << endl;
+    return 0;
+  }
+  set<int> candidates;
+  for(int i = 1; i <= N; ++i) {
+    if(strong.count(i) > 0 and weak.count(i) == 0) {
+      candidates.insert(i);
+    }
+  }
+  if(candidates.size() != 1) {
+    cout << -1 << endl;
+  } else {
+    cout << *candidates.begin() << endl;
+  }
+  return 0;
+}
