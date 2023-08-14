f288b6a394e893a2b012ea7ef6073a4af83add9f
vicfred <git@vicfred.dev>
2023-08-13 19:55:34 -0700

    abc314A 3.14

---
+++ atcoder/abc314A_3_14.cpp
@@ -0,0 +1,18 @@
+// Vicfred
+// https://atcoder.jp/contests/abc314/tasks/abc314_a
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  string pi = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+  cout << "3.";
+  for(int i = 0; i < N; ++i) {
+    cout << pi[i];
+  }
+  cout << endl;
+  return 0;
+}
