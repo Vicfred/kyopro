262532120eab1dda72e0dff2f282e334a521511f
vicfred <git@vicfred.dev>
2023-09-10 18:47:35 -0700

    abc319A Legendary Players

---
+++ atcoder/abc319A_legendary_players.cpp
@@ -0,0 +1,19 @@
+// Vicfred
+// https://atcoder.jp/contests/abc319/tasks/abc319_a
+// implementation, data structures
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+  map<string, int> ratings = {{"tourist", 3858},   {"ksun48", 3679},
+                              {"Benq", 3658},      {"Um_nik", 3648},
+                              {"apiad", 3638},     {"Stonefeang", 3630},
+                              {"ecnerwala", 3613}, {"mnbvmar", 3555},
+                              {"newbiedmy", 3516}, {"semiexp", 3481}};
+  string name;
+  cin >> name;
+  cout << ratings[name] << endl;
+  return 0;
+}
