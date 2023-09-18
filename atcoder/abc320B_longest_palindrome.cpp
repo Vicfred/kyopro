4701545adcb342ed2e6978f088b3df87a85110d1
vicfred <git@vicfred.dev>
2023-09-17 19:03:09 -0700

    ABC320C Longest Palindrome

---
+++ atcoder/abc320B_longest_palindrome.cpp
@@ -0,0 +1,33 @@
+// Vicfred
+// https://atcoder.jp/contests/abc320/tasks/abc320_b
+// brute force
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool is_palindrome(string S) {
+  string T;
+  for (int i = S.size() - 1; i >= 0; i--) {
+    T += S[i];
+  }
+  return S == T;
+}
+
+int main() {
+  string S;
+  cin >> S;
+  int n = S.size();
+  int maxima = 1;
+  for (int ini = 0; ini < n; ++ini) {
+    for (int fin = ini + 1; fin <= n; ++fin) {
+      string T = S.substr(ini, fin - ini + 1);
+      if (is_palindrome(T)) {
+        maxima = max(maxima, (int)T.size());
+      }
+    }
+  }
+  cout << maxima << endl;
+  return 0;
+}
