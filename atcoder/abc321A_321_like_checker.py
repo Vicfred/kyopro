82ec9451001bf548b335d602156bbe02b0317a80
vicfred <git@vicfred.dev>
2023-09-24 18:31:23 -0700

    ABC321A 321-like Checker

---
+++ atcoder/abc321A_321_like_checker.py
@@ -0,0 +1,12 @@
+# Vicfred
+# https://atcoder.jp/contests/abc321/tasks/abc321_a
+# implementation
+N = list(input())
+flag = True
+for i in range(len(N) - 1):
+    if int(N[i]) <= int(N[i+1]):
+        flag = False
+if flag:
+    print("Yes")
+else:
+    print("No")
