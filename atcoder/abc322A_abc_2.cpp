ada35139239524a65ec0693786eda5f9e2541c5c
vicfred <git@vicfred.dev>
2023-10-01 18:47:05 -0700

    ABC322A ABC 2

---
+++ atcoder/abc322A_abc_2.cpp
@@ -0,0 +1,23 @@
+// Vicfred
+// https://atcoder.jp/contests/abc322/tasks/abc322_a
+// implementation
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  string T;
+  cin >> T;
+  int ans = -1;
+  for(int i = 0; i + 2 < N; ++i) {
+    if(T.substr(i, 3) == "ABC") {
+      ans = i + 1;
+      break;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
