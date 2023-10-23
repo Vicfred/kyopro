43808d4daf2d5c8e95f9500e0f6615eb2b13d9c1
vicfred <git@vicfred.dev>
2023-10-22 18:53:18 -0700

    ABC325B World Meeting

---
+++ atcoder/abc325B_world_meeting.cpp
@@ -0,0 +1,31 @@
+// Vicfred
+// https://atcoder.jp/contests/abc325/tasks/abc325_b
+//
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  long long N;
+  cin >> N;
+  vector<long long> W(N);
+  vector<long long> X(N);
+  for (int i = 0; i < N; ++i) {
+    cin >> W[i] >> X[i];
+  }
+  long long maxima = 0;
+  for (int hour = 0; hour < 24; ++hour) {
+    long long employees = 0;
+    for (int i = 0; i < N; ++i) {
+      int meeting = (X[i] + hour) % 24;
+      if (meeting >= 9 and meeting <= 17) {
+        employees += W[i];
+      }
+    }
+    maxima = max(maxima, employees);
+  }
+  cout << maxima << endl;
+  return 0;
+}
