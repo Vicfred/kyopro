104504a3a8204a8584c436e453a489c31093760a
vicfred <git@vicfred.dev>
2023-10-22 18:20:57 -0700

    ABC325A Takahashi san

---
+++ atcoder/abc325A_takahashi_san.cpp
@@ -0,0 +1,14 @@
+// Vicfred
+// https://atcoder.jp/contests/abc325/tasks/abc325_a
+// implementation
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string S, T;
+  cin >> S >> T;
+  cout << S << " " << "san" << endl;
+  return 0;
+}
