8ba047ca59b4690f76e9af3b9da780dffaef6e59
vicfred <git@vicfred.dev>
2023-10-29 18:30:08 -0700

    ABC326A 2UP3DOWN

---
+++ atcoder/abc326A_2up3down.cpp
@@ -0,0 +1,17 @@
+// Vicfred
+// https://atcoder.jp/contests/abc326/tasks/abc326_a
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int X, Y;
+  cin >> X >> Y;
+  if(X - Y < -2 or X - Y > 3) {
+    cout << "No" << endl;
+  } else {
+    cout << "Yes" << endl;
+  }
+  return 0;
+}
