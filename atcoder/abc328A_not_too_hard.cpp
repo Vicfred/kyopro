b0fd12841df5523642eaafa0d6962c711c899c88
vicfred <git@vicfred.dev>
2023-11-12 19:01:28 -0800

    ABC328A Not Too Hard

---
+++ atcoder/abc328A_not_too_hard.cpp
@@ -0,0 +1,21 @@
+// Vicfred
+// https://atcoder.jp/contests/abc328/tasks/abc328_a
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N, X;
+  cin >> N >> X;
+  int ans = 0;
+  for (int i = 0; i < N; ++i) {
+    int s;
+    cin >> s;
+    if (s <= X) {
+      ans += s;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
