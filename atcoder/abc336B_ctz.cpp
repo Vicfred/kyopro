fe297bfb2d3269c62e93e68ed605ef86c5a9e1af
vicfred <git@vicfred.dev>
2024-01-14 18:59:58 -0800

    abc336B CTZ

---
+++ atcoder/abc336B_ctz.cpp
@@ -0,0 +1,28 @@
+// Vicfred
+// https://atcoder.jp/contests/abc336/tasks/abc336_b
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long N;
+  cin >> N;
+  string X;
+  for (int bit = 0; bit < 32; ++bit) {
+    if (N & (1 << bit)) {
+      X += "1";
+    } else {
+      X += "0";
+    }
+  }
+  int ans = 0;
+  for(int i = 0; i < X.size(); ++i) {
+    if(X[i] == '1') {
+      break;
+    }
+    ans += 1;
+  }
+  cout << ans << endl;
+  return 0;
+}
