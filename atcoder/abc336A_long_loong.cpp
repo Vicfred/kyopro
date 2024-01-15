24ebee0edb073976de83c2b1f0165f35880bcacf
vicfred <git@vicfred.dev>
2024-01-14 18:37:45 -0800

    ABC336A Long Loong

---
+++ atcoder/abc336A_long_loong.cpp
@@ -0,0 +1,17 @@
+// Vicfred
+// https://atcoder.jp/contests/abc336/tasks/abc336_a
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  cout << 'L';
+  for(int i = 0; i < N; ++i) {
+    cout << 'o';
+  }
+  cout << "ng" << endl;
+  return 0;
+}
