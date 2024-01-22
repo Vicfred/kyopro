bb5a96b3c061eda47f81cb6383979cc755c0b621
vicfred <git@vicfred.dev>
2024-01-21 18:46:44 -0800

    ABC337B Extended ABC

---
+++ atcoder/abc337b_extended_abc.cpp
@@ -0,0 +1,28 @@
+// Vicfred
+// https://atcoder.jp/contests/abc337/tasks/abc337_b
+// implementation
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  int idx = 0;
+  while (S[idx] == 'A') {
+    idx += 1;
+  }
+  while(S[idx] == 'B') {
+    idx += 1;
+  }
+  while(S[idx] == 'C') {
+    idx += 1;
+  }
+  if(idx == S.size()) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
