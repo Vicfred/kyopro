9cd12e7c0302744beb9c54d9d58419d5e5237321
vicfred <git@vicfred.dev>
2024-04-28 20:16:00 -0700

    ABC351A The bottom of the Ninth

---
+++ atcoder/abc351a_the_bottom_of_the_ninth.cpp
@@ -0,0 +1,24 @@
+// Vicfred
+// https://atcoder.jp/contests/abc351/tasks/abc351_a
+// math
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 0;
+  for(int i = 0; i < 9; ++i) {
+    int ins;
+    cin >> ins;
+    a += ins;
+  }
+  int b = 0;
+  for(int i = 0; i < 8; ++i) {
+    int ins;
+    cin >> ins;
+    b += ins;
+  }
+  int res = a - b;
+  res += 1;
+  cout << res << endl;
+}
