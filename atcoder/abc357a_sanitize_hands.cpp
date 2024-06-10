a9b1bbf71e3d556a3aaf07c9eb586cc81452db7e
vicfred <git@vicfred.dev>
2024-06-09 18:27:43 -0700

    ABC357A Sanitize Hands

---
+++ atcoder/abc357a_sanitize_hands.cpp
@@ -0,0 +1,29 @@
+// Vicfred
+// https://atcoder.jp/contests/abc357/tasks/abc357_a
+// implementation
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> H(N);
+  for(int i = 0; i < N; ++i) {
+    cin >> H[i];
+  }
+  int aliens = 0;
+  for(int i = 0; i < N; ++i) {
+    aliens++;
+    M -= H[i];
+    if(M <= 0) {
+      break;
+    }
+  }
+  if(M < 0) {
+    aliens--;
+  }
+  cout << aliens << endl;
+  return 0;
+}
