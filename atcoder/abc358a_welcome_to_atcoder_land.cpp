9dc9deca591a4234757ac1e74f4244011d5234c2
vicfred <git@vicfred.dev>
2024-06-16 18:21:46 -0700

    ABC358A Welcome to AtCoderLand

---
+++ atcoder/abc358a_welcome_to_atcoder_land.cpp
@@ -0,0 +1,18 @@
+// Vicfred
+// https://atcoder.jp/contests/abc358/tasks/abc358_a
+// implementation
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string S, T;
+  cin >> S >> T;
+  if(S == "AtCoder" and T == "Land") {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
