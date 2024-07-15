e06cec15cad961f7e932ff9ed5d2380996438fc2
vicfred <git@vicfred.dev>
2024-07-14 18:29:30 -0700

    ABC360A A Healthy Breakfast

---
+++ atcoder/abc360a_a_healthy_breakfast.cpp
@@ -0,0 +1,21 @@
+// Vicfred
+// https://atcoder.jp/contests/abc360/tasks/abc360_a
+// implementation
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  if(S[0] == 'R') {
+    cout << "Yes" << endl;
+    return 0;
+  } else if(S[1] == 'R' and S[2] == 'M') {
+    cout << "Yes" << endl;
+    return 0;
+  }
+  cout << "No" << endl;
+  return 0;
+}
