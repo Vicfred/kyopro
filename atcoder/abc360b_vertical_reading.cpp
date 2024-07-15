a55e18a06f4f725a9caa932049e0b13eabe42320
vicfred <git@vicfred.dev>
2024-07-14 19:39:51 -0700

    ABC360B Vertical Reading

---
+++ atcoder/abc360b_vertical_reading.cpp
@@ -0,0 +1,32 @@
+// Vicfred
+// https://atcoder.jp/contests/abc360/tasks/abc360_b
+// brute force, implementation
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main() {
+  string S, T;
+  cin >> S >> T;
+  for (int w = 1; w < S.size(); ++w) {
+    vector<string> cats(w);
+    for (int i = 0; i < S.size(); i += w) {
+      for (int j = i; j < i + w; ++j) {
+        if (j >= S.size()) {
+          break;
+        }
+        cats[j % w] += S[j];
+      }
+    }
+    set<string> nekos(begin(cats), end(cats));
+    if(nekos.count(T)) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+  return 0;
+}
