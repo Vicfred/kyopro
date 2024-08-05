3d971a68d7b88944b68870dc494190478042ccd4
vicfred <git@vicfred.dev>
2024-08-04 18:20:59 -0700

    ABC365A Leap Year

---
+++ atcoder/abc365a_leap_year.cpp
@@ -0,0 +1,21 @@
+// Vicfred
+// https://atcoder.jp/contests/abc365/tasks/abc365_a
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int Y;
+  cin >> Y;
+  if(Y % 4 != 0) {
+    cout << 365 << endl;
+  } else if(Y % 4 == 0 and Y % 100 != 0) {
+    cout << 366 << endl;
+  } else if(Y % 100 == 0 and Y % 400 != 0) {
+    cout << 365 << endl;
+  } else if (Y % 400 == 0) {
+    cout << 366 << endl;
+  }
+  return 0;
+}
