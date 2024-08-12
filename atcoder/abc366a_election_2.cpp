6fd60bd53891c36042d9ed569de90fd4bb03a203
vicfred <git@vicfred.dev>
2024-08-11 18:30:22 -0700

    ABC366A Election 2

---
+++ atcoder/abc366a_election_2.cpp
@@ -0,0 +1,17 @@
+// Vicfred
+// https://atcoder.jp/contests/abc366/tasks/abc366_a
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N, T, A;
+  cin >> N >> T >> A;
+  if (T > N / 2 or A > N / 2) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
