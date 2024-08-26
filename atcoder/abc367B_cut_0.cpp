c09f3cc94b9f951983ae1265711e6feb2adfb14f
vicfred <git@vicfred.dev>
2024-08-25 19:22:07 -0700

    ABC367B Cut.0

---
+++ atcoder/abc367B_cut_0.cpp
@@ -0,0 +1,14 @@
+// Vicfred
+// https://atcoder.jp/contests/abc367/tasks/abc367_b
+// implementation
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+  long double X;
+  cin >> X;
+  cout <<  X << endl;
+  return 0;
+}
