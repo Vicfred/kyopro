8eb9d730ec1d3eebceb7f8cb8b91f76e7ee9dede
vicfred <git@vicfred.dev>
2024-08-25 19:11:09 -0700

    ABC367A Shout Everyday

---
+++ atcoder/abc367a_shout_everyday.cpp
@@ -0,0 +1,25 @@
+// Vicfred
+// https://atcoder.jp/contests/abc367/tasks/abc367_a
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+  if (B > C) {
+    if(A > C and A < B) {
+      cout << "Yes" << endl;
+    } else {
+      cout << "No" << endl;
+    }
+    return 0;
+  }
+  if (A < B or A >= C) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
