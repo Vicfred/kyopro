407a54a6bad0b0eb91202ff79739249fa22bc396
vicfred <git@vicfred.dev>
2024-09-08 18:23:15 -0700

    ABC370A Raise Both Hands

---
+++ atcoder/abc370a_raise_both_hands.cpp
@@ -0,0 +1,21 @@
+// Vicfred
+// https://atcoder.jp/contests/abc370/tasks/abc370_a
+// implementation
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int L, R;
+  cin >> L >> R;
+  if((L ^ R) == 1) {
+    if(L == 1) {
+      cout << "Yes" << endl;
+    } else {
+      cout << "No" << endl;
+    }
+  } else {
+    cout << "Invalid" << endl;
+  }
+  return 0;
+}
