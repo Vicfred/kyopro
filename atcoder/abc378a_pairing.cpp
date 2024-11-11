1853b35206bc910137045f7ae4017025f8453c5b
vicfred <git@vicfred.dev>
2024-11-10 18:44:35 -0800

    ABC378A Pairing

---
+++ atcoder/abc378a_pairing.cpp
@@ -0,0 +1,26 @@
+// Vicfred
+// https://atcoder.jp/contests/abc378/tasks/abc378_a
+// implementation
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  vector<int> A(10);
+  cin >> A[1];
+  cin >> A[2];
+  cin >> A[3];
+  cin >> A[4];
+  vector<int> counter(10);
+  counter[A[1]]++;
+  counter[A[2]]++;
+  counter[A[3]]++;
+  counter[A[4]]++;
+  int answer = 0;
+  for (int i = 1; i <= 4; ++i) {
+    answer += counter[i] / 2;
+  }
+  cout << answer << endl;
+  return 0;
+}
