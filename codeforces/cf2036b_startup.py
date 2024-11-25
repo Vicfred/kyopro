a25ee4af7e43b9ed6ae9fc45b66fe879310da3e9
vicfred <git@vicfred.dev>
2024-11-24 19:22:20 -0800

    CF2036B Startup

---
+++ codeforces/cf2036b_startup.py
@@ -0,0 +1,18 @@
+# Vicfred
+# https://codeforces.com/contest/2036/problem/B
+# greedy
+
+t = int(input())
+for testcast in range(t):
+    n, k = list(map(int, input().split()))
+    #print(n, k)
+    MAXN = k + 2
+    sodas = [0]*MAXN
+    for bottle in range(k):
+        brand, price = list(map(int, input().split()))
+        sodas[brand] += price
+    answer = 0
+    sodas.sort(reverse=True)
+    for i in range(min(n, len(sodas))):
+        answer += sodas[i]
+    print(answer)
