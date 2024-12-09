5de5e6db308deab404eae6528022a5b47e136140
vicfred <git@vicfred.dev>
2024-12-08 19:30:23 -0800

    cf2042B Game with Colored Marbles

---
+++ codeforces/cf2042b_game_with_colored_marbles.cpp
@@ -0,0 +1,41 @@
+// Vicfred
+// https://codeforces.com/contest/2042/problem/B
+// greedy
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    map<int, int> counter;
+    for (int i = 0; i < n; ++i) {
+      cin >> c[i];
+      counter[c[i]] += 1;
+    }
+    vector<int> colors;
+    for (const auto &[key, value] : counter) {
+      colors.push_back(value);
+    }
+    sort(begin(colors), end(colors));
+    int answer = 0;
+    for (int i = 0; i < colors.size(); ++i) {
+      if (i % 2 == 0 and colors[i] == 1) {
+        answer += 1;
+      }
+      if (i % 2 == 1 and colors[i] == 1) {
+        answer -= 1;
+      }
+      answer += 1;
+    }
+    cout << answer << endl;
+  }
+  return 0;
+}
