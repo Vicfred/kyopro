32e1060332bba30db9f3e755efbc8d885db2165b
vicfred <git@vicfred.dev>
2025-03-30 19:29:03 -0700

    ABC399A Hamming Distance

---
+++ atcoder/abc399a_hamming_distance.d
@@ -0,0 +1,20 @@
+// Vicfred
+// https://atcoder.jp/contests/abc399/tasks/abc399_a
+// implementation
+import std.stdio;
+import std.conv;
+import std.string;
+
+void main() {
+  int n = readln.chomp.to!int;
+  string s = readln.chomp;
+  string t = readln.chomp;
+  
+  int ans = 0;
+  for(int i = 0; i < n; ++i) {
+    if(s[i] != t[i]) {
+      ans += 1;
+    }
+  }
+  ans.writeln;
+}
