1bbf7fcdb01d360c9d5cbb6b591dfb2a84118e58
vicfred <git@vicfred.dev>
2025-04-01 02:06:22 -0700

    ABC390A 12345

---
+++ atcoder/abc390a_12345.hs
@@ -0,0 +1,14 @@
+-- vicfred
+-- https://atcoder.jp/contests/abc390/tasks/abc390_a
+-- implementation
+import Data.Bool
+
+main :: IO ()
+main = getLine >>= putStrLn . bool "No" "Yes" . f . map read . words
+
+f :: [Int] -> Bool
+f [1,2,3,5,4] = True
+f [1,2,4,3,5] = True
+f [1,3,2,4,5] = True
+f [2,1,3,4,5] = True
+f _ = False
