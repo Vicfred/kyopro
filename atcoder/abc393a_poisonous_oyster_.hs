d4007b19a4313cb0a2c5cf9ff6d4030b439aec8f
vicfred <git@vicfred.dev>
2025-04-01 01:16:07 -0700

    ABC393A Poisonous Oyster

---
+++ atcoder/abc393a_poisonous_oyster_.hs
@@ -0,0 +1,13 @@
+-- Vicfred
+-- https://atcoder.jp/contests/abc393/tasks/abc393_a
+-- implementation
+main :: IO ()
+main = do
+  [s1, s2] <- words <$> getLine :: IO [String]
+  putStrLn $ show $ f s1 s2
+
+f :: String -> String -> Int
+f "sick" "sick" = 1
+f "sick" "fine" = 2
+f "fine" "sick" = 3
+f _ _ = 4
