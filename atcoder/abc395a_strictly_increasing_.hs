831e39966b575cdf92e47c64e8831bce6774b5a7
vicfred <git@vicfred.dev>
2025-04-01 00:25:12 -0700

    A395A Strictly Increasing?

---
+++ atcoder/abc395a_strictly_increasing_.hs
@@ -0,0 +1,10 @@
+-- Vicfred
+-- https://atcoder.jp/contests/abc395/tasks/abc395_a
+-- implementation
+import Data.Bool (bool)
+
+main :: IO ()
+main = do
+  n <- read <$> getLine :: IO Int
+  a <- map read . words <$> getLine :: IO [Int]
+  putStrLn . bool "No" "Yes" . foldr (&&) True $ zipWith (<) (init a) (tail a)
