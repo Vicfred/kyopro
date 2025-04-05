6bde795cc1eeee26850468b0b20f51e74cefbaa8
vicfred <git@vicfred.dev>
2025-04-04 18:17:48 -0700

    Project Euler 2 Even Fibonacci Numbers

---
+++ projecteuler/0002_even_fibonacci_numbers.hs
@@ -0,0 +1,8 @@
+-- vicfred
+-- https://projecteuler.net/problem=2
+-- math
+fibonacci :: [Integer]
+fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
+
+main :: IO ()
+main = print $ sum . filter even . takeWhile (< 4000000) $ fibonacci
