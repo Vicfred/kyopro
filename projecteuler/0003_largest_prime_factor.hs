b7cfeaff59a85a3edd48844a84067c75940bdf6b
vicfred <git@vicfred.dev>
2025-04-04 19:01:17 -0700

    Project Euler 3 Largest Prime Factor

---
+++ projecteuler/0003_largest_prime_factor.hs
@@ -0,0 +1,16 @@
+-- vicfred
+-- https://projecteuler.net/problem=3
+-- math
+sieve :: [Integer] -> [Integer]
+sieve (x:xs) = x : (sieve (filter ((/=0) . (`mod` x)) xs))
+
+primes :: [Integer]
+primes = sieve [2..]
+
+findLargestPrimeFactor :: Integer -> [Integer] -> Integer
+findLargestPrimeFactor 1 (x:xs) = x
+findLargestPrimeFactor n (x:xs) | n `mod` x == 0 = findLargestPrimeFactor (n `div` x) (x:xs)
+                                | otherwise = findLargestPrimeFactor n xs
+
+main :: IO ()
+main = print $ findLargestPrimeFactor 317584931803 primes
