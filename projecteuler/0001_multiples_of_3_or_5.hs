28a02ae2577740637303a5e6a5dd4259e924c104
vicfred <git@vicfred.dev>
2025-04-04 17:59:46 -0700

    Project Euler 1

--- projecteuler/0001_multiples_of_3_or_5.hs
+++ projecteuler/0001_multiples_of_3_or_5.hs
@@ -2,5 +2,4 @@
 -- https://projecteuler.net/problem=1
 -- implementation
 main :: IO ()
-main = do
-  print $ sum [x | x <- [0..1000-1], x `mod` 3 == 0 || x `mod` 5 == 0]
+main = print $ sum [x | x <- [0..1000-1], x `mod` 3 == 0 || x `mod` 5 == 0]
