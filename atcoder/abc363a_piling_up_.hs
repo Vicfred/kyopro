dfb7814fa6d0e3c0fcb7e501691a2d58ef8bda79
vicfred <git@vicfred.dev>
2025-04-10 17:55:36 -0700

    ABC363A Piling Up

--- atcoder/abc363a_piling_up_.hs
+++ atcoder/abc363a_piling_up_.hs
@@ -2,4 +2,4 @@
 -- https://atcoder.jp/contests/abc363/tasks/abc363_a
 -- implementation
 main :: IO ()
-main = getLine >>= print . (100 -) . (`mod` 100) .(read @Int)
+main = getLine >>= print . (100 -) . (`mod` 100) . (read @Int)
