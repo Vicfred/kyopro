10edf786de75e5ef1a286556b0b23ad5e99ac1b0
vicfred <git@vicfred.dev>
2025-04-17 11:57:49 -0700

    change username to lowercase

--- hackercup/consistency_chapter2.d
+++ hackercup/consistency_chapter2.d
@@ -1,4 +1,4 @@
-// Vicfred
+// vicfred
 // https://www.facebook.com/codingcompetitions/hacker-cup/2021/qualification-round/problems/A2
 // floyd warshall, graph, brute force, bfs, dfs
 import std.algorithm;
