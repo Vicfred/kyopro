d48815f1087b4399439369170bcd5fef154818fe
vicfred <git@vicfred.dev>
2025-04-19 12:08:54 -0700

    ABC386C Add comment on the problem

--- atcoder/abc386c_operate_1.cpp
+++ atcoder/abc386c_operate_1.cpp
@@ -1,6 +1,15 @@
 // vicfred
 // https://atcoder.jp/contests/abc386/tasks/abc386_c
 // string manipulation, edit distance, levenshtein, string
+
+/*
+ * The problem for K > 1 can be solved with a modification
+ * of the classical DP for the Levenshtein distance.
+ *
+ * The problem can be solved directly with the Levenshtein
+ * automaton but I couldn't implement it or find an
+ * implementation.
+*/
 #include <algorithm>
 #include <cstdint>
 #include <iostream>
