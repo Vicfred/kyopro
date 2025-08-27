17f17aad6189e0d368e5c5baa9a960725dcd1ade
vicfred <git@vicfred.dev>
2025-08-27 15:59:18 -0400

    ABC151A Next Alphabet

---
+++ atcoder/abc151A_next_alphabet.d
@@ -0,0 +1,11 @@
+// vicfred
+// https://atcoder.jp/contests/abc151/tasks/abc151_a
+// string manipulation
+import std.stdio;
+import std.string;
+
+void main() {
+  char c = readln.chomp[0];
+  char d = cast(char)(c + 1);
+  d.writeln;
+}
